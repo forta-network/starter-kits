255d5f8ade07a7fe6511d01d8cd973353e768e03
christian-forta <christian@forta.org>
2022-04-21 15:30:52 -0700

    initial version of the money laundering bot

---
+++ money-laundering-tornado-cash-py/README.md
@@ -0,0 +1,29 @@
+# Money Laundering through Tornado Cash
+
+## Description
+
+This detection bot detects when numerous large transfers are made to Tornado Cash potentially indicative of money laundering activity post-hack.
+
+## Supported Chains
+
+- Ethereum
+- BSC
+- Polygon
+- Optimism
+- Arbitrum
+
+## Alerts
+
+Describe each of the type of alerts fired by this agent
+
+- POSSIBLE-MONEY-LAUNDERING-TORNADO-CASH
+  - Fired when possible money laundering is identified
+  - Metadata will contain amount if funds transferred in the block range specifified in the configuration
+  - Findings severity will be High
+
+## Test Data
+
+The agent behaviour can be verified with the following transactions:
+
+- Ethereum block 14602829-14602878 (beanstalk farms hack - https://rekt.news/beanstalk-rekt/)
+- Ethereum block 14506449,14506451,14506454 (inverse finance hack - https://rekt.news/inverse-finance-rekt/)
---
+++ money-laundering-tornado-cash-py/src/agent.py
@@ -0,0 +1,83 @@
+import forta_agent
+from forta_agent import get_json_rpc_url
+from web3 import Web3
+
+from src.constants import BLOCK_RANGE, TORNADO_CASH_ADDRESSES, TORNADO_CASH_TRANSFER_COUNT_THRESHOLD_ETH, TORNADO_CASH_TRANSFER_COUNT_THRESHOLD_MATIC, TORNADO_CASH_ACCOUNTS_QUEUE_SIZE
+from src.findings import MoneyLaunderingTornadoCashFindings
+
+web3 = Web3(Web3.HTTPProvider(get_json_rpc_url()))
+
+ACCOUNT_TO_TORNADO_CASH_BLOCKS = {}  # dict of accounts to dicts of blocks to counts; e.g. # account 1, block 101, 1
+ACCOUNT_QUEUE = []
+
+
+def initialize():
+    """
+    this function initializes the state variables that are tracked across tx and blocks
+    it is called from test to reset state between tests
+    """
+    global ACCOUNT_TO_TORNADO_CASH_BLOCKS
+    ACCOUNT_TO_TORNADO_CASH_BLOCKS = {}
+
+    global ACCOUNT_QUEUE
+    ACCOUNT_QUEUE = []
+
+
+def detect_money_laundering(w3, transaction_event: forta_agent.transaction_event.TransactionEvent) -> list:
+    global ACCOUNT_TO_TORNADO_CASH_BLOCKS
+    global ACCOUNT_QUEUE
+
+    findings = []
+    accounts_sending_funds = set()
+
+    for trace in transaction_event.traces:
+        if trace.action.to is None:
+            continue
+
+        account = Web3.toChecksumAddress(trace.action.to)
+        if trace.action.value is not None and trace.action.value > 0 and account == TORNADO_CASH_ADDRESSES[w3.eth.chain_id]:
+            accounts_sending_funds.add(account)
+            ACCOUNT_QUEUE.append(account)
+
+            block_to_tx_count = {}
+            if Web3.toChecksumAddress(trace.action.to) not in ACCOUNT_TO_TORNADO_CASH_BLOCKS:
+                ACCOUNT_TO_TORNADO_CASH_BLOCKS[account] = block_to_tx_count
+            else:
+                block_to_tx_count = ACCOUNT_TO_TORNADO_CASH_BLOCKS[account]
+
+            if transaction_event.block_number not in block_to_tx_count.keys():
+                block_to_tx_count[transaction_event.block_number] = 1
+            else:
+                block_to_tx_count[transaction_event.block_number] += 1
+
+            #  maintain a size
+            if len(ACCOUNT_QUEUE) > TORNADO_CASH_ACCOUNTS_QUEUE_SIZE:
+                acc = ACCOUNT_QUEUE.pop(0)
+                ACCOUNT_TO_TORNADO_CASH_BLOCKS.pop(acc, None)
+
+            while max(block_to_tx_count.keys()) - min(block_to_tx_count.keys()) > BLOCK_RANGE:
+                #  remove the oldest blocks
+                oldest_block = min(block_to_tx_count, key=block_to_tx_count.get)
+                block_to_tx_count.pop(oldest_block, None)
+
+    for account in accounts_sending_funds:
+        total_txs = sum(ACCOUNT_TO_TORNADO_CASH_BLOCKS[account].values())
+        tx_threshold = TORNADO_CASH_TRANSFER_COUNT_THRESHOLD_MATIC if w3.eth.chain_id == 137 else TORNADO_CASH_TRANSFER_COUNT_THRESHOLD_ETH
+        if total_txs >= tx_threshold:
+            findings.append(MoneyLaunderingTornadoCashFindings.possible_money_laundering_tornado_cash(account, total_txs))
+
+    return findings
+
+
+def provide_handle_transaction(w3):
+    def handle_transaction(transaction_event: forta_agent.transaction_event.TransactionEvent) -> list:
+        return detect_money_laundering(w3, transaction_event)
+
+    return handle_transaction
+
+
+real_handle_transaction = provide_handle_transaction(web3)
+
+
+def handle_transaction(transaction_event: forta_agent.transaction_event.TransactionEvent):
+    return real_handle_transaction(transaction_event)
