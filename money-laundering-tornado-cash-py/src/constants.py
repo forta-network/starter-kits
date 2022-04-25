f47b45d70556c19f30af4e43a25b5cf35edb4f5a
christian-forta <christian@forta.org>
2022-04-25 13:31:13 -0700

    addressed mariko's comments

--- money-laundering-tornado-cash-py/src/constants.py
+++ money-laundering-tornado-cash-py/src/constants.py
@@ -9,13 +9,13 @@ TORNADO_CASH_ACCOUNTS_QUEUE_SIZE = 10000  # how many accounts should be tracked 
 # Obtained from https://docs.tornado.cash/general/tornado-cash-smart-contracts#tornado-cash-classic-pools-contracts
 TORNADO_CASH_ROUTER_ADDRESS = '0xd90e2f925DA726b50C4Ed8D0Fb90Ad053324F31b'
 TORNADO_CASH_ADDRESSES = {1: "0xA160cdAB225685dA1d56aa342Ad8841c3b53f291",  # Ethereum Mainnet - 100 ETH
-                          56: "0x1E34A77868E19A6647b1f2F47B51ed72dEDE95DD",  # BSC - 100 ETH
+                          56: "0x1E34A77868E19A6647b1f2F47B51ed72dEDE95DD",  # BSC - 100 BNB
                           42161: "0x1E34A77868E19A6647b1f2F47B51ed72dEDE95DD",  # ARBITRUM - 100 ETH
                           10: "0x1E34A77868E19A6647b1f2F47B51ed72dEDE95DD",  # OPTIMISM - 100 ETH
                           137: "0xa5C2254e4253490C54cef0a4347fddb8f75A4998"  # POLYGON - 100000 MATIC
                           }
 TORNADO_CASH_DEPOSIT_SIZE = 100
-TORNADO_CASH_DEPOSIT_SIZE_MATIC = 10000
+TORNADO_CASH_DEPOSIT_SIZE_MATIC = 100000
 
 
 TORNADO_CASH_DEPOSIT_TOPIC = '0xa945e51eec50ab98c161376f0db4cf2aeba3ec92755fe2fcd388bdbbb80ff196'
