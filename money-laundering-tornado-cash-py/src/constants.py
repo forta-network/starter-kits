e3eaa9348bcdc7793ff16b9010ab8cf36730c6b9
christian-forta <christian@forta.org>
2022-05-11 11:52:45 -0700

    added logging; removed tornado cash router usage

--- money-laundering-tornado-cash-py/src/constants.py
+++ money-laundering-tornado-cash-py/src/constants.py
@@ -7,7 +7,6 @@ TORNADO_CASH_TRANSFER_COUNT_THRESHOLD_BSC = 25  # 1M USD / (100 BSC * 400 USD)
 TORNADO_CASH_ACCOUNTS_QUEUE_SIZE = 10000  # how many accounts should be tracked by the bot in memory before dequeuing items
 
 # Obtained from https://docs.tornado.cash/general/tornado-cash-smart-contracts#tornado-cash-classic-pools-contracts
-TORNADO_CASH_ROUTER_ADDRESS = '0xd90e2f925DA726b50C4Ed8D0Fb90Ad053324F31b'
 TORNADO_CASH_ADDRESSES = {1: "0xA160cdAB225685dA1d56aa342Ad8841c3b53f291",  # Ethereum Mainnet - 100 ETH
                           56: "0x1E34A77868E19A6647b1f2F47B51ed72dEDE95DD",  # BSC - 100 BNB
                           42161: "0x1E34A77868E19A6647b1f2F47B51ed72dEDE95DD",  # ARBITRUM - 100 ETH
