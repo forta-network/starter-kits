34152b2235c6073d8699364ad7ac3aac9244bb61
christian-forta <christian@forta.org>
2022-05-11 14:49:20 -0700

    fixed the provide handle

--- unverified-contract-py/src/agent.py
+++ unverified-contract-py/src/agent.py
@@ -174,8 +174,8 @@ def provide_handle_transaction(w3, etherscan):
 
         cache_contract_creation(w3, etherscan, transaction_event)
         # uncomment for local testing; otherwise the process will exit
-        while (thread.is_alive()):
-            pass
+        # while (thread.is_alive()):
+        #    pass
 
         findings = FINDINGS_CACHE
         FINDINGS_CACHE = []
