1d8ba0db95f9ddd11f36e632af090ed98bf04397
christian-forta <christian@forta.org>
2022-05-11 11:34:43 -0700

    added address extraction capabilities

--- unverified-contract-py/src/agent.py
+++ unverified-contract-py/src/agent.py
@@ -1,13 +1,29 @@
+import logging
+import sys
+
 import forta_agent
 import rlp
-from findings import UnverifiedCodeContractFindings
 from forta_agent import get_json_rpc_url
-from constants import ETHERSCAN_API_KEY
-from etherscan import Etherscan
+from hexbytes import HexBytes
+from pyevmasm import disassemble_hex
 from web3 import Web3
+
+from src.constants import CONTRACT_SLOT_ANALYSIS_DEPTH, ETHERSCAN_API_KEY
+from src.etherscan import Etherscan
+from src.findings import UnverifiedCodeContractFindings
 
 web3 = Web3(Web3.HTTPProvider(get_json_rpc_url()))
 etherscan = Etherscan(ETHERSCAN_API_KEY)
+
+
+root = logging.getLogger()
+root.setLevel(logging.INFO)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+root.addHandler(handler)
 
 
 def calc_contract_address(w3, address, nonce) -> str:
@@ -20,8 +36,62 @@ def calc_contract_address(w3, address, nonce) -> str:
     return Web3.toChecksumAddress(Web3.keccak(rlp.encode([address_bytes, nonce]))[-20:])
 
 
+def is_contract(w3, address) -> bool:
+    """
+    this function determines whether address is a contract
+    :return: is_contract: bool
+    """
+    if address is None:
+        return True
+    code = w3.eth.get_code(Web3.toChecksumAddress(address))
+    return code != HexBytes('0x')
+
+
+def get_storage_addresses(w3, address) -> set:
+    """
+    this function returns the addresses that are references in the storage of a contract (first CONTRACT_SLOT_ANALYSIS_DEPTH slots)
+    :return: address_list: list (only returning contract addresses)
+    """
+    if address is None:
+        return set()
+
+    address_set = set()
+    for i in range(CONTRACT_SLOT_ANALYSIS_DEPTH):
+        mem = w3.eth.get_storage_at(Web3.toChecksumAddress(address), i)
+        if mem != HexBytes('0x0000000000000000000000000000000000000000000000000000000000000000'):
+            # looking at both areas of the storage slot as - depending on packing - the address could be at the beginning or the end.
+            if is_contract(w3, mem[0:20]):
+                address_set.add(Web3.toChecksumAddress(mem[0:20].hex()))
+            if is_contract(w3, mem[12:]):
+                address_set.add(Web3.toChecksumAddress(mem[12:].hex()))
+
+    return address_set
+
+
+def get_opcode_addresses(w3, address) -> set:
+    """
+    this function returns the addresses that are references in the opcodes of a contract
+    :return: address_list: list (only returning contract addresses)
+    """
+    if address is None:
+        return set()
+
+    code = w3.eth.get_code(Web3.toChecksumAddress(address))
+    opcode = disassemble_hex(code.hex())
+    address_set = set()
+    for op in opcode.splitlines():
+        for param in op.split(' '):
+            if param.startswith('0x') and len(param) == 42:
+                if is_contract(w3, param):
+                    address_set.add(Web3.toChecksumAddress(param))
+
+    return address_set
+
+
 def detect_unverified_contract_creation(w3, etherscan, transaction_event: forta_agent.transaction_event.TransactionEvent) -> list:
     findings = []
+
+    logging.info(f"Scanning transaction {transaction_event.transaction.hash} on chain {w3.eth.chain_id}")
 
     created_contract_addresses = []
     for trace in transaction_event.traces:
@@ -31,7 +101,13 @@ def detect_unverified_contract_creation(w3, etherscan, transaction_event: forta_
                 nonce = transaction_event.transaction.nonce if transaction_event.from_ == trace.action.from_ else 1  # for contracts creating other contracts, the nonce would be 1
                 created_contract_address = calc_contract_address(w3, trace.action.from_, nonce)
                 if not etherscan.is_verified(created_contract_address):
-                    findings.append(UnverifiedCodeContractFindings.unverified_code(trace.action.from_, created_contract_address))
+                    logging.info(f"Identified unverified contract: {created_contract_address}")
+                    storage_addresses = get_storage_addresses(w3, created_contract_address)
+                    opcode_addresses = get_opcode_addresses(w3, created_contract_address)
+
+                    findings.append(UnverifiedCodeContractFindings.unverified_code(trace.action.from_, created_contract_address, set.union(storage_addresses, opcode_addresses)))
+                else:
+                    logging.info(f"Identified verified contract: {created_contract_address}")
 
     return findings
 
--- unverified-contract-py/src/agent_test.py
+++ unverified-contract-py/src/agent_test.py
@@ -1,13 +1,35 @@
+from forta_agent import FindingSeverity, FindingType, create_transaction_event
+
 import agent
 from etherscan_mock import EtherscanMock
-from forta_agent import FindingSeverity, FindingType, create_transaction_event
-from web3_mock import EOA_ADDRESS, Web3Mock
+from web3_mock import (CONTRACT_NO_ADDRESS, CONTRACT_WITH_ADDRESS, EOA_ADDRESS,
+                       Web3Mock)
 
 w3 = Web3Mock()
 etherscan = EtherscanMock()
 
 
 class TestUnverifiedContractAgent:
+    def test_get_opcode_addresses_eoa(self):
+        addresses = agent.get_opcode_addresses(w3, EOA_ADDRESS)
+        assert len(addresses) == 0, "should be empty"
+
+    def test_get_opcode_addresses_no_addr(self):
+        addresses = agent.get_opcode_addresses(w3, CONTRACT_NO_ADDRESS)
+        assert len(addresses) == 0, "should not be empty"
+
+    def test_get_opcode_addresses_with_addr(self):
+        addresses = agent.get_opcode_addresses(w3, CONTRACT_WITH_ADDRESS)
+        assert len(addresses) == 1, "should not be empty"
+
+    def test_storage_addresses_with_addr(self):
+        addresses = agent.get_opcode_addresses(w3, CONTRACT_WITH_ADDRESS)
+        assert len(addresses) == 1, "should not be empty"
+
+    def test_storage_addresses_on_eoa(self):
+        addresses = agent.get_opcode_addresses(w3, EOA_ADDRESS)
+        assert len(addresses) == 0, "should be empty; EOA has no storage"
+
     def test_calc_contract_address(self):
         contract_address = agent.calc_contract_address(w3, EOA_ADDRESS, 9)
         assert contract_address == "0x728ad672409DA288cA5B9AA85D1A55b803bA97D7", "should be the same contract address"
@@ -39,6 +61,7 @@ class TestUnverifiedContractAgent:
         assert finding.severity == FindingSeverity.Medium
         assert finding.type == FindingType.Suspicious
         assert finding.description == f'{EOA_ADDRESS} created contract 0x728ad672409DA288cA5B9AA85D1A55b803bA97D7'
+        assert len(finding.metadata) > 0
 
     def test_detect_unverified_contract_with_verified_contract(self):
         tx_event = create_transaction_event({
@@ -64,7 +87,6 @@ class TestUnverifiedContractAgent:
         findings = agent.detect_unverified_contract_creation(w3, etherscan, tx_event)
         assert len(findings) == 0, "should have 0 finding"
 
-
     def test_detect_unverified_contract_call_only(self):
         tx_event = create_transaction_event({
             'transaction': {
@@ -78,7 +100,7 @@ class TestUnverifiedContractAgent:
             'traces': [{'type': 'call',
                         'action': {
                             'from': EOA_ADDRESS,
-                            'to': '0x728ad672409DA288cA5B9AA85D1A55b803bA97D7', #unverified contract
+                            'to': '0x728ad672409DA288cA5B9AA85D1A55b803bA97D7',  # unverified contract
                             'value': 1,
                         }
                         }
