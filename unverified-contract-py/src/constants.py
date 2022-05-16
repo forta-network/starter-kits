532bc9e915133c9a6ec06f85e2ffe05e7b1185be
christian-forta <christian@forta.org>
2022-05-16 14:24:03 -0700

    removing and disabling API keys

--- unverified-contract-py/src/constants.py
+++ unverified-contract-py/src/constants.py
@@ -1,5 +1,2 @@
-ETHERSCAN_API_KEY = '82WZA718ZKYJIYK2ETA4VZNRVK9NJZBN1G'
-BSC_API_KEY = 'YM5QIBU1WUXM8H2G4SYAN5GBYGHP7YR9Y2'
-POLYGON_API_KEY = 'XATYCGJQ7S674WZ3UUDDDQ2ZCH7MTC922S'
 CONTRACT_SLOT_ANALYSIS_DEPTH = 20  # how many slots should be read to extract contract addresses from created contract
 WAIT_TIME = 30  # how many minutes after contract creation we will wait for the creator to share source code on etherscan
