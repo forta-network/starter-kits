19c4d521c083bfc41daee7029a80b0d5728679d3
christian-forta <christian@forta.org>
2022-06-21 14:29:30 -0700

    initial commit; not completed yet

---
+++ time-series-analyzer-template/src/constants.py
@@ -0,0 +1,8 @@
+BOT_ID = "0x492c05269cbefe3a1686b999912db1fb5a39ce2e4578ac3951b0542440f435d9"
+ALERT_NAME = "Reentrancy calls detected"
+
+CONTRACT_ADDRESS = "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45"
+
+BUCKET_WINDOW_IN_MINUTES = 5
+TRAINING_WINDOW_IN_BUCKET_SIZE = 12*24*7  # recommended to cover training period during which the periodicity can be observed
+INTERVAL_WIDTH = 0.80
