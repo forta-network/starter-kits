ec1b75e931e9a86402dea0a0ffc14a2118f46a8e
christian-forta <christian@forta.org>
2022-06-28 16:12:58 -0700

    final commit after deployment and local testing

--- time-series-analyzer-template/src/findings.py
+++ time-series-analyzer-template/src/findings.py
@@ -6,7 +6,7 @@ class TimeSeriesAnalyzerFinding:
     @staticmethod
     def breakout(direction: str, expected_value: float, range_boundary: float, observed_value: float, contract_address: str, bot_id: str, alert_name: str, type: FindingType, severity: FindingSeverity) -> Finding:
 
-        meta_data = {"Expected_value": expected_value, "Range_boundary": range_boundary, "Observed_value": observed_value, "Contract_address": contract_address}
+        meta_data = {"Expected_value": str(expected_value), "Range_boundary": str(range_boundary), "Observed_value": str(observed_value), "Contract_address": contract_address}
 
         return Finding({
             'name': 'Time series analysis bot identified breakout',
