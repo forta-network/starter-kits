3bcf6a3a21aae967782245c2e698aa435f95df50
christian-forta <christian@forta.org>
2022-07-01 14:58:28 -0700

    fixed FPs; integrated new bots; optimized API call

--- alert-combiner-py/src/agent_test.py
+++ alert-combiner-py/src/agent_test.py
@@ -1,9 +1,11 @@
-import agent
-from forta_agent import create_block_event
-from web3_mock import Web3Mock, EOA_ADDRESS, CONTRACT
-from forta_explorer_mock import FortaExplorerMock
-import pandas as pd
 import time
+
+import pandas as pd
+from forta_agent import create_block_event
+
+import agent
+from forta_explorer_mock import FortaExplorerMock
+from web3_mock import CONTRACT, EOA_ADDRESS, Web3Mock
 
 w3 = Web3Mock()
 
@@ -14,6 +16,15 @@ class TestAlertCombiner:
 
     def test_is_contract_null(self):
         assert not agent.is_contract(w3, '0x0000000000a00000000000000000000000000000'), "EOA shouldn't be identified as a contract"
+
+    def test_is_address_valid(self):
+        assert agent.is_address(w3, '0x7328BBc3EaCfBe152f569f2C09f96f915F2C8D73'), "this should be a valid address"
+
+    def test_is_address_aaa(self):
+        assert not agent.is_address(w3, '0x7328BBaaaaaaaaa52f569f2C09f96f915F2C8D73'), "this shouldnt be a valid address"
+
+    def test_is_address_aAa(self):
+        assert not agent.is_address(w3, '0x7328BBaaaaAaaaa52f569f2C09f96f915F2C8D73'), "this shouldnt be a valid address"
 
     def test_is_contract_contract(self):
         assert agent.is_contract(w3, CONTRACT), "EOA should be identified as a contract"
@@ -38,7 +49,7 @@ class TestAlertCombiner:
 
             ["2022-04-30T23:55:17.284158264Z", "Money Laundering", "ethereum",
              "SUSPICIOUS", {"transactionHash": "0x53244cc27feed6c1d7f44381119cf14054ef2aa6ea7fbec5af4e4258a5a02617", "block": {"number": 14688607, "chainId": 1}, "agent": {"id": "0x4adff9a0ed29396d51ef3b16297070347aab25575f04a4e2bd62ec43ca4508d2"}},
-             "HIGH", {}, "POSSIBLE-MONEY-LAUNDERING-TORNADO-CASH", "description", ["0x1c5dCdd006EA78a7E4783f9e6021C32935a10fb4"], [], "0x32abd26df70f12b4d2527a092b8f42a467dd6356fcff57a0d9241ac1c6244e13"]
+             "HIGH", {}, "POSSIBLE-MONEY-LAUNDERING-TORNADO-CASH", "0x1c5dCdd006EA78a7E4783f9e6021C32935a10fb4 potentially engaged in money laundering", ["0x1c5dCdd006EA78a7E4783f9e6021C32935a10fb4"], [], "0x32abd26df70f12b4d2527a092b8f42a467dd6356fcff57a0d9241ac1c6244e13"]
         ], columns=['createdAt', 'name', 'protocol', 'findingType', 'source', 'severity', 'metadata', 'alertId', 'description', 'addresses', 'contracts', 'hash'])
 
         forta_explorer.set_df(df_forta)
@@ -74,7 +85,7 @@ class TestAlertCombiner:
 
             ["2022-04-30T23:55:17.284158264Z", "Money Laundering", "ethereum",
              "SUSPICIOUS", {"transactionHash": "0x53244cc27feed6c1d7f44381119cf14054ef2aa6ea7fbec5af4e4258a5a02617", "block": {"number": 14688607, "chainId": 1}, "agent": {"id": "0x4adff9a0ed29396d51ef3b16297070347aab25575f04a4e2bd62ec43ca4508d2"}},
-             "HIGH", {}, "POSSIBLE-MONEY-LAUNDERING-TORNADO-CASH", "description", ["0x1c5dCdd006EA78a7E4783f9e6021C32935a10fb4"], [], "0x32abd26df70f12b4d2527a092b8f42a467dd6356fcff57a0d9241ac1c6244e13"]
+             "HIGH", {}, "POSSIBLE-MONEY-LAUNDERING-TORNADO-CASH", "0x1c5dCdd006EA78a7E4783f9e6021C32935a10fb4 potentially engaged in money laundering", ["0x1c5dCdd006EA78a7E4783f9e6021C32935a10fb4"], [], "0x32abd26df70f12b4d2527a092b8f42a467dd6356fcff57a0d9241ac1c6244e13"]
         ], columns=['createdAt', 'name', 'protocol', 'findingType', 'source', 'severity', 'metadata', 'alertId', 'description', 'addresses', 'contracts', 'hash'])
 
         forta_explorer.set_df(df_forta)
@@ -109,7 +120,7 @@ class TestAlertCombiner:
 
             ["2022-04-30T23:55:17.284158264Z", "Money Laundering", "ethereum",
              "SUSPICIOUS", {"transactionHash": "0x53244cc27feed6c1d7f44381119cf14054ef2aa6ea7fbec5af4e4258a5a02617", "block": {"number": 14688607, "chainId": 1}, "agent": {"id": "0x4adff9a0ed29396d51ef3b16297070347aab25575f04a4e2bd62ec43ca4508d2"}},
-             "HIGH", {}, "POSSIBLE-MONEY-LAUNDERING-TORNADO-CASH", "description", ["0x1c5dCdd006EA78a7E4783f9e6021C32935a10fb4"], [], "0x32abd26df70f12b4d2527a092b8f42a467dd6356fcff57a0d9241ac1c6244e13"]
+             "HIGH", {}, "POSSIBLE-MONEY-LAUNDERING-TORNADO-CASH", "0x1c5dCdd006EA78a7E4783f9e6021C32935a10fb4 potentially engaged in money laundering", ["0x1c5dCdd006EA78a7E4783f9e6021C32935a10fb4"], [], "0x32abd26df70f12b4d2527a092b8f42a467dd6356fcff57a0d9241ac1c6244e13"]
         ], columns=['createdAt', 'name', 'protocol', 'findingType', 'source', 'severity', 'metadata', 'alertId', 'description', 'addresses', 'contracts', 'hash'])
 
         forta_explorer.set_df(df_forta)
