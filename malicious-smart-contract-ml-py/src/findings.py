2ae55f7f56f34d06eccf50f52c255591c4b3e83c
christian-forta <christian@forta.org>
2022-09-07 09:46:17 -0700

    initial malicious smart contract ML bot

---
+++ malicious-smart-contract-ml-py/src/findings.py
@@ -0,0 +1,19 @@
+from forta_agent import Finding, FindingType, FindingSeverity
+
+
+class MaliciousContractFindings:
+
+    @staticmethod
+    def malicious_contract_creation(from_address: str, contract_address: str, contained_addresses: set, model_score: float, model_threshold: float) -> Finding:
+        metadata = {"address_contained_in_created_contract_" + str(i): address for i, address in enumerate(contained_addresses, 1)}
+        metadata["model_score"] = str(model_score)
+        metadata["model_threshold"] = str(model_threshold)
+
+        return Finding({
+            'name': 'Malcious Contract Creation',
+            'description': f'{from_address} created contract {contract_address}',
+            'alert_id': 'MALICIOUS-CONTRACT-CREATION',
+            'type': FindingType.Suspicious,
+            'severity': FindingSeverity.High,
+            'metadata': metadata
+        })
