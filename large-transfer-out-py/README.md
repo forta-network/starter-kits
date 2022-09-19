e3e827533052d26ab14fb1efefdd43cb0114cede
christian-forta <christian@forta.org>
2022-09-19 15:05:40 -0700

    initial version of large transfer out bot

---
+++ large-transfer-out-py/README.md
@@ -0,0 +1,25 @@
+# Large Transfer Out Bot
+
+## Description
+
+This agent detects transactions with large native transfers that the account didnt hold X days ago, which could be indicative of money laundering. By itself, this bot will be quite noisy.
+
+## Supported Chains
+
+- Ethereum
+
+## Alerts
+
+Describe each of the type of alerts fired by this agent
+
+- LARGE-TRANSFER-OUT
+  - Fired when a transaction contains a large native transfer and account didnt have those assets X days ago
+  - Severity is always set to "low" (mention any conditions where it could be something else)
+  - Type is always set to "suspicious" (mention any conditions where it could be something else)
+  - Mention any other type of metadata fields included with this alert
+
+## Test Data
+
+The agent behaviour can be verified with the following transactions:
+
+- 0x39ed9312dabfe228ab03659192540da18b97f89eb7b89abaa9a6da03011e9668 (Gera Coin Attacker)
---
+++ large-transfer-out-py/src/agent.py
@@ -0,0 +1,62 @@
+import forta_agent
+from forta_agent import Finding, FindingType, FindingSeverity, get_json_rpc_url
+from src.constants import LARGE_TRANSFER_THRESHOLD_IN_WEI, DAY_LOOKBACK_WINDOW, LOOKBACK_WINDOW_VALUE_THRESHOLD_IN_WEI
+from web3 import Web3
+
+web3 = Web3(Web3.HTTPProvider(get_json_rpc_url()))
+
+
+def initialize():
+    """
+    this function initializes the state variables that are tracked across tx and blocks
+    it is called from test to reset state between tests
+    """
+
+def detect_suspicious_native_transfers(w3, transaction_event: forta_agent.transaction_event.TransactionEvent) -> list:
+    findings = []
+
+    # filter the transaction logs for any Tether transfers
+    value = transaction_event.transaction.value
+    if value >= LARGE_TRANSFER_THRESHOLD_IN_WEI:
+        to = transaction_event.to
+        from_ = transaction_event.from_
+
+        block_number = transaction_event.block_number
+        BLOCK_TIME = 15  # seconds
+        older_block_number = block_number - (24 * 60 * 60 * DAY_LOOKBACK_WINDOW)//BLOCK_TIME
+        older_value = w3.eth.get_balance(Web3.toChecksumAddress(from_), block_identifier=older_block_number)
+        current_value = w3.eth.get_balance(Web3.toChecksumAddress(from_), block_identifier=block_number)
+
+        if older_value < LOOKBACK_WINDOW_VALUE_THRESHOLD_IN_WEI:
+            findings.append(Finding({
+                'name': 'Large Native Transfer Out',
+                'description': f'High amount of native tokens transferred: {value}',
+                'alert_id': 'LARGE-TRANSFER-OUT',
+                'severity': FindingSeverity.Low,
+                'type': FindingType.Info,
+                'metadata': {
+                    'to': to,
+                    'from': from_,
+                    'current_block': block_number,
+                    'balance_at_current_block': current_value,
+                    'balance_at_older_block': older_value,
+                    'older_block': older_block_number,
+                    'transfer_value': value
+                }
+            }))
+
+    return findings
+
+
+def provide_handle_transaction(w3):
+    def handle_transaction(transaction_event: forta_agent.transaction_event.TransactionEvent) -> list:
+        return detect_suspicious_native_transfers(w3, transaction_event)
+
+    return handle_transaction
+
+
+real_handle_transaction = provide_handle_transaction(web3)
+
+
+def handle_transaction(transaction_event: forta_agent.transaction_event.TransactionEvent):
+    return real_handle_transaction(transaction_event)
