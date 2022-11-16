a45f055c1f53092ec427f6d47e0bda7b3a5b13fb
christian-forta <christian@forta.org>
2022-11-16 11:30:36 -0800

    added new account funding

--- entity-cluster-bot/src/web3_mock.py
+++ entity-cluster-bot/src/web3_mock.py
@@ -6,6 +6,12 @@ EOA_ADDRESS_NEW = '0xdec08cb92a506B88411da9Ba290f3694BE223c26'
 EOA_ADDRESS_SMALL_TX = '0x6ADEBC8729d03c3dFAAD6660B746754Bc475E13d'  
 EOA_ADDRESS_LARGE_TX = '0x942dFB0C7e87fb5f07e25EC7Ff805e7F973cF929'  
 
+
+EOA_ADDRESS_FUNDER_NEW = '0x1c5dCdd006EA78a7E4783f9e6021C3293aaaaaaa' 
+EOA_ADDRESS_FUNDER_OLD = '0xdec08cb92a506B88411da9Ba290f3694Bbbbbbbb' 
+EOA_ADDRESS_FUNDED_NEW = '0x1c5dCdd006EA78a7E4783f9e6021C329cccccccc' 
+EOA_ADDRESS_FUNDED_OLD = '0xdec08cb92a506B88411da9Ba290f3694Bddddddd' 
+
 CONTRACT = '0xf11ED77fD65840b64602526DDC38311E9923c81B'
 
 
@@ -18,11 +24,21 @@ class EthMock:
     def __init__(self):
         self.contract = ContractMock()
 
-    def get_transaction_count(self, address):
-        if address == EOA_ADDRESS_SMALL_TX:
+    def get_transaction_count(self, address, block_number=0):
+        if address.lower() == EOA_ADDRESS_SMALL_TX.lower():
             return 499
-        elif address == EOA_ADDRESS_LARGE_TX:
+        elif address.lower() == EOA_ADDRESS_LARGE_TX.lower():
             return 501
+        elif address.lower() == EOA_ADDRESS_FUNDER_NEW.lower():
+            return 0
+        elif address.lower() == EOA_ADDRESS_FUNDER_OLD.lower():
+            return 6
+        elif address.lower() == EOA_ADDRESS_FUNDED_NEW.lower():
+            return 0
+        elif address.lower() == EOA_ADDRESS_FUNDED_OLD.lower():
+            return 6
+        elif address.lower() == EOA_ADDRESS_OLD.lower():
+            return 6
         return 0
 
     def get_code(self, address):
