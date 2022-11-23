4a0f3f45496e98973d8e4d54e2814736151d9bb3
christian-forta <christian@forta.org>
2022-11-23 14:51:40 -0800

    added happy path

--- alert-combiner-py/src/agent_test.py
+++ alert-combiner-py/src/agent_test.py
@@ -4,22 +4,24 @@ import agent
 import json
 import os
 from datetime import datetime, timedelta
-from constants import (ALERT_MAX_QUEUE_SIZE, ALERTS_LOOKBACK_WINDOW_IN_HOURS, BASE_BOTS,
-                       ALERTS_KEY, ALERTED_CLUSTERS_KEY, ENTITY_CLUSTER_ALERTS_KEY, FP_MITIGATION_ALERTS_KEY)
+from constants import (ALERTS_DATA_MAX_QUEUE_SIZE, ALERTS_LOOKBACK_WINDOW_IN_HOURS, BASE_BOTS,
+                       ALERTS_DATA_KEY, ALERTED_CLUSTERS_KEY, ENTITY_CLUSTERS_KEY, FP_MITIGATION_ALERTS_KEY)
 from web3_mock import CONTRACT, EOA_ADDRESS, Web3Mock
+from luabase_mock import LuabaseMock
 
 w3 = Web3Mock()
+luabase = LuabaseMock()
 
 
 class TestAlertCombiner:
 
     def remove_persistent_state():
-        if os.path.isfile(ALERTS_KEY):
-            os.remove(ALERTS_KEY)
+        if os.path.isfile(ALERTS_DATA_KEY):
+            os.remove(ALERTS_DATA_KEY)
         if os.path.isfile(ALERTED_CLUSTERS_KEY):
             os.remove(ALERTED_CLUSTERS_KEY)
-        if os.path.isfile(ENTITY_CLUSTER_ALERTS_KEY):
-            os.remove(ENTITY_CLUSTER_ALERTS_KEY)
+        if os.path.isfile(ENTITY_CLUSTERS_KEY):
+            os.remove(ENTITY_CLUSTERS_KEY)
         if os.path.isfile(FP_MITIGATION_ALERTS_KEY):
             os.remove(FP_MITIGATION_ALERTS_KEY)
 
@@ -93,6 +95,8 @@ class TestAlertCombiner:
         assert not agent.in_list(alert, BASE_BOTS), "should be in list"
 
     def test_initialize(self):
+        TestAlertCombiner.remove_persistent_state()
+
         subscription_json = agent.initialize()
         json.dumps(subscription_json)
         assert True, "Bot should initialize successfully"
@@ -105,13 +109,13 @@ class TestAlertCombiner:
                  "hash": "0xabc",
                  "description": "description",
                  "alertId": "IMPOSSIBLE-2",
-                 "createdAt": datetime.now().strftime("%Y-%m-%dT%H:%M:%S%fZ"),  # 2022-11-18T03:01:21.457234676Z
+                 "createdAt": datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f123Z"),  # 2022-11-18T03:01:21.457234676Z
                  "source":
                     {"bot": {'id': "0x0ffe038c802784f739bb27fcd4274f71c384fea78de87c9ef8d5b3fb72b514c7"}}
                  }
              })
         alert_list = []
-        agent.update_list(alert, ALERT_MAX_QUEUE_SIZE, BASE_BOTS, ALERTS_LOOKBACK_WINDOW_IN_HOURS, alert_list)
+        agent.update_list(alert, ALERTS_DATA_MAX_QUEUE_SIZE, BASE_BOTS, ALERTS_LOOKBACK_WINDOW_IN_HOURS, alert_list)
 
         assert len(alert_list) == 1, "should be in list"
 
@@ -123,13 +127,13 @@ class TestAlertCombiner:
                  "hash": "0xabc",
                  "description": "description",
                  "alertId": "IMPOSSIBLE-2",
-                 "createdAt": (datetime.now() - timedelta(days=3)).strftime("%Y-%m-%dT%H:%M:%S%fZ"),  # 2022-11-18T03:01:21.457234676Z
+                 "createdAt": (datetime.now() - timedelta(days=3)).strftime("%Y-%m-%dT%H:%M:%S.%f123Z"),  # 2022-11-18T03:01:21.457234676Z
                  "source":
                     {"bot": {'id': "0x0ffe038c802784f739bb27fcd4274f71c384fea78de87c9ef8d5b3fb72b514c7"}}
                  }
              })
         alert_list = []
-        agent.update_list(alert, ALERT_MAX_QUEUE_SIZE, BASE_BOTS, ALERTS_LOOKBACK_WINDOW_IN_HOURS, alert_list)
+        agent.update_list(alert, ALERTS_DATA_MAX_QUEUE_SIZE, BASE_BOTS, ALERTS_LOOKBACK_WINDOW_IN_HOURS, alert_list)
 
         assert len(alert_list) == 0, "should not be in list as the alert is too old"
 
@@ -143,7 +147,7 @@ class TestAlertCombiner:
                      "hash": "0xabc",
                      "description": "description",
                      "alertId": "IMPOSSIBLE-2",
-                     "createdAt": datetime.now().strftime("%Y-%m-%dT%H:%M:%S%fZ"),  # 2022-11-18T03:01:21.457234676Z
+                     "createdAt": datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f123Z"),  # 2022-11-18T03:01:21.457234676Z
                      "source":
                         {"bot": {'id': "0x0ffe038c802784f739bb27fcd4274f71c384fea78de87c9ef8d5b3fb72b514c7"}}
                      }
@@ -161,17 +165,162 @@ class TestAlertCombiner:
                  "hash": "0xabc",
                  "description": "description",
                  "alertId": "IMPOSSIBLE-2",
-                 "createdAt": datetime.now().strftime("%Y-%m-%dT%H:%M:%S%fZ"),  # 2022-11-18T03:01:21.457234676Z
+                 "createdAt": datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f123Z"),  # 2022-11-18T03:01:21.457234676Z
                  "source":
                     {"bot": {'id': "0x0ffe038c802784f739bb27fcd4274f71c384fea78de87c9ef8d5b3fb72b514c7"}}
                  }
              })
         alert_list = []
-        agent.update_list(alert, ALERT_MAX_QUEUE_SIZE, BASE_BOTS, ALERTS_LOOKBACK_WINDOW_IN_HOURS, alert_list)
+        agent.update_list(alert, ALERTS_DATA_MAX_QUEUE_SIZE, BASE_BOTS, ALERTS_LOOKBACK_WINDOW_IN_HOURS, alert_list)
 
         assert len(alert_list) == 1, "should be in list"
 
-        agent.persist(alert_list, ALERTS_KEY)
-        alert_list_loaded = agent.load(ALERTS_KEY)
+        agent.persist(alert_list, ALERTS_DATA_KEY)
+        alert_list_loaded = agent.load(ALERTS_DATA_KEY)
 
         assert len(alert_list_loaded) == 1, "should be in loaded list"
+
+    def test_persist_and_initialize(self):
+        TestAlertCombiner.remove_persistent_state()
+        alert = create_alert_event(
+            {"alert":
+                {"name": "x",
+                 "hash": "0xabc",
+                 "description": "description",
+                 "alertId": "IMPOSSIBLE-2",
+                 "createdAt": datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f123Z"),  # 2022-11-18T03:01:21.457234676Z
+                 "source":
+                    {"bot": {'id': "0x0ffe038c802784f739bb27fcd4274f71c384fea78de87c9ef8d5b3fb72b514c7"}}
+                 }
+             })
+        alert_list = []
+        agent.update_list(alert, ALERTS_DATA_MAX_QUEUE_SIZE, BASE_BOTS, ALERTS_LOOKBACK_WINDOW_IN_HOURS, alert_list)
+
+        assert len(alert_list) == 1, "should be in list"
+
+        agent.persist(alert_list, ALERTS_DATA_KEY)
+        agent.initialize()
+
+        assert len(alert_list_loaded) == 1, "should be in loaded list"
+
+    def generate_alert(address: str, bot_id: str, alert_id: str):
+        return create_alert_event(
+            {"alert":
+                {"name": "x",
+                 "hash": "0xabc",
+                 "addresses": [address],
+                 "description": f"{address} description",
+                 "alertId": alert_id,
+                 "createdAt": datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f123Z"),  # 2022-11-18T03:01:21.457234676Z
+                 "source":
+                    {"bot": {'id': bot_id}, "block": {"chainId": 1}}
+                 }
+             })
+
+    def test_alert_simple_case(self):
+        # three alerts in diff stages for a given EOA
+        # no FP
+        # anomaly score < 10 E-8
+        TestAlertCombiner.remove_persistent_state()
+        agent.initialize()
+
+        alert_event = TestAlertCombiner.generate_alert(EOA_ADDRESS, "0xa91a31df513afff32b9d85a2c2b7e786fdd681b3cdd8d93d6074943ba31ae400", "FUNDING-TORNADO-CASH")  # funding, TC -> alert count 100; ad-scorer transfer-in -> denominator 100000
+        agent.detect_attack(w3, luabase, alert_event)
+        assert len(agent.FINDINGS_CACHE) == 0, "no alert should have been raised"
+
+        alert_event = TestAlertCombiner.generate_alert(EOA_ADDRESS, "0x0b241032ca430d9c02eaa6a52d217bbff046f0d1b3f3d2aa928e42a97150ec91", "SUSPICIOUS-CONTRACT-CREATION")  # preparation -> alert count = 200, suspicious ML; ad-scorer contract-creation -> denominator 10000
+        agent.detect_attack(w3, luabase, alert_event)
+        assert len(agent.FINDINGS_CACHE) == 0, "no alert should have been raised"
+
+        alert_event = TestAlertCombiner.generate_alert(EOA_ADDRESS, "0xbc06a40c341aa1acc139c900fd1b7e3999d71b80c13a9dd50a369d8f923757f5", "FLASHBOT-TRANSACTION")  # exploitation, flashbot -> alert count = 5000; ad-scorer tx-count -> denominator 10000000
+        agent.detect_attack(w3, luabase, alert_event)
+
+        # 100/100000 * 200/10000 * 5000/10000000 -> 10E-8
+
+        assert len(agent.FINDINGS_CACHE) == 1, "alert should have been raised"
+
+
+    def test_alert_simple_case_contract(self):
+        # three alerts in diff stages for a given contract
+        # no FP
+        # anomaly score < 10 E-8
+        TestAlertCombiner.remove_persistent_state()
+        agent.initialize()
+
+        alert_event = TestAlertCombiner.generate_alert(CONTRACT, "0xa91a31df513afff32b9d85a2c2b7e786fdd681b3cdd8d93d6074943ba31ae400", "FUNDING-TORNADO-CASH")  # funding, TC -> alert count 100; ad-scorer transfer-in -> denominator 100000
+        agent.detect_attack(w3, luabase, alert_event)
+        assert len(agent.FINDINGS_CACHE) == 0, "no alert should have been raised"
+
+        alert_event = TestAlertCombiner.generate_alert(CONTRACT, "0x0b241032ca430d9c02eaa6a52d217bbff046f0d1b3f3d2aa928e42a97150ec91", "SUSPICIOUS-CONTRACT-CREATION")  # preparation -> alert count = 200, suspicious ML; ad-scorer contract-creation -> denominator 10000
+        agent.detect_attack(w3, luabase, alert_event)
+        assert len(agent.FINDINGS_CACHE) == 0, "no alert should have been raised"
+
+        alert_event = TestAlertCombiner.generate_alert(CONTRACT, "0xbc06a40c341aa1acc139c900fd1b7e3999d71b80c13a9dd50a369d8f923757f5", "FLASHBOT-TRANSACTION")  # exploitation, flashbot -> alert count = 5000; ad-scorer tx-count -> denominator 10000000
+        agent.detect_attack(w3, luabase, alert_event)
+
+        #100/100000 * 200/10000 * 5000/10000000 -> 10E-8
+
+        assert len(agent.FINDINGS_CACHE) == 0, "alert should have been raised as this is a contract"
+
+
+    def test_alert_simple_case_older_alerts(self):
+        # three alerts in diff stages for a given older alerts
+        # no FP
+        # anomaly score < 10 E-8
+        TestAlertCombiner.remove_persistent_state()
+        agent.initialize()
+
+        alert_event = TestAlertCombiner.generate_alert(EOA_ADDRESS, "0xa91a31df513afff32b9d85a2c2b7e786fdd681b3cdd8d93d6074943ba31ae400", "FUNDING-TORNADO-CASH")  # funding, TC -> alert count 100; ad-scorer transfer-in -> denominator 100000
+        alert_event.alert.created_at = (datetime.now() - timedelta(hours=ALERTS_LOOKBACK_WINDOW_IN_HOURS + 1)).strftime("%Y-%m-%dT%H:%M:%S.%f123Z")
+        agent.detect_attack(w3, luabase, alert_event)
+        assert len(agent.FINDINGS_CACHE) == 0, "no alert should have been raised"
+
+        alert_event = TestAlertCombiner.generate_alert(EOA_ADDRESS, "0x0b241032ca430d9c02eaa6a52d217bbff046f0d1b3f3d2aa928e42a97150ec91", "SUSPICIOUS-CONTRACT-CREATION")  # preparation -> alert count = 200, suspicious ML; ad-scorer contract-creation -> denominator 10000
+        agent.detect_attack(w3, luabase, alert_event)
+        assert len(agent.FINDINGS_CACHE) == 0, "no alert should have been raised"
+
+        alert_event = TestAlertCombiner.generate_alert(EOA_ADDRESS, "0xbc06a40c341aa1acc139c900fd1b7e3999d71b80c13a9dd50a369d8f923757f5", "FLASHBOT-TRANSACTION")  # exploitation, flashbot -> alert count = 5000; ad-scorer tx-count -> denominator 10000000
+        agent.detect_attack(w3, luabase, alert_event)
+
+        # 100/100000 * 200/10000 * 5000/10000000 -> 10E-8
+        assert len(agent.FINDINGS_CACHE) == 0, "alert should have been raised funding alert is too old"
+
+
+    # def test_alert_proper_handling_of_min(self):
+    #     # three alerts in diff stages for a given EOA
+    #     # within one stage two different alerts that generate a diff anomaly score
+    #     # no FP
+    #     # anomaly score < 10 E-8
+
+    #     alert = TestAlertCombiner.generate_alert(EOA_ADDRESS, "0xa91a31df513afff32b9d85a2c2b7e786fdd681b3cdd8d93d6074943ba31ae400", "FUNDING-TORNADO-CASH") # funding, TC
+
+    #     # asset alert raised
+
+    # def test_alert_too_few_alerts(self):
+    #     # two alerts in diff stages for a given EOA
+    #     # no FP
+    #     # anomaly score < 10 E-8
+
+    #     # asset alert raised
+
+    # def test_alert_FP_mitigation(self):
+    #     # three alerts in diff stages for a given EOA
+    #     # FP mitigation
+    #     # anomaly score < 10 E-8
+
+    #     # asset no alert raised
+
+    # def test_alert_cluster_alert(self):
+    #     # three alerts in diff stages across two EOAs that are clustered
+    #     # no FP
+    #     # anomaly score < 10 E-8
+
+    #     # asset alert raised
+
+    # def test_alert_cluster_fp_mitigation(self):
+    #     # three alerts in diff stages across two EOAs that are clustered
+    #     # FP mitigation
+    #     # anomaly score < 10 E-8
+
+    #     # asset no alert raised
+
--- alert-combiner-py/src/luabase.py
+++ alert-combiner-py/src/luabase.py
@@ -11,7 +11,7 @@ LUABASE_CACHE = {}
 class Luabase:
     def execute_query(self, sql: str) -> pd.DataFrame:
         url = "https://q.luabase.com/run"
-        payload = {"block": 
+        payload = {"block":
                    {"details": {
                     "sql": sql,
                     }
@@ -25,35 +25,62 @@ class Luabase:
         print(data)
         return pd.DataFrame(data["data"])
 
-    def get_denominator(self, ad_scorer: str, start_date: datetime, end_date: datetime):
+    def get_chain_name(chain_id: int) -> str:
+        chain_name = ""
+        if chain_id == 1:
+            chain_name = "ethereum"
+        elif chain_id == 137:
+            chain_name = "polygon"
+        else:
+            raise ValueError(f"Invalid chain_id: {chain_id}")
+        return chain_name
+
+    def get_denominator(self, chain_id: int, ad_scorer: str, start_date: datetime, end_date: datetime):
         #  TODO - age out cache
         global LUABASE_CACHE
 
+        chain_name = Luabase.get_chain_name(chain_id)
+
         sql = ""
+        cache_key = f"{chain_name}-{ad_scorer}-{start_date.strftime('%Y-%m-%dT%H')}"
         if ad_scorer == 'contract-creation':
-            sql = f"SELECT COUNT(DISTINCT hash) FROM ethereum.transactions WHERE CAST(block_timestamp as date)  >= '{start_date.strftime('%Y-%m-%d')}' AND CAST(block_timestamp as date)  <='{end_date.strftime('%Y-%m-%d')}' AND to_address is null"
+            sql = f"SELECT COUNT(DISTINCT hash) FROM {chain_name}.transactions WHERE CAST(block_timestamp as datetime)  >= '{start_date.strftime('%Y-%m-%dT%H:%M:%S')}' AND CAST(block_timestamp as datetime)  <='{end_date.strftime('%Y-%m-%dT%H:%M:%S')}' AND to_address is null"
         elif ad_scorer == 'contract-interactions':
-            sql = f"SELECT COUNT(DISTINCT hash) FROM ethereum.transactions JOIN (SELECT address FROM ethereum.contracts) as contracts ON to_address == contracts.address WHERE CAST(block_timestamp as date)  >= '{start_date.strftime('%Y-%m-%d')}' AND CAST(block_timestamp as date)  <='{end_date.strftime('%Y-%m-%d')}' "
+            sql = f"SELECT COUNT(DISTINCT hash) FROM {chain_name}.transactions JOIN (SELECT address FROM {chain_name}.contracts) as contracts ON to_address == contracts.address WHERE CAST(block_timestamp as datetime)  >= '{start_date.strftime('%Y-%m-%dT%H:%M:%S')}' AND CAST(block_timestamp as datetime)  <='{end_date.strftime('%Y-%m-%dT%H:%M:%S')}' "
         elif ad_scorer == 'tx-count':
-            sql = f"SELECT COUNT(DISTINCT hash) FROM ethereum.transactions WHERE CAST(block_timestamp as date)  >= '{start_date.strftime('%Y-%m-%d')}' AND CAST(block_timestamp as date)  <='{end_date.strftime('%Y-%m-%d')}'"
+            sql = f"SELECT COUNT(DISTINCT hash) FROM {chain_name}.transactions WHERE CAST(block_timestamp as datetime)  >= '{start_date.strftime('%Y-%m-%dT%H:%M:%S')}' AND CAST(block_timestamp as datetime)  <='{end_date.strftime('%Y-%m-%dT%H:%M:%S')}'"
         elif ad_scorer == 'transfer-in':
-            sql = f"SELECT COUNT(DISTINCT hash) FROM (SELECT * FROM ethereum.transactions LEFT OUTER JOIN (SELECT address FROM ethereum.contracts) as contracts ON to_address == contracts.address WHERE CAST(block_timestamp as date)  >= '{start_date.strftime('%Y-%m-%d')}' AND CAST(block_timestamp as date)  <='{end_date.strftime('%Y-%m-%d')}' AND input== '0x' and value>0) WHERE address == ''"
+            sql = f"SELECT COUNT(DISTINCT hash) FROM (SELECT * FROM {chain_name}.transactions LEFT OUTER JOIN (SELECT address FROM {chain_name}.contracts) as contracts ON to_address == contracts.address WHERE CAST(block_timestamp as datetime)  >= '{start_date.strftime('%Y-%m-%dT%H:%M:%S')}' AND CAST(block_timestamp as datetime)  <='{end_date.strftime('%Y-%m-%dT%H:%M:%S')}' AND input== '0x' and value>0) WHERE address == ''"
         elif ad_scorer == 'transfer-out-large-amount':
-            sql = f"SELECT COUNT(DISTINCT hash) FROM (SELECT * FROM ethereum.transactions LEFT OUTER JOIN (SELECT address FROM ethereum.contracts) as contracts ON from_address == contracts.address WHERE CAST(block_timestamp as date)  >= '{start_date.strftime('%Y-%m-%d')}' AND CAST(block_timestamp as date)  <='{end_date.strftime('%Y-%m-%d')}' AND input== '0x' and value>100000000000000000000) WHERE address == ''"
+            sql = f"SELECT COUNT(DISTINCT hash) FROM (SELECT * FROM {chain_name}.transactions LEFT OUTER JOIN (SELECT address FROM {chain_name}.contracts) as contracts ON from_address == contracts.address WHERE CAST(block_timestamp as datetime)  >= '{start_date.strftime('%Y-%m-%dT%H:%M:%S')}' AND CAST(block_timestamp as datetime)  <='{end_date.strftime('%Y-%m-%dT%H:%M:%S')}' AND input== '0x' and value>100000000000000000000) WHERE address == ''"
         elif ad_scorer == 'data-eoa-to':
-            sql = f"SELECT COUNT(DISTINCT hash) FROM (SELECT * FROM ethereum.transactions LEFT OUTER JOIN (SELECT address FROM ethereum.contracts) as contracts ON to_address == contracts.address WHERE CAST(block_timestamp as date)  >= '{start_date.strftime('%Y-%m-%d')}' AND CAST(block_timestamp as date)  <='{end_date.strftime('%Y-%m-%d')}' AND input!= '0x') WHERE address == ''"
+            sql = f"SELECT COUNT(DISTINCT hash) FROM (SELECT * FROM {chain_name}.transactions LEFT OUTER JOIN (SELECT address FROM {chain_name}.contracts) as contracts ON to_address == contracts.address WHERE CAST(block_timestamp as datetime)  >= '{start_date.strftime('%Y-%m-%dT%H:%M:%S')}' AND CAST(block_timestamp as datetime)  <='{end_date.strftime('%Y-%m-%dT%H:%M:%S')}' AND input!= '0x') WHERE address == ''"
         elif ad_scorer == 'erc-approvalAll':
-            sql = f"SELECT COUNT(DISTINCT hash) FROM ethereum.transactions WHERE CAST(block_timestamp as date)  >= '{start_date.strftime('%Y-%m-%d')}' AND CAST(block_timestamp as date)  <='{end_date.strftime('%Y-%m-%d')}' AND CAST(input AS CHAR) like '0xa22cb465%'"
+            sql = f"SELECT COUNT(DISTINCT hash) FROM {chain_name}.transactions WHERE CAST(block_timestamp as datetime)  >= '{start_date.strftime('%Y-%m-%dT%H:%M:%S')}' AND CAST(block_timestamp as datetime)  <='{end_date.strftime('%Y-%m-%dT%H:%M:%S')}' AND CAST(input AS CHAR) like '0xa22cb465%'"
         elif ad_scorer == 'erc-approvals':
-            sql = f"SELECT COUNT(DISTINCT hash) FROM ethereum.transactions WHERE CAST(block_timestamp as date)  >= '{start_date.strftime('%Y-%m-%d')}' AND CAST(block_timestamp as date)  <='{end_date.strftime('%Y-%m-%d')}' AND CAST(input AS CHAR) like '0x095ea7b3%'"
+            sql = f"SELECT COUNT(DISTINCT hash) FROM {chain_name}.transactions WHERE CAST(block_timestamp as datetime)  >= '{start_date.strftime('%Y-%m-%dT%H:%M:%S')}' AND CAST(block_timestamp as datetime)  <='{end_date.strftime('%Y-%m-%dT%H:%M:%S')}' AND CAST(input AS CHAR) like '0x095ea7b3%'"
         elif ad_scorer == 'erc-transfers':
-            sql = f"SELECT COUNT(DISTINCT hash) FROM ethereum.transactions WHERE CAST(block_timestamp as date)  >= '{start_date.strftime('%Y-%m-%d')}' AND CAST(block_timestamp as date)  <='{end_date.strftime('%Y-%m-%d')}' AND CAST(input AS CHAR) like '0xa9059cbb%'"
+            sql = f"SELECT COUNT(DISTINCT hash) FROM {chain_name}.transactions WHERE CAST(block_timestamp as datetime)  >= '{start_date.strftime('%Y-%m-%dT%H:%M:%S')}' AND CAST(block_timestamp as datetime)  <='{end_date.strftime('%Y-%m-%dT%H:%M:%S')}' AND CAST(input AS CHAR) like '0xa9059cbb%'"
         else:
             raise ValueError(f"Invalid ad scorer: {ad_scorer}")
 
-        if sql in LUABASE_CACHE:
-            return LUABASE_CACHE[sql]
+        if cache_key in LUABASE_CACHE:
+            return LUABASE_CACHE[cache_key]
         else:
             value = Luabase().execute_query(sql)
-            LUABASE_CACHE[sql] = value.iloc[0]['uniqExact(hash)']
+            LUABASE_CACHE[cache_key] = value.iloc[0]['uniqExact(hash)']
             return value.iloc[0]['uniqExact(hash)']
+
+    def get_alert_count(self, chain_id: int, bot_id: str, alert_id: str, start_date: datetime, end_date: datetime) -> int:
+        chain_name = Luabase.get_chain_name(chain_id)
+
+        sql = f"select COUNT() from forta.{chain_name}_alerts WHERE CAST(substring(block_timestamp,1,19) as datetime)  >= '{start_date.strftime('%Y-%m-%dT%H:%M:%S')}' AND CAST(substring(block_timestamp,1,19)  as datetime)  <= '{end_date.strftime('%Y-%m-%dT%H:%M:%S')}' AND bot_id = '{bot_id}' AND alert_id = '{alert_id}'"
+        cache_key = f"{chain_name}-{bot_id}-{alert_id}-{start_date.strftime('%Y-%m-%dT%H')}"
+
+        if cache_key in LUABASE_CACHE:
+            return LUABASE_CACHE[cache_key]
+        else:
+            value = Luabase().execute_query(sql)
+            LUABASE_CACHE[cache_key] = value.iloc[0]['count()']
+            return value.iloc[0]['count()']
+
