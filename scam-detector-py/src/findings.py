48e799946e86be21b1b7d079b14e05badf718d9c
christian-forta <christian@forta.org>
2022-12-18 11:03:37 -0800

    remove label as work around for serial issue

--- scam-detector-py/src/findings.py
+++ scam-detector-py/src/findings.py
@@ -20,14 +20,14 @@ class AlertCombinerFinding:
         meta_data = {**attacker_address_md, **start_date, **end_date, **involved_addresses, **involved_alert_ids, **involved_alert_hashes}
 
         labels = []
-        if alert_id == "ATTACK-DETECTOR-ICE-PHISHING":
-            labels = [Label({
-    		    'entity_type': EntityType.Address,
-    		    'label_type': LabelType.Scam,
-    		    'entity': attacker_address,
-                'confidence': 0.99,
-                'custom_value': ""
-    	    })]
+        # if alert_id == "ATTACK-DETECTOR-ICE-PHISHING":
+        #     labels = [Label({
+    	# 	    'entity_type': EntityType.Address,
+    	# 	    'label_type': LabelType.Scam,
+    	# 	    'entity': attacker_address,
+        #         'confidence': 0.99,
+        #         'custom_value': ""
+    	#     })]
 
         return Finding({
             'name': 'Attack detector identified an EOA with past alerts mapping to attack behavior',
