39a08e7302502fbd6df1a58012cb536a17697bc9
Mariko Wakabayashi <mariko@openzeppelin.com>
2023-01-23 11:50:19 -0600

    Remove luabase from malicious account funding

--- malicious-account-funding-py/src/agent_test.py
+++ malicious-account-funding-py/src/agent_test.py
@@ -7,53 +7,44 @@ KNOWN_MALICIOUS_ACCOUNT = "0x000000000532b45f47779fce440748893b257865"
 
 w3 = Web3Mock()
 
+
 class TestKnownMaliciousAccountFunding:
-
     def test_funding(self):
-        agent.initialize()
-        agent.update_known_malicious_accounts(1)
+        tx_event = create_transaction_event(
+            {
+                "transaction": {
+                    "hash": "0",
+                    "from": KNOWN_MALICIOUS_ACCOUNT,
+                    "value": 10,
+                    "to": EOA_ADDRESS,
+                },
+                "block": {"number": 0},
+                "logs": [{}],
+                "receipt": {"logs": []},
+            }
+        )
 
-        tx_event = create_transaction_event({
-            'transaction': {
-                'hash': "0",
-                'from': KNOWN_MALICIOUS_ACCOUNT,
-                'value': 10,
-                'to': EOA_ADDRESS,
-            },
-            'block': {
-                'number': 0
-            },
-            'logs': [
-                {}
-            ],
-            'receipt': {
-                'logs': []}
-        })
-        
         findings = agent.detect_funding(w3, tx_event)
         assert len(findings) == 1, "this should have triggered a finding"
         assert findings[0].alert_id == "MALICIOUS-ACCOUNT-FUNDING"
         assert findings[0].severity == FindingSeverity.High
 
     def test_funding_novalue(self):
-        agent.initialize()
+        tx_event = create_transaction_event(
+            {
+                "transaction": {
+                    "hash": "0",
+                    "from": KNOWN_MALICIOUS_ACCOUNT,
+                    "value": 0,
+                    "to": EOA_ADDRESS,
+                },
+                "block": {"number": 0},
+                "logs": [{}],
+                "receipt": {"logs": []},
+            }
+        )
 
-        tx_event = create_transaction_event({
-            'transaction': {
-                'hash': "0",
-                'from': KNOWN_MALICIOUS_ACCOUNT,
-                'value': 0,
-                'to': EOA_ADDRESS,
-            },
-            'block': {
-                'number': 0
-            },
-            'logs': [
-                {}
-            ],
-            'receipt': {
-                'logs': []}
-        })
-        
         findings = agent.detect_funding(w3, tx_event)
-        assert len(findings) == 0, "this should have not triggered a finding as no funds were transferred"
+        assert (
+            len(findings) == 0
+        ), "this should have not triggered a finding as no funds were transferred"
