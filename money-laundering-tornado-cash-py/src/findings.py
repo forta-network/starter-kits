9e0b75797020e8df9a0d1d5d4e59fff429b384f9
christian-forta <christian@forta.org>
2023-01-26 13:39:26 -0800

    added label/ anomaly score to TC ML bot

--- money-laundering-tornado-cash-py/src/findings.py
+++ money-laundering-tornado-cash-py/src/findings.py
@@ -1,10 +1,15 @@
-from forta_agent import Finding, FindingType, FindingSeverity
+from forta_agent import Finding, FindingType, FindingSeverity, EntityType
 
 
 class MoneyLaunderingTornadoCashFindings:
 
     @staticmethod
-    def possible_money_laundering_tornado_cash(from_address: str, funds_transferred: int) -> Finding:
+    def possible_money_laundering_tornado_cash(from_address: str, funds_transferred: int, anomaly_score: float) -> Finding:
+        labels = [{"entity": from_address,
+                   "entity_type": EntityType.Address,
+                   "label": "attacker",
+                   "confidence": 0.5}]
+        
         return Finding({
             'name': 'Possible Money Laundering With Tornado Cash',
             'description': f'{from_address} potentially engaged in money laundering',
@@ -12,6 +17,8 @@ class MoneyLaunderingTornadoCashFindings:
             'type': FindingType.Suspicious,
             'severity': FindingSeverity.High,
             'metadata': {
+                "anomaly_score": anomaly_score,
                 "total_funds_transferred": str(funds_transferred)
-            }
+            },
+            "labels": labels
         })
