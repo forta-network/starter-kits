91b1caf374375842b5cab25618cf5b46e44617a4
Mariko Wakabayashi <mariko@openzeppelin.com>
2023-01-27 13:35:43 -0600

    Update malicious contract v3 bot

--- malicious-smart-contract-ml-v3-py/src/constants.py
+++ malicious-smart-contract-ml-v3-py/src/constants.py
@@ -26,19 +26,3 @@ ERC777_SIGHASHES = {"0x959b8c3f", "0xfad8b32a"}
 # 0x4f1ef286 - upgradeToAndCall(address,bytes)
 # 0x8f283970 - changeAdmin(address)
 PROXY_SIGHASHES = {"0x5c60da1b", "0x3659cfe6", "0x4f1ef286", "0x8f283970"}
-
-CHAIN_ID_METADATA_MAPPING = {
-    1: (
-        "ethereum",
-        172,  # alert_count
-        32_713,  # contract deployment
-    ),  # alert_count = avg of last 24 hrs of Nov 14, 2022.
-    137: ("polygon", 194, 6_681),
-    56: ("binance", 182, 299_458),
-    43114: ("avalanche", 23, 432),
-    42161: ("arbitrum", 302, 9_800),
-    10: ("optimism", 15, 1_651),
-    250: ("fantom", 7, 345_486),
-}
-
-LUABASE_SUPPORTED_CHAINS = {1, 137, 43114, 250}
