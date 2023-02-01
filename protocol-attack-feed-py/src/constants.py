46933a25b51e0d7d59206ac9606f04d49f2c9a84
christian-forta <christian@forta.org>
2023-02-01 10:42:05 -0800

    added initial version of protocol attack feed

---
+++ protocol-attack-feed-py/src/constants.py
@@ -0,0 +1,5 @@
+BASE_BOTS = [("0x80ed808b586aeebe9cdd4088ea4dea0a8e322909c0e4493c993e060e89c09ed1", "ATTACK-DETECTOR-2", "description"),  # 0x5711caa8fdcd832ce1be554f2229345181a646ac likely involved in an attack (ATTACK-DETECTOR-2).
+             ("0x80ed808b586aeebe9cdd4088ea4dea0a8e322909c0e4493c993e060e89c09ed1", "ATTACK-DETECTOR-1", "description"),  # 0xe3174149f80d1ea429970ec5043e361bc003ddbd likely involved in an attack (ATTACK-DETECTOR-1 on 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2 (wrapped ether))
+             ]
+
+CACHE_VERSION = "V1"
