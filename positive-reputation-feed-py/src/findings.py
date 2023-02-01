0cfbc29d72731a90dd84b4599cce249e5a96e2a9
christian-forta <christian@forta.org>
2023-01-31 16:46:19 -0800

    initial version of pos rep feed bot

---
+++ positive-reputation-feed-py/src/findings.py
@@ -0,0 +1,27 @@
+# Copyright 2022 The Forta Foundation
+
+from forta_agent import Finding, FindingType, FindingSeverity, EntityType
+
+
+class PositiveReputationFinding:
+
+    @staticmethod
+    def create_finding(address: str, base_bots: list) -> Finding:
+        base_bots_metadata = {"base_bots_" + str(i): address for i, address in enumerate(base_bots, 1)}
+
+        confidence = 0.5 if len(base_bots) > 1 else 0.3
+
+        labels = [{"entity": address,
+                   "entity_type": EntityType.Address,
+                   "label": "attacker",
+                   "confidence": confidence}]  # very low
+
+        return Finding({
+                       'name': 'Attack detector identified an EOA with behavior consistent with an attack',
+                       'description': f'EOA {address} was assigned positive reputation.',
+                       'alert_id': 'POSITIVE-REPUTATION-1',
+                       'type': FindingType.Info,
+                       'severity': FindingSeverity.Info,
+                       'metadata': base_bots_metadata,
+                       'labels': labels
+                       })
