56bb1f4d66bc71bf38c197dbbc3327092e55e37f
Mariko Wakabayashi <mariko@openzeppelin.com>
2023-03-02 16:39:24 -0600

    Use bot alert rate pkg for cex funding

--- cex-funding-py/src/findings.py
+++ cex-funding-py/src/findings.py
@@ -1,22 +1,45 @@
+from bot_alert_rate import calculate_alert_rate, ScanCountType
 from forta_agent import Finding, FindingSeverity, FindingType, EntityType
+
+from src.keys import BOT_ID
 
 
 class CEXFundingFinding:
+    def __init__(self, name: str, to: str, value: int, chain_id: str):
+        self.alert_id = "CEX-FUNDING-1"
+        self.name = name
+        self.to = to
+        self.value = value
+        self.chain_id = chain_id
 
-    @staticmethod
-    def cex_funding(name: str, to: str, value: int, anomaly_score: float) -> Finding:
+    def emit_finding(self) -> Finding:
+        labels = [
+            {
+                "entity": self.to,
+                "entity_type": EntityType.Address,
+                "label": "attacker",
+                "confidence": 0.1,
+            }
+        ]  # very low
 
-        labels = [{"entity": to,
-                   "entity_type": EntityType.Address,
-                   "label": "attacker",
-                   "confidence": 0.1}]  # very low
-
-        return Finding({
-                    "name": "CEX Funding",
-                    "description": f"CEX Funding from {name} of {value} wei to {to}",
-                    "alert_id": "CEX-FUNDING-1",
-                    "type": FindingType.Suspicious,
-                    "severity": FindingSeverity.Low,
-                    "metadata": {"anomaly_score": anomaly_score, "CEX_name": name, "to": to, "value": value},
-                    'labels': labels
-                })
+        return Finding(
+            {
+                "name": "CEX Funding",
+                "description": f"CEX Funding from {self.name} of {self.value} wei to {self.to}",
+                "alert_id": self.alert_id,
+                "type": FindingType.Suspicious,
+                "severity": FindingSeverity.Low,
+                "metadata": {
+                    "anomaly_score": calculate_alert_rate(
+                        self.chain_id,
+                        BOT_ID,
+                        self.alert_id,
+                        ScanCountType.TRANSFER_COUNT,
+                    ),
+                    "CEX_name": self.name,
+                    "to": self.to,
+                    "value": self.value,
+                },
+                "labels": labels,
+            }
+        )
