df53cdcae01ec14520610640c31274ee4a9513a6
christian-forta <christian@forta.org>
2023-03-14 08:26:57 -0700

    limit finding size array to address out of mem

--- alert-combiner-py/src/findings.py
+++ alert-combiner-py/src/findings.py
@@ -8,7 +8,7 @@ class AlertCombinerFinding:
 
     @staticmethod
     def alert_combiner(attacker_address: str, victim_address: str, victim_name: str, start_date: datetime, end_date: datetime, involved_addresses: set, involved_alerts: set, alert_id: str, hashes: set, victim_metadata: dict) -> Finding:
-        involved_addresses = list(involved_addresses)[0:500]
+        involved_addresses = list(involved_addresses)[0:250]
         hashes = list(hashes)[0:10]
 
         attacker_address_md = {"attacker_address": attacker_address}
