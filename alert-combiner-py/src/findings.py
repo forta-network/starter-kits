5c57255c7d8f3e2edaa515b65eea8f82bf6cc680
christian-forta <christian@forta.org>
2023-03-20 14:56:12 -0700

    added labels; modified doc accordingly

--- alert-combiner-py/src/findings.py
+++ alert-combiner-py/src/findings.py
@@ -1,6 +1,6 @@
 # Copyright 2022 The Forta Foundation
 
-from forta_agent import Finding, FindingType, FindingSeverity
+from forta_agent import Finding, FindingType, FindingSeverity, Label, EntityType
 import pandas as pd
 import forta_agent
 
@@ -8,14 +8,14 @@ import forta_agent
 class AlertCombinerFinding:
 
     @staticmethod
-    def create_finding(address: str, victim_address: str, victim_name, anomaly_score: float, severity: FindingSeverity, alert_id: str, 
-        alert_event: forta_agent.alert_event.AlertEvent, alert_data: pd.DataFrame, victim_metadata: dict, anomaly_scores_by_stage: pd.DataFrame) -> Finding:
+    def create_finding(addresses: str, victim_address: str, victim_name, anomaly_score: float, severity: FindingSeverity, alert_id: str, 
+        alert_event: forta_agent.alert_event.AlertEvent, alert_data: pd.DataFrame, victim_metadata: dict, anomaly_scores_by_stage: pd.DataFrame, chain_id: int) -> Finding:
         # alert_data -> 'stage', 'created_at', 'anomaly_score', 'alert_hash', 'bot_id', 'alert_id', 'addresses'
 
         anomaly_scores = {}
         for index, row in anomaly_scores_by_stage.iterrows():
             anomaly_scores[f'anomaly_score_stage_{row["stage"]}'] = row["anomaly_score"]
-        attacker_address = {"attacker_address": address}
+        attacker_address = {"attacker_address": addresses}
         anomaly_score = {"anomaly_score": anomaly_score}
         involved_addresses = set()
         alert_data["addresses"].apply(lambda x: [involved_addresses.add(item) for item in x])
@@ -30,11 +30,26 @@ class AlertCombinerFinding:
 
         victim_clause = f" on {victim_name} ({victim_address.lower()})" if victim_address else ""
 
+        labels = []
+        for address in addresses:
+            labels.append(Label({
+                'entityType': EntityType.Address,
+                'label': "attacker",
+                'entity': address,
+                'confidence': 0.99,
+                'remove': "false",
+                'metadata': {
+                    'alert_id': alert_id,
+                    'chain_id': chain_id
+                }
+            }))
+
         return Finding({
                        'name': 'Attack detector identified an EOA with behavior consistent with an attack',
-                       'description': f'{address} likely involved in an attack ({alert_event.alert_hash}){victim_clause}. Anomaly score: {anomaly_score}',
+                       'description': f'{addresses} likely involved in an attack ({alert_event.alert_hash}){victim_clause}. Anomaly score: {anomaly_score}',
                        'alert_id': alert_id,
                        'type': FindingType.Exploit,
                        'severity': severity,
-                       'metadata': meta_data
+                       'metadata': meta_data,
+                       'labels': labels
                        })
