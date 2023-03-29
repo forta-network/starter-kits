2651498538d3ea974b56691284cd22ea2a979aae
christian-forta <christian@forta.org>
2023-03-29 11:28:07 -0700

    added label and updated SDK

--- positive-reputation-py/src/findings.py
+++ positive-reputation-py/src/findings.py
@@ -1,15 +1,29 @@
-from forta_agent import Finding, FindingType, FindingSeverity
+from forta_agent import Finding, FindingType, FindingSeverity, Label, EntityType
 
 
 class PositiveReputationFindings:
 
     @staticmethod
-    def positive_reputation(address: str) -> Finding:
+    def positive_reputation(address: str, chain_id: int) -> Finding:
+        labels = []
+        labels.append(Label({
+            'entityType': EntityType.Address,
+            'label': "benign",
+            'entity': address,
+            'confidence': 0.80,
+            'remove': "false",
+            'metadata': {
+                'alert_id': 'POSITIVE-REPUTATION-1',
+                'chain_id': chain_id
+            }
+        }))
+
         finding = Finding({
             'name': 'Positive Reputation',
             'description': f'{address} has positive reputation',
             'alert_id': 'POSITIVE-REPUTATION-1',
             'type': FindingType.Info,
             'severity': FindingSeverity.Info,
+            'labels': labels
         })
         return finding
