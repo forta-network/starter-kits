bddb418dfd5c74c9e86836d06224594f060e1db6
Tayfun Sur <tayfunsur@gmail.com>
2023-04-20 19:41:52 +0300

    Use alert rate pkg for victim notification bot

--- victim-notification-py/src/findings.py
+++ victim-notification-py/src/findings.py
@@ -1,10 +1,13 @@
 from forta_agent import Finding, FindingType, FindingSeverity, Label, EntityType
+from bot_alert_rate import calculate_alert_rate, ScanCountType
+
+from src.keys import BOT_ID
 
 
 class VictimNotificationFinding:
 
     @staticmethod
-    def Victim(victim_address: str, notifier_address: str, chain_id:int) -> Finding:
+    def Victim(victim_address: str, notifier_address: str, chain_id: int) -> Finding:
         labels = []
         labels.append(Label({
             'entityType': EntityType.Address,
@@ -27,12 +30,20 @@ class VictimNotificationFinding:
             }
         }))
 
+        metadata = {"anomaly_score": calculate_alert_rate(
+            chain_id,
+            BOT_ID,
+            'VICTIM-NOTIFICATION-1',
+            ScanCountType.TRANSFER_COUNT,
+        )}
+
         finding = Finding({
             'name': 'Victim Notified',
             'description': f'{victim_address} was notified to be a victim by {notifier_address}',
             'alert_id': 'VICTIM-NOTIFICATION-1',
             'type': FindingType.Info,
             'severity': FindingSeverity.Info,
-            'labels': labels
+            'labels': labels,
+            'metadata': metadata
         })
         return finding
