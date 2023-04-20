2c5c09b06140ebfb5368df2ddb46232cb6a45e9e
christian-forta <christian@forta.org>
2023-04-20 15:01:50 -0700

    fixed date timezone issues

--- bot-monitor-py/src/models_test.py
+++ bot-monitor-py/src/models_test.py
@@ -1,18 +1,18 @@
 from models import AlertRateModel
 import pandas as pd
 from prophet import Prophet
-from datetime import datetime
+from datetime import datetime, timezone
 
 class TestUtils:
     def test_alert_rate_model_in_range(self):
         # Initialize AlertRateModel
         alert_rate_model = AlertRateModel()
-        start_time = datetime(2020, 12, 31, 23, 0)
+        start_time = datetime(2020, 12, 31, 23, 0, tzinfo=timezone.utc)
         # Create sample timestamps
-        timestamp_1 = datetime(2021, 1, 1, 0, 0)
-        timestamp_2 = datetime(2021, 1, 1, 1, 0)
-        timestamp_3 = datetime(2021, 1, 1, 2, 0)
-        timestamp_4 = datetime(2021, 1, 1, 3, 0)
+        timestamp_1 = datetime(2021, 1, 1, 0, 0, tzinfo=timezone.utc)
+        timestamp_2 = datetime(2021, 1, 1, 1, 0, tzinfo=timezone.utc)
+        timestamp_3 = datetime(2021, 1, 1, 2, 0, tzinfo=timezone.utc)
+        timestamp_4 = datetime(2021, 1, 1, 3, 0, tzinfo=timezone.utc)
 
         # Update the model with the sample timestamps
         alert_rate_model.update(timestamp_1)
@@ -31,13 +31,13 @@ class TestUtils:
     def test_alert_rate_model_missing_value(self):
         # Initialize AlertRateModel
         alert_rate_model = AlertRateModel()
-        start_time = datetime(2020, 12, 31, 23, 0)
+        start_time = datetime(2020, 12, 31, 23, 0, tzinfo=timezone.utc)
         alert_rate_model.update(start_time)
 
         # Create sample timestamps
-        timestamp_1 = datetime(2021, 1, 1, 0, 0)
-        timestamp_3 = datetime(2021, 1, 1, 2, 0)
-        timestamp_4 = datetime(2021, 1, 1, 3, 0)
+        timestamp_1 = datetime(2021, 1, 1, 0, 0, tzinfo=timezone.utc)
+        timestamp_3 = datetime(2021, 1, 1, 2, 0, tzinfo=timezone.utc)
+        timestamp_4 = datetime(2021, 1, 1, 3, 0, tzinfo=timezone.utc)
 
         # Update the model with the sample timestamps
         alert_rate_model.update(timestamp_1)
@@ -53,13 +53,13 @@ class TestUtils:
     def test_get_time_series_date(self):
         # Initialize AlertRateModel
         alert_rate_model = AlertRateModel()
-        start_time = datetime(2020, 12, 31, 23, 0)
+        start_time = datetime(2020, 12, 31, 23, 0, tzinfo=timezone.utc)
         alert_rate_model.update(start_time)
 
         # Create sample timestamps
-        timestamp_1 = datetime(2021, 1, 1, 0, 0)
-        timestamp_3 = datetime(2021, 1, 1, 2, 0)
-        timestamp_4 = datetime(2021, 1, 1, 3, 0)
+        timestamp_1 = datetime(2021, 1, 1, 0, 0, tzinfo=timezone.utc)
+        timestamp_3 = datetime(2021, 1, 1, 2, 0, tzinfo=timezone.utc)
+        timestamp_4 = datetime(2021, 1, 1, 3, 0, tzinfo=timezone.utc)
 
         # Update the model with the sample timestamps
         alert_rate_model.update(timestamp_1)
