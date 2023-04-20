819093c3ade6342eea1cd00b8a8d2c85b9ce3a37
Tayfun Sur <tayfunsur@gmail.com>
2023-04-20 19:56:24 +0300

    Use alert rate pkg for social eng contract bot

--- social-eng-contract-py/src/findings.py
+++ social-eng-contract-py/src/findings.py
@@ -1,10 +1,13 @@
 from forta_agent import Finding, FindingSeverity, FindingType, EntityType
+from bot_alert_rate import calculate_alert_rate, ScanCountType
+
+from src.keys import BOT_ID
 
 
 class SocialEngContractFindings:
 
     @staticmethod
-    def social_eng_contract_creation(from_address: str, contract_address: str, impersonated_contract: str, anomaly_score: float) -> Finding:
+    def social_eng_contract_creation(from_address: str, contract_address: str, impersonated_contract: str, chain_id: int) -> Finding:
         labels = [{"entity": from_address,
                    "entityType": EntityType.Address,
                    "label": "attacker",
@@ -24,7 +27,11 @@ class SocialEngContractFindings:
             'alert_id': 'SOCIAL-ENG-CONTRACT-CREATION',
             'type': FindingType.Exploit,
             'severity': FindingSeverity.High,
-            'metadata': {"anomaly_score": anomaly_score, "impersonated_contract": impersonated_contract},
+            'metadata': {"anomaly_score": calculate_alert_rate(
+                chain_id,
+                BOT_ID,
+                "SOCIAL-ENG-CONTRACT-CREATION",
+                ScanCountType.TRANSFER_COUNT,
+            ), "impersonated_contract": impersonated_contract},
             'labels': labels
         })
-
