cb86d503969f5d5f954fb77a930c0260f267d2b5
Tayfun Sur <tayfunsur@gmail.com>
2023-04-20 19:39:47 +0300

    Use alert rate pkg for sanctioned addresses bot

--- sanctioned-addresses-py/src/agent_test.py
+++ sanctioned-addresses-py/src/agent_test.py
@@ -1,14 +1,18 @@
 from unittest.mock import Mock
 from forta_agent import create_transaction_event
 from .agent import provide_handle_transaction
+import src.agent
 
 mock_check_chainalysis_oracle = Mock()
 
 handle_transaction = provide_handle_transaction(mock_check_chainalysis_oracle)
 mock_tx_event = create_transaction_event({})
 
+
 class TestSanctionedAddressTxBot:
     def test_returns_findings(self):
+        src.agent.initialize()
+
         mock_finding = {'some': 'finding'}
         mock_check_chainalysis_oracle.return_value = [mock_finding]
 
