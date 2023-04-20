8b26b47f739576ec744faad2b54785f79fed2772
Tayfun Sur <tayfunsur@gmail.com>
2023-04-20 19:41:33 +0300

    Use alert rate pkg for unverified contract bot

--- unverified-contract-py/src/agent_test.py
+++ unverified-contract-py/src/agent_test.py
@@ -6,6 +6,7 @@ import agent
 from blockexplorer_mock import BlockExplorerMock
 from web3_mock import (CONTRACT_NO_ADDRESS, CONTRACT_WITH_ADDRESS, EOA_ADDRESS,
                        Web3Mock)
+from unittest.mock import patch
 
 w3 = Web3Mock()
 blockexplorer = BlockExplorerMock(1)
@@ -36,9 +37,10 @@ class TestUnverifiedContractAgent:
         contract_address = agent.calc_contract_address(w3, EOA_ADDRESS, 9)
         assert contract_address == "0x728ad672409DA288cA5B9AA85D1A55b803bA97D7", "should be the same contract address"
 
-    def test_detect_unverified_contract_with_unverified_contract_no_trace(self):
+    @patch("src.findings.calculate_alert_rate", return_value=1.0)
+    def test_detect_unverified_contract_with_unverified_contract_no_trace(self, mocker):
         agent.initialize()
-        
+
         tx_event = create_transaction_event(
             {
                 "transaction": {
@@ -55,22 +57,30 @@ class TestUnverifiedContractAgent:
         )
         agent.cache_contract_creation(w3, blockexplorer, tx_event)
         time.sleep(1 * 60 + 10)
-        agent.detect_unverified_contract_creation(w3, blockexplorer, wait_time=1, infinite=False)
+        agent.detect_unverified_contract_creation(
+            w3, blockexplorer, wait_time=1, infinite=False)
         assert len(agent.FINDINGS_CACHE) == 1, "should have 1 finding"
         assert agent.FINDINGS_CACHE[0].metadata["anomaly_score"] == 1.0, "should have anomaly score of 1.0"
-        assert agent.FINDINGS_CACHE[0].labels[0].toDict()["entity"] == EOA_ADDRESS, "should have EOA address as label"
-        assert agent.FINDINGS_CACHE[0].labels[0].toDict()["entity_type"] == EntityType.Address, "should have label_type address"
-        assert agent.FINDINGS_CACHE[0].labels[0].toDict()["label"] == 'attacker', "should have attacker as label"
-        assert agent.FINDINGS_CACHE[0].labels[0].toDict()["confidence"] == 0.3, "should have 0.3 as label confidence"
-        assert agent.FINDINGS_CACHE[0].labels[1].toDict()["entity"] == '0x728ad672409DA288cA5B9AA85D1A55b803bA97D7', "should have contract address as label"
-        assert agent.FINDINGS_CACHE[0].labels[1].toDict()["label"] == 'attacker_contract', "should have attacker as label"
-        assert agent.FINDINGS_CACHE[0].labels[1].toDict()["confidence"] == 0.3, "should have 0.3 as label confidence"
-        assert agent.FINDINGS_CACHE[0].labels[1].toDict()["entity_type"] == EntityType.Address, "should have label_type address"
-        
-        
+        assert agent.FINDINGS_CACHE[0].labels[0].toDict(
+        )["entity"] == EOA_ADDRESS, "should have EOA address as label"
+        assert agent.FINDINGS_CACHE[0].labels[0].toDict(
+        )["entity_type"] == EntityType.Address, "should have label_type address"
+        assert agent.FINDINGS_CACHE[0].labels[0].toDict(
+        )["label"] == 'attacker', "should have attacker as label"
+        assert agent.FINDINGS_CACHE[0].labels[0].toDict(
+        )["confidence"] == 0.3, "should have 0.3 as label confidence"
+        assert agent.FINDINGS_CACHE[0].labels[1].toDict(
+        )["entity"] == '0x728ad672409DA288cA5B9AA85D1A55b803bA97D7', "should have contract address as label"
+        assert agent.FINDINGS_CACHE[0].labels[1].toDict(
+        )["label"] == 'attacker_contract', "should have attacker as label"
+        assert agent.FINDINGS_CACHE[0].labels[1].toDict(
+        )["confidence"] == 0.3, "should have 0.3 as label confidence"
+        assert agent.FINDINGS_CACHE[0].labels[1].toDict(
+        )["entity_type"] == EntityType.Address, "should have label_type address"
+
     def test_detect_unverified_contract_with_unverified_contract_trace(self):
         agent.initialize()
-        
+
         tx_event = create_transaction_event({
 
             'transaction': {
@@ -97,9 +107,11 @@ class TestUnverifiedContractAgent:
 
         agent.cache_contract_creation(w3, blockexplorer, tx_event)
         time.sleep(1 * 60 + 10)
-        agent.detect_unverified_contract_creation(w3, blockexplorer, wait_time=1, infinite=False)
+        agent.detect_unverified_contract_creation(
+            w3, blockexplorer, wait_time=1, infinite=False)
         assert len(agent.FINDINGS_CACHE) == 1, "should have 1 finding"
-        finding = next((x for x in agent.FINDINGS_CACHE if x.alert_id == 'UNVERIFIED-CODE-CONTRACT-CREATION'), None)
+        finding = next((x for x in agent.FINDINGS_CACHE if x.alert_id ==
+                       'UNVERIFIED-CODE-CONTRACT-CREATION'), None)
         assert finding.severity == FindingSeverity.Medium
         assert finding.type == FindingType.Suspicious
         assert finding.description == f'{EOA_ADDRESS} created contract 0x728ad672409DA288cA5B9AA85D1A55b803bA97D7'
@@ -107,7 +119,7 @@ class TestUnverifiedContractAgent:
 
     def test_detect_unverified_contract_with_verified_contract(self):
         agent.initialize()
-        
+
         tx_event = create_transaction_event({
             'transaction': {
                 'hash': "0",
@@ -131,18 +143,19 @@ class TestUnverifiedContractAgent:
 
         agent.cache_contract_creation(w3, blockexplorer, tx_event)
         time.sleep(1 * 60 + 10)
-        agent.detect_unverified_contract_creation(w3, blockexplorer, wait_time=1, infinite=False)
+        agent.detect_unverified_contract_creation(
+            w3, blockexplorer, wait_time=1, infinite=False)
 
         assert len(agent.FINDINGS_CACHE) == 0, "should have 0 finding"
 
     def test_detect_unverified_contract_call_only(self):
         agent.initialize()
-        
+
         tx_event = create_transaction_event({
             'transaction': {
                 'hash': "0",
                 'from': EOA_ADDRESS,
-                'nonce': 7,  
+                'nonce': 7,
                 'to': '0x0000000000000000000000000000000000000000'
             },
             'block': {
@@ -163,5 +176,6 @@ class TestUnverifiedContractAgent:
 
         agent.cache_contract_creation(w3, blockexplorer, tx_event)
         time.sleep(1 * 60 + 10)
-        agent.detect_unverified_contract_creation(w3, blockexplorer, wait_time=1, infinite=False)
+        agent.detect_unverified_contract_creation(
+            w3, blockexplorer, wait_time=1, infinite=False)
         assert len(agent.FINDINGS_CACHE) == 0, "should have 0 finding"
