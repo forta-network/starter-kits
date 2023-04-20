d659026aa851a0267d4318c7e07453520f60582c
Tayfun Sur <tayfunsur@gmail.com>
2023-04-20 19:38:30 +0300

    Use alert rate pkg for money laundring tc

--- money-laundering-tornado-cash-py/src/findings.py
+++ money-laundering-tornado-cash-py/src/findings.py
@@ -1,15 +1,18 @@
 from forta_agent import Finding, FindingType, FindingSeverity, EntityType
+from bot_alert_rate import calculate_alert_rate, ScanCountType
+
+from src.keys import BOT_ID
 
 
 class MoneyLaunderingTornadoCashFindings:
 
     @staticmethod
-    def possible_money_laundering_tornado_cash(from_address: str, funds_transferred: int, anomaly_score: float) -> Finding:
+    def possible_money_laundering_tornado_cash(from_address: str, funds_transferred: int, chain_id: int) -> Finding:
         labels = [{"entity": from_address,
                    "entity_type": EntityType.Address,
                    "label": "attacker",
                    "confidence": 0.5}]
-        
+
         return Finding({
             'name': 'Possible Money Laundering With Tornado Cash',
             'description': f'{from_address} potentially engaged in money laundering',
@@ -17,7 +20,12 @@ class MoneyLaunderingTornadoCashFindings:
             'type': FindingType.Suspicious,
             'severity': FindingSeverity.High,
             'metadata': {
-                "anomaly_score": anomaly_score,
+                "anomaly_score": calculate_alert_rate(
+                        chain_id,
+                        BOT_ID,
+                        'POSSIBLE-MONEY-LAUNDERING-TORNADO-CASH',
+                        ScanCountType.TRANSFER_COUNT,
+                ),
                 "total_funds_transferred": str(funds_transferred)
             },
             "labels": labels
