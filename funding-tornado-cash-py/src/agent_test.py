4c5a19e022a394f44f718275667c57b4d8f1e805
Tayfun Sur <tayfunsur@gmail.com>
2023-04-20 19:35:01 +0300

    Use alert rate pkg for funding tornado cash

--- funding-tornado-cash-py/src/agent_test.py
+++ funding-tornado-cash-py/src/agent_test.py
@@ -1,9 +1,10 @@
 from forta_agent import FindingSeverity, create_transaction_event, EntityType
+from unittest.mock import patch
 
 import agent
 from forta_agent import Label
 from constants import TORNADO_CASH_ADDRESSES, TORNADO_CASH_WITHDRAW_TOPIC, TORNADO_CASH_ADDRESSES_HIGH
-from web3_mock import EOA_ADDRESS_TC,EOA_ADDRESS_NEW, EOA_ADDRESS_OLD, Web3Mock
+from web3_mock import EOA_ADDRESS_TC, EOA_ADDRESS_NEW, EOA_ADDRESS_OLD, Web3Mock
 
 w3 = Web3Mock()
 
@@ -32,7 +33,7 @@ class TestTornadoCashFunding:
             'receipt': {
                 'logs': []}
         })
-        
+
         findings = agent.detect_funding(w3, tx_event)
         assert len(findings) == 1, "this should have triggered a finding"
         assert findings[0].alert_id == "FUNDING-TORNADO-CASH"
@@ -60,11 +61,12 @@ class TestTornadoCashFunding:
             'receipt': {
                 'logs': []}
         })
-        
+
         findings = agent.detect_funding(w3, tx_event)
         assert len(findings) == 0, "this should not have triggered a finding"
 
-    def test_funding_high_amount(self):
+    @patch("src.findings.calculate_alert_rate", return_value=0.5)
+    def test_funding_high_amount(self, mocker):
         agent.initialize()
 
         tx_event = create_transaction_event({
@@ -86,15 +88,18 @@ class TestTornadoCashFunding:
             'receipt': {
                 'logs': []}
         })
-        
+
         findings = agent.detect_funding(w3, tx_event)
         assert len(findings) == 1, "this should have triggered a finding"
         assert findings[0].alert_id == "FUNDING-TORNADO-CASH-HIGH"
         assert findings[0].severity == FindingSeverity.Info
 
         assert findings[0].metadata["anomaly_score"] == 0.5, "should have anomaly score of 0.5"
-        assert findings[0].labels[0].toDict()["entity"] == "0xA1B4355Ae6b39bb403Be1003b7D0330C811747DB", "should have EOA address as label"
-        assert findings[0].labels[0].toDict()["entity_type"] == EntityType.Address, "should have label_type address"
-        assert findings[0].labels[0].toDict()["label"] == 'benign', "should have benign as label"
-        assert findings[0].labels[0].toDict()["confidence"] == 0.1, "should have 0.1 as label confidence"
-
+        assert findings[0].labels[0].toDict(
+        )["entity"] == "0xA1B4355Ae6b39bb403Be1003b7D0330C811747DB", "should have EOA address as label"
+        assert findings[0].labels[0].toDict(
+        )["entity_type"] == EntityType.Address, "should have label_type address"
+        assert findings[0].labels[0].toDict(
+        )["label"] == 'benign', "should have benign as label"
+        assert findings[0].labels[0].toDict(
+        )["confidence"] == 0.1, "should have 0.1 as label confidence"
--- funding-tornado-cash-py/src/findings.py
+++ funding-tornado-cash-py/src/findings.py
@@ -1,11 +1,13 @@
 from forta_agent import Finding, FindingType, FindingSeverity, EntityType, Label
+from bot_alert_rate import calculate_alert_rate, ScanCountType
+from src.keys import BOT_ID
 
 
 class FundingTornadoCashFindings:
 
     @staticmethod
-    def funding_tornado_cash(to_address: str, type: str, anomaly_score: float, chain_id:int) -> Finding:
-        if type=="low":
+    def funding_tornado_cash(to_address: str, type: str, chain_id: int) -> Finding:
+        if type == "low":
             labels = [Label({
                 'entityType': EntityType.Address,
                 'label': "attacker",
@@ -15,7 +17,7 @@ class FundingTornadoCashFindings:
                     'alert_id': 'FUNDING-TORNADO-CASH',
                     'chain_id': chain_id
                 }
-    	    })]
+            })]
 
             finding = Finding({
                 'name': 'Tornado Cash Funding',
@@ -23,7 +25,12 @@ class FundingTornadoCashFindings:
                 'alert_id': 'FUNDING-TORNADO-CASH',
                 'type': FindingType.Suspicious,
                 'severity': FindingSeverity.Low,
-                'metadata': {'anomaly_score': anomaly_score},
+                'metadata': {'anomaly_score': calculate_alert_rate(
+                    chain_id,
+                    BOT_ID,
+                    'FUNDING-TORNADO-CASH',
+                    ScanCountType.TRANSFER_COUNT,
+                )},
                 'labels': labels
             })
         else:
@@ -36,7 +43,7 @@ class FundingTornadoCashFindings:
                     'alert_id': 'FUNDING-TORNADO-CASH-HIGH',
                     'chain_id': chain_id
                 }
-    	    })]
+            })]
 
             finding = Finding({
                 'name': 'Tornado Cash Funding',
@@ -44,7 +51,12 @@ class FundingTornadoCashFindings:
                 'alert_id': 'FUNDING-TORNADO-CASH-HIGH',
                 'type': FindingType.Info,
                 'severity': FindingSeverity.Info,
-                'metadata': {'anomaly_score': anomaly_score},
+                'metadata': {'anomaly_score': calculate_alert_rate(
+                    chain_id,
+                    BOT_ID,
+                    'FUNDING-TORNADO-CASH',
+                    ScanCountType.TRANSFER_COUNT,
+                )},
                 'labels': labels
             })
         return finding
