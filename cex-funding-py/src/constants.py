f6e30f9863fef475fad65ca0426855e5e3456aba
Mariko Wakabayashi <mariko@openzeppelin.com>
2023-05-09 21:01:29 -0500

    Update file

--- cex-funding-py/src/constants.py
+++
@@ -1,5 +0,0 @@
-
-#  chainId, address, name, threshold (in wei)
-CEXES = {(1, "0x4e5b2e1dc63f6b91cb6cd759936495434c7e972f", "FixFloat", 2000000000000000000),
-                 (56, "0x4727250679294802377dd6ca6541b8e459077c95", "FixFloat", 10000000000000000000)
-                  }
