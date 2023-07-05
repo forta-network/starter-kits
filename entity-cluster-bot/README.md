6889e4ea4fb0c84a9dd74c1c04c4a49686c36f08
Nicolás Melendez <nfmelendez@gmail.com>
2023-07-05 15:05:27 -0300

    Performance and Sharding of the Entity Cluster Bot

--- entity-cluster-bot/README.md
+++ entity-cluster-bot/README.md
@@ -12,7 +12,104 @@ Further, contracts created by an address are also considered part of the same en
 
 The bot generates a graph and identifies and reports on the connected components.
 
-A entity is created if this condition is observed accounts with less than MAX_NONCE transactions within a ENTITY_CLUSTER_WINDOW_IN_DAYS timeframe. Entities will age out within MAX_AGE_IN_DAYS.
+A entity is created if this condition is observed accounts with less than MAX_NONCE transactions. Entities will age out within MAX_AGE_IN_DAYS.
+
+
+## Sharding Implementation Details
+The bot has a graph of the connections between the addresses that must be shared between many intances. All instances read and write the shared graph at the same time, so to avoid race condition it use a 
+mutex implemented in dynamodb using  [DynamoDb conditional write](http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/WorkingWithItems.html#WorkingWithItems.ConditionalUpdate) and [atomic compare-and-swap](https://en.wikipedia.org/wiki/Compare-and-swap) 
+
+Also we store the graph in s3 as the graph compressed can be around 15MB and optimize costs
+
+Each instance updates the shared graoh every TX_SAVE_STEP 
+
+
+
+## Infrastructure
+
+The bot needs 2 dynamo tables and one s3 bucket.  the dynamodb and the s3 can be in different region, so is better to look for the cheapest regions at the moment before deploy.
+
+### DYNAMO_TABLE= "FORTA-ENTITY-CLUSTER"
+It store the metadata of the graph
+Definition:
+Table class: DynamoDB standard
+Partition Key: p(String)
+Sort Key: s(String)
+Capacity: 1 write, 1 read
+
+
+### DYNAMO_MUTEX_TABLE_NAME = 'FortaEntityClusterMutex'
+Stores and manages the mutex, one per chain. 
+The mutex also has a expire time of 10s at application level and a dynamo ttl at infrastructure level to avoid the mutex being lock if an intance is shutdown in the forta network. 
+The Mutex only use dynamo write capacity
+Definition:
+Table class: DynamoDB standard
+Partition Key: lockname (String)
+Sort Key: -
+ttl feature: on
+ttl name field: ttl
+Capacity: 1 write, 1 read
+
+
+### S3_BUCKET= "fortaentitycluster"
+S3 bucket to store the shared graph, one chared graph per chain.
+Definition:
+Just the standard s3
+
+
+### Infrastructure cost
+Each instance updates the graph every TX_SAVE_STEP, asking for the mutex, saving metadata, reading from s3,  writing in s3 and releasing the mutex all these operations cost $$$. The lower the TX_SAVE_STEP, the more accurate the alert are as the 
+instances "knows what is hapening in the other" but there are more operation over the infrastructure so the cost are higher. By design there is a relationship betweeen cost and accuracy. 
+
+Having a TX_SAVE_STEP = 150*6 (that are around 1 block of ethereum in every 10s, that is around a minute) the cost are:
+DYNAMO_TABLE 0.65 USD/Month (1 write capacity, 1 read capacity) 
+DYNAMO_MUTEX_TABLE_NAME 0.65 USD/Month (1 write capacity, 1 read capacity) 
+S3_BUCKET 0.3 USD/Month
+Total: 1.6 USD/Month 
+
+### IAM permission
+
+Keep them as low as possible, here a template:
+
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:PutItem",
+                "dynamodb:DescribeTable",
+                "dynamodb:DeleteItem",
+                "dynamodb:GetItem",
+                "dynamodb:Scan",
+                "dynamodb:Query",
+                "dynamodb:UpdateItem"
+            ],
+            "Resource": "HIDDEN"
+        },
+        {
+            "Sid": "VisualEditor1",
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject",
+                "s3:GetObject"
+            ],
+            "Resource": "HIDDEN"
+        }
+    ]
+}
+
+Credential should be in keys.py
+
+AWS_ACCESS_KEY=
+AWS_SECRET_KEY=
+
+
+
+## RPC Timeout
+Web3 RPC call are time consuming, there is now a HTTP_RPC_TIMEOUT=2 config to avoid waiting to long if we hit a slow server from the provider. If the rpc call doesn't finish in 2 second it will raise a exception and will continue with the next transaction. usually a rpc call should be 500ms
+
 
 ## Supported Chains
 
@@ -24,7 +121,7 @@ A entity is created if this condition is observed accounts with less than MAX_NO
   - Fired when a new entity with more than one address is detected
   - Severity is always set to "info"
   - Type is always set to "info"
-  - Metadata will contain a unique entity identifier along with all the addresses that are currently associated with the entity.
+  - Metadata will contain a unique entity identifier along with all the addresses that are currently associated with the entity. Also it will have the diagram so can be processed online via d3.js or similar
 
 ## Test Data
 
@@ -36,3 +133,12 @@ creates entity with 0x098b716b8aaf21512996dc57eb0615e2383e2f96 and 0xe708f172407
 
 DFX Finance Exploiter
 npm run tx 0xc8268a45d0043875d8d2eb953b25fbaf3de5fb00f7f9aea0597bc9d5138df0cf,0x5d029c08f5c283716f9fe962218f8429c7ca0b2eb50dda4062b33dbdf74314f6
+
+
+## Experimental
+There is a entity graph viewer that get the metadata of an alert and extract the graph diagram for alerts between 8 and 16 entities
+
+..entity-cluster-bot/viewer/index.html?alertId=0x1d9d84ab28c3c507885329630269326094262c998d9292fb8bef95eb3748c7f4
+
+
+
---
+++ entity-cluster-bot/src/dyndbmutex.py
@@ -0,0 +1,221 @@
+import logging
+import boto3
+import botocore
+import datetime
+import uuid
+import os
+from boto3.dynamodb.conditions import Attr
+
+try:
+    from src.keys import AWS_ACCESS_KEY
+    from src.keys import AWS_SECRET_KEY
+    from src.constants import  GRAPH_KEY, DEV_DYNAMO_TABLE, S3_BUCKET, DYNAMO_MUTEX_TABLE_NAME
+except ModuleNotFoundError:
+    from keys import AWS_ACCESS_KEY
+    from keys import AWS_SECRET_KEY
+    from constants import  GRAPH_KEY, DEV_DYNAMO_TABLE, S3_BUCKET, DYNAMO_MUTEX_TABLE_NAME
+
+
+
+
+logger = logging.getLogger('dyndbmutex')
+
+def setup_logging():
+    logger.setLevel(logging.INFO)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(levelname)s %(asctime)s - %(name)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+
+DEFAULT_MUTEX_TABLE_NAME = DYNAMO_MUTEX_TABLE_NAME
+NO_HOLDER = '__empty__'
+TWO_DAYS_IN_MINUTES = 2*24*60
+
+class AcquireLockFailedError(Exception):
+        pass
+
+
+def timestamp_millis():
+    return int((datetime.datetime.utcnow() -
+                datetime.datetime(1970, 1, 1)).total_seconds() * 1000)
+
+
+class MutexTable:
+
+    def __init__(self, region_name='us-west-2', ttl_minutes=TWO_DAYS_IN_MINUTES):
+        endpoint_url = os.environ.get("DYNAMO_DB_URL", None)
+        self.dbresource = boto3.resource('dynamodb', region_name=region_name, aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY)
+        self.dbclient = boto3.client('dynamodb', region_name=region_name, aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY)
+        self.table_name = os.environ.get('DD_MUTEX_TABLE_NAME', DEFAULT_MUTEX_TABLE_NAME)
+        logger.info("Mutex table name is " + self.table_name)
+        self.ttl_minutes = ttl_minutes
+        self.get_table()
+
+    def get_table(self):
+        try:
+            self.dbclient.describe_table(TableName=self.table_name)
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == 'ResourceNotFoundException':
+                return self.create_table()
+            else:
+                raise
+        else:
+            return self.dbresource.Table(self.table_name)
+
+    def delete_table(self):
+        self.dbclient.delete_table(TableName=self.table_name)
+        logger.info("Deleted table " + self.table_name)
+
+    def get_lock(self, lockname):
+        return self.get_table().get_item(Key={'lockname': lockname})
+
+    def create_table(self):
+        try:
+            table = self.dbresource.create_table(
+                TableName=self.table_name,
+                KeySchema=[
+                    {
+                        'AttributeName': 'lockname',
+                        'KeyType': 'HASH'  # Partition key
+                    },
+                ],
+                AttributeDefinitions=[
+                    {
+                        'AttributeName': 'lockname',
+                        'AttributeType': 'S'
+                    },
+                ],
+                ProvisionedThroughput={
+                    'ReadCapacityUnits': 2,
+                    'WriteCapacityUnits': 2
+                }
+            )
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == 'ResourceInUseException':
+                logger.debug("Table already exists", exc_info=e)
+            else:
+                raise
+        else:
+            logger.debug("Called create_table")
+            table.wait_until_exists()
+            logger.info("Created table " + self.table_name)
+            try:
+                self.dbclient.update_time_to_live(
+                TableName=self.table_name,
+                TimeToLiveSpecification={
+                 'Enabled': True,
+                 'AttributeName': 'ttl'
+                })
+            except botocore.exceptions.ClientError as e:
+                logger.error("Error setting TTL on table", exc_info=e)
+            return table
+
+    def write_lock_item(self, lockname, caller, waitms):
+        expire_ts = timestamp_millis() + waitms
+        ttl = expire_ts//1000 + self.ttl_minutes*60
+        logger.debug("Write_item: lockname=" + lockname + ", caller=" +
+                     caller + ", Expire time is " + str(expire_ts))
+        try:
+            self.get_table().put_item(
+                Item={
+                    'lockname': lockname,
+                    'expire_ts': expire_ts,
+                    'holder': caller,
+                    'ttl': ttl
+                },
+                # TODO: adding Attr("holder").eq(caller) should make it re-entrant
+                ConditionExpression=Attr("holder").eq(NO_HOLDER) | Attr('lockname').not_exists()
+            )
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == 'ConditionalCheckFailedException':
+                logger.info("Write_item: lockname=" + lockname +
+                             ", caller=" + caller + ", lock is being held")
+                return False
+        logger.debug("Write_item: lockname=" + lockname +
+                     ", caller=" + caller + ", lock is acquired")
+        return True
+
+    def clear_lock_item(self, lockname, caller):
+        try:
+            self.get_table().put_item(
+                Item={
+                    'lockname': lockname,
+                    'expire_ts': 0,
+                    'holder': NO_HOLDER
+                },
+                ConditionExpression=Attr("holder").eq(caller) | Attr('lockname').not_exists()
+            )
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == 'ConditionalCheckFailedException':
+                logger.warning("clear_lock_item: lockname=" + lockname + ", caller=" + caller +
+                             " release failed")
+                return False
+        logger.debug("clear_lock_item: lockname=" + lockname + ", caller=" + caller + " release succeeded")
+        return True
+
+    def prune_expired(self, lockname, caller):
+        now = timestamp_millis()
+        logger.debug("Prune: lockname=" + lockname + ", caller=" + caller +
+                     ", Time now is %s" + str(now))
+        try:
+            self.get_table().put_item(
+                Item={
+                    'lockname': lockname,
+                    'expire_ts': 0,
+                    'holder': NO_HOLDER
+                },
+                ConditionExpression=Attr("expire_ts").lt(now) | Attr('lockname').not_exists()
+            )
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == 'ConditionalCheckFailedException':
+                logger.info("Prune: lockname=" + lockname + ", caller=" + caller +
+                             " Prune failed")
+                return False
+        logger.debug("Prune: lockname=" + lockname + ", caller=" + caller + " Prune succeeded")
+        return True
+
+
+class DynamoDbMutex:
+
+    def __init__(self, name, holder=None,
+                 timeoutms=10 * 1000, region_name='us-west-2', ttl_minutes=TWO_DAYS_IN_MINUTES):
+        if holder is None:
+            holder = str(uuid.uuid4())
+        self.lockname = name
+        self.holder = holder
+        self.timeoutms = timeoutms
+        self.table = MutexTable(region_name=region_name, ttl_minutes=ttl_minutes)
+        self.locked = False
+
+    def lock(self):
+        self.table.prune_expired(self.lockname, self.holder)
+        self.locked = self.table.write_lock_item(self.lockname, self.holder, self.timeoutms)
+        logger.info("mutex.lock(): lockname=" + self.lockname + ", locked = " + str(self.locked))
+        return self.locked
+
+    def release(self):
+        released = self.table.clear_lock_item(self.lockname, self.holder)
+        self.locked = not released
+        logger.info("mutex.release(): lockname=" + self.lockname + ", locked = " + str(self.locked))
+
+    def __enter__(self):
+        locked = self.lock()
+        if not locked:
+            raise AcquireLockFailedError()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.release()
+
+    def is_locked(self):
+        return self.locked
+
+    def get_raw_lock(self):
+        return self.table.get_lock(self.lockname)
+
+    @staticmethod
+    def delete_table(region_name='us-west-2'):
+        table = MutexTable(region_name)
+        table.delete_table()
