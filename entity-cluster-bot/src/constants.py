8ce7f0498c91a0f9aedf292f215fd7195a7f2351
Nicolás Melendez <nfmelendez@gmail.com>
2023-07-07 09:47:39 -0300

    Forta prod configuration

--- entity-cluster-bot/src/constants.py
+++ entity-cluster-bot/src/constants.py
@@ -8,12 +8,12 @@ GRAPH_KEY = "graph_key"
 NEW_FUNDED_MAX_NONCE = 1
 NEW_FUNDED_MAX_WEI_TRANSFER_THRESHOLD = 1000000000000000000 # 1 ETH
 
-S3_BUCKET= "fortaentitycluster"
+S3_BUCKET= "prod-research-bot-data"
 S3_REGION="us-east-1"
 
 DYNAMO_TABLE= "prod-research-bot-data"
 DEV_DYNAMO_TABLE= "prod-research-bot-data"
-DYNAMO_REGION="us-west-1"
+DYNAMO_REGION="us-east-1"
 DYNAMODB_PRIMARY_KEY = 'itemId'
 DYNAMODB_SORT_KEY = 'sortKey'
 DYNAMODB_TTL_KEY = 'expiresAt'
