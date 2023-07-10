d5f254a4d5a103856fcd601d882e404336e94a6e
christian-forta <christian@forta.org>
2023-07-10 09:44:41 -0700

    refactor for better caching

--- positive-reputation-py/src/blockexplorer.py
+++ positive-reputation-py/src/blockexplorer.py
@@ -4,35 +4,38 @@ import datetime
 import requests
 import pandas as pd
 
-from src.api_keys import BSC_API_KEY, ETHERSCAN_API_KEY, POLYGON_API_KEY, ARBITRUM_API_KEY, OPTIMISM_API_KEY, FANTOM_API_KEY, AVALANCHE_API_KEY
-
+from src.storage import get_secrets
 
 class BlockExplorer:
     api_key = ""
     host = ""
+    SECRETS_JSON = None
 
     def __init__(self, chain_id):
+        if BlockExplorer.SECRETS_JSON is None:
+            BlockExplorer.SECRETS_JSON = get_secrets()
+        
         if chain_id == 1:
             self.host = "https://api.etherscan.io"
-            self.api_key = ETHERSCAN_API_KEY
+            self.api_key = BlockExplorer.SECRETS_JSON['apiKeys']['ETHERSCAN_TOKEN']
         elif chain_id == 137:
             self.host = "https://api.polygonscan.com"
-            self.api_key = POLYGON_API_KEY
+            self.api_key = BlockExplorer.SECRETS_JSON['apiKeys']['POLYGONSCAN_TOKEN']  
         elif chain_id == 56:
             self.host = "https://api.bscscan.com"
-            self.api_key = BSC_API_KEY
+            self.api_key = BlockExplorer.SECRETS_JSON['apiKeys']['BSCSCAN_TOKEN']  
         elif chain_id == 42161:
             self.host = "https://api.arbiscan.io"
-            self.api_key = ARBITRUM_API_KEY
+            self.api_key = BlockExplorer.SECRETS_JSON['apiKeys']['ARBISCAN_TOKEN']  
         elif chain_id == 10:
             self.host = "https://api-optimistic.etherscan.io"
-            self.api_key = OPTIMISM_API_KEY
+            self.api_key = BlockExplorer.SECRETS_JSON['apiKeys']['OPTIMISTICSCAN_TOKEN']  
         elif chain_id == 250:
             self.host = "https://api.ftmscan.com"
-            self.api_key = FANTOM_API_KEY
+            self.api_key = BlockExplorer.SECRETS_JSON['apiKeys']['FTMSCAN_TOKEN']  
         elif chain_id == 43114:
             self.host = "https://api.snowtrace.io"
-            self.api_key = AVALANCHE_API_KEY
+            self.api_key = BlockExplorer.SECRETS_JSON['apiKeys']['SNOWTRACE_TOKEN']  
 
 
     def get_first_tx(self, address: str) -> datetime:
