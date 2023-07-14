1c3849975cb8f61094fb4057605979ca4d8fec67
apehex <apehex@protonmail.com>
2023-07-14 21:07:20 +0200

    Use generic metrics instead of targeting specific contracts

--- batched-tx-disperse-multisend-py/src/agent.py
+++ batched-tx-disperse-multisend-py/src/agent.py
@@ -1,48 +1,54 @@
-"""Forta agent scanning for batched transactions"""
+"""Forta agent scanning for batched transactions."""
 
 from web3 import Web3
 from forta_agent import get_json_rpc_url, FindingSeverity
 from forta_agent.transaction_event import TransactionEvent
 
+import src.metrics.batch as batch
 import src.options as options
-import src.disperse as disperse
 import src.findings as findings
-import src.multisend as multisend
 
-# CONSTANTS ###################################################################
+# FILTERS #####################################################################
 
-ADDRESS_TO_NAME = {
-    disperse.ADDRESS.lower(): 'Disperse',
-    multisend.ADDRESS.lower(): 'Multisend'}
+# PARSERS #####################################################################
+
+def _parse(log: TransactionEvent, w3: Web3) -> dict:
+    """Gather and parse the data from all the relevant sources in one dict."""
+    _truc = {}
+    _from = str(getattr(log.transaction, 'from_', '')).lower()
+    _to = str(getattr(log.transaction, 'to', '')).lower() # could be None in case of a contract creation
+    _data = str(getattr(log.transaction, 'data', '')).lower()
+    return _truc
 
 # SCANNER #####################################################################
 
-def handle_transaction_factory(w3: Web3, token: str=options.TARGET_TOKEN) -> callable:
+def handle_transaction_factory(
+    w3: Web3,
+    target_token: str=options.TARGET_TOKEN,
+    target_contract: str=options.TARGET_CONTRACT,
+    min_transfer_count: int=options.MIN_TRANSFER_COUNT,
+    min_transfer_total_erc20: int=options.MIN_TRANSFER_TOTAL_ERC20,
+    min_transfer_total_native: int=options.MIN_TRANSFER_TOTAL_NATIVE,
+    min_confidence_score: float=options.MIN_CONFIDENCE_SCORE,
+    min_malicious_score: int=options.MIN_MALICIOUS_SCORE
+) -> callable:
+    """Setup the main"""
     _chain_id = w3.eth.chain_id
-    _parsers = { # input data parsers for each target contract
-        disperse.ADDRESS.lower(): disperse.parse_transaction_input_factory(w3=w3, token=token),
-        multisend.ADDRESS.lower(): multisend.parse_transaction_input_factory(w3=w3, token=token)}
 
-    def _handle_transaction(transaction_event: TransactionEvent) -> list:
+    def _handle_transaction(log: TransactionEvent) -> list:
+        """Main function called on the logs gathered by the Forta network."""
         _findings = []
-        _from = str(getattr(transaction_event.transaction, 'from_', '')).lower()
-        _to = str(getattr(transaction_event.transaction, 'to', '')).lower() # could be None in case of a contract creation
-        _data = str(getattr(transaction_event.transaction, 'data', '')).lower()
-        _wrapped_tx = []
-        
-        if _to in _parsers and options.TARGET_CONTRACT in _to:
-            _token, _wrapped_tx, _is_manual = _parsers[_to](_data)
-            if _wrapped_tx:
-                _findings.append(findings.FormatBatchTxFinding(
-                    origin=_from,
-                    contract=ADDRESS_TO_NAME[_to],
-                    token=_token,
-                    transactions=_wrapped_tx,
-                    chain_id=_chain_id,
-                    severity=FindingSeverity.Low if _is_manual else FindingSeverity.Info))
-
+        print(batch.confidence_score(log=log, w3=w3))
+        # _findings.append(findings.FormatBatchTxFinding(
+        #     origin=_from,
+        #     contract=ADDRESS_TO_NAME[_to],
+        #     token=_token,
+        #     transactions=_wrapped_tx,
+        #     chain_id=_chain_id,
+        #     severity=FindingSeverity.Low if _is_manual else FindingSeverity.Info))
         return _findings
 
     return _handle_transaction
 
+# run with the default settings
 handle_transaction = handle_transaction_factory(w3=Web3(Web3.HTTPProvider(get_json_rpc_url())))
--- batched-tx-disperse-multisend-py/src/options.py
+++ batched-tx-disperse-multisend-py/src/options.py
@@ -4,8 +4,8 @@
 
 TARGET_CONTRACT = '' # leave empty to disable the filtering by contract, otherwise enter an address like '0x767fe9edc9e0df98e07454847909b5e959d7ca0e'
 TARGET_TOKEN = '' # leave empty to disable the filtering by token, otherwise enter an address like '0x767fe9edc9e0df98e07454847909b5e959d7ca0e'
-MIN_TRANSFER_COUNT = 0
+MIN_TRANSFER_COUNT = 4
 MIN_TRANSFER_TOTAL_ERC20 = 0
-MIN_TRANSFER_TOTAL_NATIVE = 0
-MIN_CONFIDENCE_SCORE = 0.5
-MIN_ANOMALY_SCORE = 0.5
+MIN_TRANSFER_TOTAL_NATIVE = 10**18 # 1 ETH
+MIN_CONFIDENCE_SCORE = 0.6
+MIN_MALICIOUS_SCORE = 0.6
