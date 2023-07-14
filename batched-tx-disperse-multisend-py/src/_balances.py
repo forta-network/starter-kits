e0f7fbf33477dccc68fbc40a169d228fcc63a80f
apehex <apehex@protonmail.com>
2023-07-14 21:01:10 +0200

    Fix: skip balance checks on contract creation

--- batched-tx-disperse-multisend-py/src/_balances.py
+++ batched-tx-disperse-multisend-py/src/_balances.py
@@ -7,6 +7,8 @@ from web3 import Web3
 # TODO: cache the requests
 def get_balance_delta(w3: Web3, address: str, block: int) -> int:
     """Calculate the difference in balance before / after a given block."""
-    _before = w3.eth.get_balance(Web3.toChecksumAddress(address), block - 1)
-    _after = w3.eth.get_balance(Web3.toChecksumAddress(address), block)
+    _before = _after = 0
+    if address:
+        _before = w3.eth.get_balance(Web3.toChecksumAddress(address), block - 1)
+        _after = w3.eth.get_balance(Web3.toChecksumAddress(address), block)
     return _after - _before
