25f2d59beeb584ef374742b88a8dd1ef76ba6680
apehex <apehex@protonmail.com>
2023-07-14 19:53:20 +0200

    Analyse the event log & balance updates

--- batched-tx-disperse-multisend-py/src/metrics/_indicators.py
+++ batched-tx-disperse-multisend-py/src/metrics/_indicators.py
@@ -1,6 +1,11 @@
 """Base indicators on transactions and their metadata."""
 
+from forta_agent.transaction_event import TransactionEvent
+from web3 import Web3
+
 import src._addresses as addresses
+import src._balances as balances
+import src._events as events
 import src._inputs as inputs
 import src._selectors as selectors
 
@@ -25,4 +30,23 @@ def input_data_has_array_of_values(data: str) -> bool:
 
 # EVENTS INDICATORS ###########################################################
 
+def log_has_multiple_erc20_transfer_events(log: TransactionEvent, floor: int) -> bool:
+    return len(events.parse_log(log=log, abi=events.ERC20_TRANSFER_EVENT)) >= floor
+
+def log_has_multiple_erc721_transfer_events(log: TransactionEvent, floor: int) -> bool:
+    return len(events.parse_log(log=log, abi=events.ERC721_TRANSFER_EVENT)) >= floor
+
 # BALANCES INDICATORS #########################################################
+
+def _get_all_balance_updates(w3: Web3, addresses: list, block: int) -> list:
+    """List all the addresses that sustained a balance change."""
+    _deltas = [balances.get_balance_delta(w3=w3, address=_a, block=block) for _a in addresses]
+    return [_d for _d in _deltas if abs(_d) > 0]
+
+def multiple_native_token_balances_have_been_updated(w3: Web3, data: str, block: int, floor: int) -> bool:
+    _recipients = inputs.get_array_of_address_candidates(data)
+    _deltas = _get_all_balance_updates(w3=w3, addresses=_recipients, block=block)
+    return len(_deltas) >= floor
+
+def receiver_contract_balance_did_not_change(w3: Web3, address: str, block: int, tolerance: int=10**17) -> bool:
+    return balances.get_balance_delta(w3=w3, address=address, block=block) <= tolerance # in case the contract has a fee, set to 0.1 EHT by default
--- batched-tx-disperse-multisend-py/src/metrics/batch.py
+++ batched-tx-disperse-multisend-py/src/metrics/batch.py
@@ -1,35 +1,49 @@
 """Evaluate the probability that multiple transfers were bundled in a transaction."""
 
+from forta_agent.transaction_event import TransactionEvent
+from web3 import Web3
+
 import src.metrics._indicators as indicators
 import src.metrics._probabilities as probabilities
 
 # CONFIDENCE ##################################################################
 
-def confidence_score(receiver: str, data: str) -> float:
+def confidence_score(log: TransactionEvent, w3: Web3) -> float:
     """Evaluate the probability that multiple transfers were bundled in a transaction."""
     _scores = []
+    _data = str(getattr(log.transaction, 'data', '')).lower()
+    _block = int(log.block.number)
     # method selector
     _scores.append(probabilities.indicator_to_probability(
-        indicator=indicators.input_data_has_batching_selector(data),
+        indicator=indicators.input_data_has_batching_selector(_data),
         true_score=0.9, # almost certainty
         false_score=0.5)) # not all selectors are in the wordlist: neutral
     # list of recipients
     _scores.append(probabilities.indicator_to_probability(
-        indicator=indicators.input_data_has_array_of_addresses(data),
+        indicator=indicators.input_data_has_array_of_addresses(_data),
         true_score=0.7, # the list of recipients is necessary for batching, and not seen in many other types of transactions
         false_score=0.1)) # without a list of recipients, there is almost no chance the contract performs batching
     # list of amounts to transfer
     _scores.append(probabilities.indicator_to_probability(
-        indicator=indicators.input_data_has_array_of_values(data),
+        indicator=indicators.input_data_has_array_of_values(_data),
         true_score=0.6, # low prob: the array of values could have another meaning
         false_score=0.4)) # batching can happpen without a list of values: NFT transfers or same amount for all
+    # erc20 events OR erc721 events OR balance updates
+    _has_token_transfers = (
+        indicators.log_has_multiple_erc20_transfer_events(log=log, floor=4)
+        or indicators.log_has_multiple_erc721_transfer_events(log=log, floor=4)
+        or indicators.multiple_native_token_balances_have_been_updated(w3=w3, data=_data, block=_block, floor=4))
+    _scores.append(probabilities.indicator_to_probability(
+        indicator=_has_token_transfers,
+        true_score=0.8, # a list of transfers almost certainly means batching
+        false_score=0.2)) # it's possible the transfered token doesn't follow ERC20 and did not emit an event
     return probabilities.conflation(_scores)
 
 # ANOMALY #####################################################################
 
 # events differ from input data
 
-def anomaly_score(receiver: str, data: str) -> float:
+def malicious_score(log: TransactionEvent, w3: Web3) -> float:
     """Evaluate."""
     _scores = []
     return probabilities.conflation(_scores)
