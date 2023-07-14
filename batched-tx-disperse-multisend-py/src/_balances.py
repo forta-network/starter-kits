b3fac2cb411b5190a1dbdf55eaaad72d8cff7011
apehex <apehex@protonmail.com>
2023-07-14 19:53:20 +0200

    Analyse the event log & balance updates

---
+++ batched-tx-disperse-multisend-py/src/_balances.py
@@ -0,0 +1,12 @@
+"""Track the evolution of native token balances."""
+
+from web3 import Web3
+
+# DELTA #######################################################################
+
+# TODO: cache the requests
+def get_balance_delta(w3: Web3, address: str, block: int) -> int:
+    """Calculate the difference in balance before / after a given block."""
+    _before = w3.eth.get_balance(Web3.toChecksumAddress(address), block - 1)
+    _after = w3.eth.get_balance(Web3.toChecksumAddress(address), block)
+    return _after - _before
