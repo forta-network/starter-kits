0f040bec5c6043cabcf6639a5f6fb838f22cad43
Tayfun Sur <tayfunsur@gmail.com>
2023-08-04 12:13:47 +0300

    Remove unused calculations&variables

--- money-laundering-tornado-cash-py/src/agent.py
+++ money-laundering-tornado-cash-py/src/agent.py
@@ -6,13 +6,9 @@ from forta_agent import get_json_rpc_url
 from web3 import Web3
 from os import environ
 
-from src.constants import (BLOCK_RANGE, TORNADO_CASH_ACCOUNTS_QUEUE_SIZE,
-                           TORNADO_CASH_ADDRESSES, TORNADO_CASH_DEPOSIT_SIZE,
-                           TORNADO_CASH_DEPOSIT_SIZE_MATIC,
-                           TORNADO_CASH_DEPOSIT_TOPIC,
-                           TORNADO_CASH_TRANSFER_COUNT_THRESHOLD_BSC,
-                           TORNADO_CASH_TRANSFER_COUNT_THRESHOLD_ETH,
-                           TORNADO_CASH_TRANSFER_COUNT_THRESHOLD_MATIC)
+from src.constants import (TORNADO_CASH_ACCOUNTS_QUEUE_SIZE,
+                           TORNADO_CASH_ADDRESSES, TORNADO_CASH_TRANSFER_AMOUNT_THRESHOLDS,
+                           TORNADO_CASH_DEPOSIT_TOPIC,)
 from src.findings import MoneyLaunderingTornadoCashFindings
 from src.storage import get_secrets
 
@@ -21,7 +17,6 @@ SECRETS_JSON = get_secrets()
 web3 = Web3(Web3.HTTPProvider(get_json_rpc_url()))
 
 # dict of accounts to dicts of blocks to counts; e.g. # account 1, block 101, 1
-ACCOUNT_TO_TORNADO_CASH_BLOCKS = {}
 ACCOUNT_QUEUE = []
 
 
@@ -41,8 +36,6 @@ def initialize():
     this function initializes the state variables that are tracked across tx and blocks
     it is called from test to reset state between tests
     """
-    global ACCOUNT_TO_TORNADO_CASH_BLOCKS
-    ACCOUNT_TO_TORNADO_CASH_BLOCKS = {}
 
     global ACCOUNT_QUEUE
     ACCOUNT_QUEUE = []
@@ -54,7 +47,6 @@ def initialize():
 
 
 def detect_money_laundering(w3, transaction_event: forta_agent.transaction_event.TransactionEvent) -> list:
-    global ACCOUNT_TO_TORNADO_CASH_BLOCKS
     global ACCOUNT_QUEUE
 
     logging.info(
@@ -74,30 +66,15 @@ def detect_money_laundering(w3, transaction_event: forta_agent.transaction_event
             logging.info(
                 f"Identified account {account} on chain {w3.eth.chain_id}")
 
-            block_to_tx_count = {}
-            if account not in ACCOUNT_TO_TORNADO_CASH_BLOCKS:
-                ACCOUNT_TO_TORNADO_CASH_BLOCKS[account] = block_to_tx_count
             else:
                 block_to_tx_count = ACCOUNT_TO_TORNADO_CASH_BLOCKS[account]
 
-            if transaction_event.block_number not in block_to_tx_count.keys():
-                block_to_tx_count[transaction_event.block_number] = 1
-            else:
-                block_to_tx_count[transaction_event.block_number] += 1
 
             #  maintain a size
             if len(ACCOUNT_QUEUE) > TORNADO_CASH_ACCOUNTS_QUEUE_SIZE:
                 acc = ACCOUNT_QUEUE.pop(0)
-                ACCOUNT_TO_TORNADO_CASH_BLOCKS.pop(acc, None)
-
-            while max(block_to_tx_count.keys()) - min(block_to_tx_count.keys()) > BLOCK_RANGE[w3.eth.chain_id]:
-                #  remove the oldest blocks
-                oldest_block = min(block_to_tx_count,
-                                   key=block_to_tx_count.get)
-                block_to_tx_count.pop(oldest_block, None)
 
     if account in ACCOUNT_QUEUE:
-        total_txs = sum(ACCOUNT_TO_TORNADO_CASH_BLOCKS[account].values())
         logging.info(f"Account {account} total txs {total_txs}")
 
         tx_threshold = TORNADO_CASH_TRANSFER_COUNT_THRESHOLD_ETH
