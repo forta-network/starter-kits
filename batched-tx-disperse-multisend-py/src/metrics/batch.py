888a5b1ea813bf237d8dded0de303cdf9658c2b1
apehex <apehex@protonmail.com>
2023-08-26 20:36:25 +0200

    Tidy up

--- batched-tx-disperse-multisend-py/src/metrics/batch.py
+++ batched-tx-disperse-multisend-py/src/metrics/batch.py
@@ -24,6 +24,7 @@ def confidence_score(
     _from = str(getattr(log.transaction, 'from_', '')).lower()
     _data = str(getattr(log.transaction, 'data', '')).lower()
     _value = int(getattr(log.transaction, 'value', ''))
+    _logs = tuple(log.logs)
     # method selector
     _scores.append(probabilities.indicator_to_probability(
         indicator=indicators.input_data_has_batching_selector(_data),
@@ -41,8 +42,8 @@ def confidence_score(
         false_score=0.4)) # batching can happpen without a list of values: NFT transfers or same amount for all
     # erc20 events OR erc721 events OR balance updates
     _has_any_token_transfers = (
-        indicators.log_has_multiple_erc20_transfer_events(tx=log, min_count=min_transfer_count, min_total=min_transfer_total_erc20) # erc20
-        or indicators.log_has_multiple_erc721_transfer_events(tx=log, min_count=min_transfer_count) # erc721
+        indicators.log_has_multiple_erc20_transfer_events(logs=_logs, min_count=min_transfer_count, min_total=min_transfer_total_erc20) # erc20
+        or indicators.log_has_multiple_erc721_transfer_events(logs=_logs, min_count=min_transfer_count) # erc721
         or (
             _value >= max_batching_fee # don't go further if the sender's balance didn't move
             and indicators.transaction_value_matches_input_arrays(value=_value, data=_data, min_count=min_transfer_count, tolerance=max_batching_fee))) # only called if there are no ERC20 / ERC721 events (net opt)
@@ -64,9 +65,10 @@ def malicious_score(
 ) -> float:
     """Evaluate the provabability that a batch transaction is malicious."""
     _scores = []
+    _logs = tuple(log.logs)
     # transfer of amount 0
     _scores.append(probabilities.indicator_to_probability(
-        indicator=indicators.log_has_erc20_transfer_of_null_amount(tx=log),
+        indicator=indicators.log_has_erc20_transfer_of_null_amount(logs=_logs),
         true_score=0.9, # certainty
         false_score=0.5)) # neutral
     return probabilities.conflation(_scores)
--- batched-tx-disperse-multisend-py/src/parsing/events.py
+++ batched-tx-disperse-multisend-py/src/parsing/events.py
@@ -16,24 +16,22 @@ from web3.types import ABIEvent
 
 # TODO variants with / without indexing
 
-ERC20_APPROVAL_EVENT = '{"name":"Approval","type":"event","anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}]}'
-ERC20_TRANSFER_EVENT = '{"name":"Transfer","type":"event","anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}]}'
+ERC20_APPROVAL_EVENT = ABIEvent(json.loads('{"name":"Approval","type":"event","anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}]}'))
+ERC20_TRANSFER_EVENT = ABIEvent(json.loads('{"name":"Transfer","type":"event","anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}]}'))
+ERC721_APPROVAL_EVENT = ABIEvent(json.loads('{"name":"Approval","type":"event","anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_approved","type":"address"},{"indexed":true,"name":"_tokenId","type":"uint256"}]}'))
+ERC721_APPROVAL_FOR_ALL_EVENT = ABIEvent(json.loads('{"name":"ApprovalForAll","type":"event","anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_operator","type":"address"},{"indexed":false,"name":"_approved","type":"bool"}]}'))
+ERC721_TRANSFER_EVENT = ABIEvent(json.loads('{"name":"Transfer","type":"event","anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":true,"name":"_tokenId","type":"uint256"}]}'))
 
-ERC721_APPROVAL_EVENT = '{"name":"Approval","type":"event","anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_approved","type":"address"},{"indexed":true,"name":"_tokenId","type":"uint256"}]}'
-ERC721_APPROVAL_FOR_ALL_EVENT = '{"name":"ApprovalForAll","type":"event","anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_operator","type":"address"},{"indexed":false,"name":"_approved","type":"bool"}]}'
-ERC721_TRANSFER_EVENT = '{"name":"Transfer","type":"event","anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":true,"name":"_tokenId","type":"uint256"}]}'
-
-@functools.lru_cache(maxsize=128)
-def _get_input_names(abi: str) -> tuple:
+def _get_input_names(abi: ABIEvent) -> tuple:
     """Extract the name of each input of an event, from its ABI."""
-    return tuple(_a.get('name', '') for _a in json.loads(abi).get('inputs', []))
+    return tuple(_a.get('name', '') for _a in abi.get('inputs', []))
 
 @functools.lru_cache(maxsize=128)
 def _abi_codec() -> ABICodec:
     """Wrapper around the registry for encoding & decoding ABIs."""
     return ABICodec(build_strict_registry())
 
-# PARSE #######################################################################
+# FORMAT ######################################################################
 
 @functools.lru_cache(maxsize=128)
 def _get_arg_value(event: 'AttributeDict', name: str) -> str:
@@ -54,28 +52,46 @@ def _parse_event(event: 'AttributeDict', names: tuple) -> dict:
         'to': _get_arg_value(event=event, name=names[1]),
         'value': _get_arg_value(event=event, name=names[2])}
 
-@functools.lru_cache(maxsize=128)
-def _get_event_data(logs: tuple, abi: str, codec: ABICodec) -> list:
-    _abi = ABIEvent(json.loads(abi))
-    # parse logs
-    _results = []
-    for _log in logs:
-        _log.topics = [HexBytes(_topic) for _topic in _log.topics]
-        try:
-            _results.append(get_event_data(codec, _abi, _log))
-        except MismatchedABI:
-            continue
-        except LogTopicError:
-            continue
-        except Exception as e:
-            logging.error(e)
-            raise e
-    return _results
+# DECODE ######################################################################
 
-@functools.lru_cache(maxsize=128)
-def parse_logs(logs: tuple, abi: str=ERC20_TRANSFER_EVENT, codec: ABICodec=_abi_codec()) -> tuple:
-    """Extract all the event matching a given ABI."""
+def get_event_data_factory(abi: ABIEvent, codec: ABICodec) -> list:
+    """Adapt the parsing logic to a given event."""
+
+    @functools.lru_cache(maxsize=128)
+    def _get_event_data(logs: tuple) -> list:
+        """Extract event data from the hex log topics."""
+        _results = []
+        for _log in logs:
+            _log.topics = [HexBytes(_topic) for _topic in _log.topics]
+            try:
+                _results.append(get_event_data(codec, abi, _log))
+            except MismatchedABI: # topic and event don't match
+                continue
+            except LogTopicError: # topic and event match, but the args are not split between topic and data as expected ("indexed" issue)
+                continue
+            except Exception as e:
+                logging.error(e)
+                raise e
+        return _results
+
+    return _get_event_data
+
+def parse_logs_factory(abi: ABIEvent=ERC20_TRANSFER_EVENT, codec: ABICodec=_abi_codec()) -> callable:
+    """Adapt the parsing logic to a given event."""
     _inputs = _get_input_names(abi)
-    _events = _get_event_data(logs=logs, abi=abi, codec=codec)
-    # return the args of each event in a dict
-    return tuple(_parse_event(event=_e, names=_inputs) for _e in _events)
+    _get_event_data = get_event_data_factory(abi=abi, codec=codec)
+
+    @functools.lru_cache(maxsize=128)
+    def _parse_logs(logs: tuple) -> tuple:
+        """Extract all the event matching a given ABI."""
+        _events = _get_event_data(logs=logs)
+        # return the args of each event in a dict
+        return tuple(_parse_event(event=_e, names=_inputs) for _e in _events)
+
+    return _parse_logs
+
+# SHORTHANDS ##################################################################
+
+filter_logs_for_erc20_transfer_events = parse_logs_factory(abi=ERC20_TRANSFER_EVENT, codec=_abi_codec())
+
+filter_logs_for_erc721_transfer_events = parse_logs_factory(abi=ERC721_TRANSFER_EVENT, codec=_abi_codec())
