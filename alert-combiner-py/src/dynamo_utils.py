6c5cb3f94a6aed041ef740fa30cef1f0cc6378ff
Vxatz <vxatzistratis@gmail.com>
2023-10-31 16:20:29 +0200

    feat: Update dynamo db utils

--- alert-combiner-py/src/dynamo_utils.py
+++ alert-combiner-py/src/dynamo_utils.py
@@ -1,9 +1,12 @@
 import logging
 from datetime import datetime
 import time
+import json
+import botocore
+import hashlib
 import pandas as pd
 
-from src.constants import ALERTS_LOOKBACK_WINDOW_IN_HOURS
+from src.constants import ALERTS_LOOKBACK_WINDOW_IN_HOURS, DATAFRAME_SIZE_LIMIT
 from src.utils import Utils
 
 TEST_TAG = "attack-detector-test_v2"
@@ -28,12 +31,38 @@ class DynamoUtils:
             return int(time.time()) + int(expiry_offset)
 
     def _put_item(self, dynamo, item):
-        response = dynamo.put_item(Item=item)
-        if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
-            logging.error(f"Error putting item in dynamoDB: {response}")
-            Utils.ERROR_CACHE.add(Utils.alert_error(f'dynamo_utils._put_item HTTPStatusCode {response["ResponseMetadata"]["HTTPStatusCode"]}', "dynamo_utils._put_item", ""))
-        else:
-            logging.info(f"Successfully put item in dynamoDB: {response}")
+        response = None
+        error_message = f'dynamo_utils._put_item'
+
+        # Check if 'cluster' key exists in item
+        if 'cluster' in item:
+            error_message += f', Cluster: {item["cluster"]}'
+
+        # Check if 'address' key exists in item
+        if 'address' in item:
+            error_message += f', Address: {item["address"]}'
+
+        try:
+            response = dynamo.put_item(Item=item)
+            if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
+                logging.error(f"Error putting item in dynamoDB: {response}")           
+                Utils.ERROR_CACHE.add(Utils.alert_error(f'dynamo_utils._put_item HTTPStatusCode {response["ResponseMetadata"]["HTTPStatusCode"]}', "dynamo_utils._put_item", ""))
+            else:
+                logging.info(f"Successfully put item in dynamoDB: {response}")
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == 'ValidationException':
+                logging.error(f"ValidationException when calling the PutItem operation: {e}")
+                error_message = f'dynamo_utils._put_item'
+
+                if 'cluster' in item:
+                    error_message += f', Cluster: {item["cluster"]}'
+
+                if 'address' in item:
+                    error_message += f', Address: {item["address"]}'
+                Utils.ERROR_CACHE.add(Utils.alert_error(error_message, "dynamo_utils._put_item", ""))
+            else:
+                logging.error(f"Error putting item in dynamoDB: {e}")
+                Utils.ERROR_CACHE.add(Utils.alert_error(f'dynamo_utils._put_item Other Exception {e}', "dynamo_utils._put_item", ""))
 
     def put_entity_cluster(self, dynamo, alert_created_at_str: str, address: str, cluster: str):
         logging.debug(f"putting entity clustering alert for {address} in dynamo DB")
@@ -43,13 +72,14 @@ class DynamoUtils:
         logging.debug(f"itemId: {itemId}")
         sortId = f"{address}"
         logging.debug(f"sortId: {sortId}")
+        sortIdHash = hashlib.sha256(sortId.encode()).hexdigest()
         
         expiresAt = self._get_expires_at(alert_created_at)
         logging.debug(f"expiresAt: {expiresAt}")
         
         item = {
             "itemId": itemId,
-            "sortKey": sortId,
+            "sortKey": sortIdHash,
             "address": address,
             "cluster": cluster,
             "expiresAt": expiresAt
@@ -63,13 +93,13 @@ class DynamoUtils:
         logging.debug(f"itemId: {itemId}")
         sortId = f"{address}"
         logging.debug(f"sortId: {sortId}")
-
+        sortIdHash = hashlib.sha256(sortId.encode()).hexdigest()
         expiresAt = self._get_expires_at()
         logging.debug(f"expiresAt: {expiresAt}")
 
         item = {
             "itemId": itemId,
-            "sortKey": sortId,
+            "sortKey": sortIdHash,
             "address": address,
             "expiresAt": expiresAt
         }
@@ -82,13 +112,13 @@ class DynamoUtils:
         logging.debug(f"itemId: {itemId}")
         sortId = f"{address}"
         logging.debug(f"sortId: {sortId}")
-
+        sortIdHash = hashlib.sha256(sortId.encode()).hexdigest()
         expiresAt = self._get_expires_at()
         logging.debug(f"expiresAt: {expiresAt}")
 
         item = {
             "itemId": itemId,
-            "sortKey": sortId,
+            "sortKey": sortIdHash,
             "address": address,
             "expiresAt": expiresAt
         }
@@ -103,6 +133,7 @@ class DynamoUtils:
         logging.debug(f"itemId: {itemId}")
         sortId = f"{cluster}"
         logging.debug(f"sortId: {sortId}")
+        sortIdHash = hashlib.sha256(sortId.encode()).hexdigest()
         expiresAt = self._get_expires_at(last_alert_created_at)
         logging.debug(f"expiresAt: {expiresAt}")    
         logging.debug(f"Dataframe length before filtering: {len(dataframe)}")
@@ -110,12 +141,31 @@ class DynamoUtils:
         dataframe = dataframe[dataframe["created_at"] > (pd.to_datetime(last_alert_created_at, unit='s') - expiry_offset)]
         logging.debug(f"Dataframe length after filtering: {len(dataframe)}")
         dataframe_json = dataframe.to_json(orient="records")
-        
+        dataframe_json_size = len(json.dumps(dataframe_json))
+
+        if dataframe_json_size > DATAFRAME_SIZE_LIMIT:
+            grouped = dataframe.groupby(['bot_id', 'alert_id'])
+
+            # Function to keep the top half of entries based on 'anomaly_score'
+            def reduce_entries(group):
+                if len(group) > 1:
+                    half = len(group) // 2
+                    return group.nsmallest(half, 'anomaly_score')
+                else:
+                    return group
+
+            # Apply the data reduction function to each group and concatenate the results
+            dataframe_to_store = grouped.apply(reduce_entries).reset_index(drop=True)
+        else:
+            dataframe_to_store = dataframe
+
+        dataframe_to_store_json = dataframe_to_store.to_json(orient="records")
+
         item = {
             "itemId": itemId,
-            "sortKey": sortId,
+            "sortKey": sortIdHash,
             "cluster": cluster,
-            "dataframe": dataframe_json,
+            "dataframe": dataframe_to_store_json,
             "expiresAt": expiresAt
         }
 
@@ -127,13 +177,14 @@ class DynamoUtils:
         logging.debug(f"itemId: {itemId}")
         sortId = f"{transaction_hash}"
         logging.debug(f"sortId: {sortId}")
+        sortIdHash = hashlib.sha256(sortId.encode()).hexdigest()
 
         expiresAt = self._get_expires_at()
         logging.debug(f"expiresAt: {expiresAt}")
 
         item = {
             "itemId": itemId,
-            "sortKey": sortId,
+            "sortKey": sortIdHash,
             "transaction_hash": transaction_hash,
             "metadata": metadata,
             "expiresAt": expiresAt
@@ -142,19 +193,33 @@ class DynamoUtils:
         self._put_item(dynamo, item)
 
     def _query_items(self, dynamo, itemId, sortKey=None):
-        if sortKey:
-            response = dynamo.query(KeyConditionExpression='itemId = :id AND sortKey = :sid',
-                                    ExpressionAttributeValues={
-                                        ':id': itemId,
-                                        ':sid': sortKey
-                                    }
-                                    )
-        else:
-            response = dynamo.query(KeyConditionExpression='itemId = :id',
-                                    ExpressionAttributeValues={
-                                        ':id': itemId,
-                                    }
-                                    )
+        response = None
+        try:
+            if sortKey:
+                response = dynamo.query(KeyConditionExpression='itemId = :id AND sortKey = :sid',
+                                        ExpressionAttributeValues={
+                                            ':id': itemId,
+                                            ':sid': sortKey
+                                        }
+                                        )
+            else:
+                response = dynamo.query(KeyConditionExpression='itemId = :id',
+                                        ExpressionAttributeValues={
+                                            ':id': itemId,
+                                        }
+                                        )
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == 'ValidationException':
+                logging.error(f"ValidationException when calling the Query operation: {str(e)}")
+                Utils.ERROR_CACHE.add(Utils.alert_error(f'dynamo_utils._query_items ValidationException (SORT_KEY: {str(sortKey)}, ITEM_ID: {itemId})', "dynamo_utils._query_items", ""))
+            else:
+                logging.error(f"Error querying items in dynamoDB: {str(e)}")
+                Utils.ERROR_CACHE.add(Utils.alert_error(f'dynamo_utils._query_items Other Exception (SORT_KEY: {str(sortKey)}, ITEM_ID: {itemId})', "dynamo_utils._query_items", ""))
+            return []
+
+        if response is None:
+            return []
+
         return response.get('Items', [])
 
     def read_entity_clusters(self, dynamo, address: str) -> dict:
@@ -162,8 +227,9 @@ class DynamoUtils:
         itemId = f"{self.tag}|{self.chain_id}|entity_cluster"
         sortKey = f"{address}"
         logging.debug(f"Reading entity clusters for address {address}, itemId {itemId}")
+        sortIdHash = hashlib.sha256(sortKey.encode()).hexdigest()
 
-        items = self._query_items(dynamo, itemId, sortKey)
+        items = self._query_items(dynamo, itemId, sortIdHash)
 
         logging.debug(f"Items retrieved: {len(items)}")
         for item in items:
@@ -202,8 +268,9 @@ class DynamoUtils:
         alert_data = pd.DataFrame()
         itemId = f"{self.tag}|{self.chain_id}|alert"
         sortKey = f"{cluster}"
+        sortIdHash = hashlib.sha256(sortKey.encode()).hexdigest()
         
-        items = self._query_items(dynamo, itemId, sortKey)
+        items = self._query_items(dynamo, itemId, sortIdHash)
 
         logging.debug(f"Items retrieved: {len(items)}")
         for item in items:
