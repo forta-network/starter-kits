deddbb8ffd2cd39ddd31bf0d2710d2d35fb94576
Vxatz <vxatzistratis@gmail.com>
2023-11-07 11:31:30 +0200

    feat: Use Forta DB to load secrets

--- address-poisoning-py/src/blockexplorer.py
+++ address-poisoning-py/src/blockexplorer.py
@@ -1,4 +1,4 @@
-from src.keys import *
+from src.storage import get_secrets
 import requests
 import logging
 import json
@@ -6,27 +6,29 @@ import json
 class BlockExplorer():
 
     def __init__(self, chain_id):
+        self.api_keys = get_secrets()
+
         if chain_id == 1:
             self.host = "https://api.etherscan.io/api"
-            self.api_key = ETHERSCAN_API_KEY
+            self.api_key = self.api_keys['apiKeys']['ETHERSCAN_TOKEN']
         elif chain_id == 137:
             self.host = "https://api.polygonscan.com/api"
-            self.api_key = POLYGON_API_KEY
+            self.api_key = self.api_keys['apiKeys']['POLYGONSCAN_TOKEN']
         elif chain_id == 56:
             self.host = "https://api.bscscan.com/api"
-            self.api_key = BSC_API_KEY
+            self.api_key = self.api_keys['apiKeys']['BSCSCAN_TOKEN']
         elif chain_id == 42161:
             self.host = "https://api.arbiscan.io/api"
-            self.api_key = ARBITRUM_API_KEY
+            self.api_key = self.api_keys['apiKeys']['ARBISCAN_TOKEN']
         elif chain_id == 10:
             self.host = "https://api-optimistic.etherscan.io/api"
-            self.api_key = OPTIMISM_API_KEY
+            self.api_key = self.api_keys['apiKeys']['OPTIMISTICSCAN_TOKEN']
         elif chain_id == 250:
             self.host = "https://api.ftmscan.com/api"
-            self.api_key = FANTOM_API_KEY
+            self.api_key = self.api_keys['apiKeys']['FTMSCAN_TOKEN']
         elif chain_id == 43114:
             self.host = "https://api.snowtrace.io/api"
-            self.api_key = AVALANCHE_API_KEY
+            self.api_key = self.api_keys['apiKeys']['SNOWTRACE_TOKEN']
 
 
     def make_token_history_query(self, address_info):
