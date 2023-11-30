cbaa1b3f37986cebe33332424496b87916369524
Vxatz <vxatzistratis@gmail.com>
2023-11-30 17:13:22 +0200

    test: Add more tests

--- positive-reputation-py/src/blockexplorer_mock.py
+++ positive-reputation-py/src/blockexplorer_mock.py
@@ -1,4 +1,4 @@
-from web3_mock import EOA_ADDRESS_NEW, EOA_ADDRESS_OLD
+from web3_mock import EOA_ADDRESS_NEW, EOA_ADDRESS_OLD, EOA_ADDRESS_CONTRACT_DEPLOYER
 from datetime import datetime, timedelta
 
 class BlockExplorerMock:
@@ -11,5 +11,13 @@ class BlockExplorerMock:
             return datetime.now()
         elif address.lower() == EOA_ADDRESS_OLD.lower():
             return datetime.now() - timedelta(days=91)
+        elif address.lower() == EOA_ADDRESS_CONTRACT_DEPLOYER.lower():
+            return datetime.now() - timedelta(days=31)
         else:
             raise ValueError('Unknown address')
+
+    def has_deployed_high_tx_count_contract(self, address: str, chain_id: int) -> bool:
+        if address.lower() == EOA_ADDRESS_CONTRACT_DEPLOYER.lower():
+            return True
+        else:
+            return False
