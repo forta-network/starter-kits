6275a933422f57b72e4cc90d18cf44242be38df1
Carlos Salort Sanchez <salort@forta.org>
2023-12-06 17:26:37 +0100

    Updated threshold based on experiment results

--- malicious-smart-contract-ml-py/src/constants.py
+++ malicious-smart-contract-ml-py/src/constants.py
@@ -1,6 +1,6 @@
 CONTRACT_SLOT_ANALYSIS_DEPTH = 20  # how many slots should be read to extract contract addresses from created contract
 
-MODEL_THRESHOLD = 0.2  # threshold for model prediction
+MODEL_THRESHOLD = 0.15  # threshold for model prediction
 SAFE_CONTRACT_THRESHOLD = 0.1  # threshold for labelling safe contract
 BYTE_CODE_LENGTH_THRESHOLD = (
     60  # ignore contracts with byte code length below this threshold
