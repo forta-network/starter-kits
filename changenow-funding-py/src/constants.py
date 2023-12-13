d34f2c92e4066dac137b8dce7c98e9041346c9ca
Tayfun Sur <tayfunsur@gmail.com>
2023-12-13 10:45:47 +0300

    Move Changenow bot into starter-kits

---
+++ changenow-funding-py/src/constants.py
@@ -0,0 +1,36 @@
+CHANGENOW_ADDRESSES = {
+    1: ['0x077d360f11d220e4d5d831430c81c26c9be7c4a4'],
+    10: ['0x0a1ce4496471867fac0ad71b785e5258993c9b33'],
+    56: ['0x975d9bd9928f398c7e01f6ba236816fa558cd94b', '0xa96be652a08d9905f15b7fbe2255708709becd09'],
+    137: ['0x3a0d24d59af3a3444dc6ef12cdb0c6e38c985288'],
+    43114: ['0xc275119660fefe4519083ea6e57cbd1b672bc020'],
+    42161: ['0xafbe66947a21960b4d8a2c8f4534983c5dcbc56f'],
+}
+
+# At the time of deployment thresholds are set to ~$150
+CHANGENOW_THRESHOLD = {
+    1: 0.09,
+    10: 0.09,
+    56: 0.5,
+    137: 115,
+    43114: 6,
+    42161: 0.09
+}
+
+PROTOCOLS = {
+    1: 'ethereum',
+    10: 'optimism',
+    56: 'binance smart chain',
+    137: 'polygon',
+    43114: 'avalanche',
+    42161: 'arbitrum'
+}
+
+CURRENCIES = {
+    1: "ETH",
+    10: "ETH",
+    56: "BSC",
+    137: "MATIC",
+    43114: "AVAX",
+    42161: "ETH"
+}
---
+++ changenow-funding-py/src/findings.py
@@ -0,0 +1,66 @@
+from forta_agent import Finding, FindingType, FindingSeverity, EntityType
+from src.constants import *
+
+class FundingChangenowFindings:
+
+    def funding_changenow(transaction, type, anomaly_score, chain_id):
+        if type=="new-eoa":
+            finding = Finding({
+                'name': 'Changenow Funding',
+                'description': f'{transaction.to} received initial funds from ChangeNOW',
+                'alert_id': 'FUNDING-CHANGENOW-NEW-ACCOUNT',
+                'type': FindingType.Info,
+                'severity': FindingSeverity.Low,
+                'protocol': PROTOCOLS[chain_id],
+                'addresses': list(transaction.addresses.keys()),
+                'metadata': {
+                    "amount funded": f"{transaction.transaction.value / 10e17} {CURRENCIES[chain_id]}",
+                    "receiving address": f"{transaction.to}",
+                    "anomaly_score": anomaly_score
+                },
+                'labels': [
+                    {
+                        'entityType': EntityType.Address,
+                        'entity': transaction.to,
+                        'label': "attacker",
+                        'confidence': 0.2
+                    },
+                    {
+                        'entityType': EntityType.Transaction,
+                        'entity': transaction.transaction.hash,
+                        'label': "attacker-funding",
+                        'confidence': 0.2
+                    },
+                ]
+            })
+        else:
+            finding = Finding({
+                'name': 'Changenow Funding',
+                'description': f'{transaction.to} received a low amount of funds from ChangeNOW',
+                'alert_id': 'FUNDING-CHANGENOW-LOW-AMOUNT',
+                'type': FindingType.Info,
+                'severity': FindingSeverity.Low,
+                'protocol': PROTOCOLS[chain_id],
+                'addresses': list(transaction.addresses.keys()),
+                'metadata': {
+                    "agent threshold": f"{CHANGENOW_THRESHOLD[chain_id]} {CURRENCIES[chain_id]}",
+                    "amount funded": f"{transaction.transaction.value / 10e17} {CURRENCIES[chain_id]}",
+                    "receiving address": f"{transaction.to}",
+                    "anomaly_score": anomaly_score
+                },
+                'labels': [
+                    {
+                        'entityType': EntityType.Address,
+                        'entity': transaction.to,
+                        'label': "attacker",
+                        'confidence': 0.2
+                    },
+                    {
+                        'entityType': EntityType.Transaction,
+                        'entity': transaction.transaction.hash,
+                        'label': "attacker-funding",
+                        'confidence': 0.2
+                    },
+                ]
+            })
+        return finding
