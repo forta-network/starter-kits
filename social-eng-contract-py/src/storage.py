d5127d03b47ba311993b4c09568737edfbaece49
Tayfun Sur <tayfunsur@gmail.com>
2024-01-19 14:04:33 +0300

    Refactor with async await

--- social-eng-contract-py/src/storage.py
+++ social-eng-contract-py/src/storage.py
@@ -9,18 +9,18 @@ test_mode = "main" if 'FORTA_ENV' in os.environ and 'production' in os.environ.g
     'FORTA_ENV') else "test"
 
 
-def _token():
-    tk = forta_agent.fetch_jwt({})
+async def _token():
+    tk = await forta_bot.fetch_jwt({})
     return {"Authorization": f"Bearer {tk}"}
 
 
-def _load_json(key: str) -> object:
+async def _load_json(key: str) -> object:
     if test_mode == "test":
         # loading json from local file secrets.json
         with open("secrets.json") as f:
             return json.load(f)
     else:
-        res = requests.get(f"{owner_db}{key}", headers=_token())
+        res = await requests.get(f"{owner_db}{key}", headers=await _token())
         if res.status_code == 200:
             return res.json()
         else:
@@ -28,5 +28,5 @@ def _load_json(key: str) -> object:
                 f"error loading json from owner db: {res.status_code}, {res.text}")
 
 
-def get_secrets():
-    return _load_json("secrets.json")
+async def get_secrets():
+    return await _load_json("secrets.json")
