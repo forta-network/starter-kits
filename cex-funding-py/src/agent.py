84a30a2ff012f3e78d5674ae7a1ceac22065eca2
Roberto Cantu <Roberto_Cantu@live.com>
2024-02-05 16:58:06 -0600

    Refac: Logic update to run in prod (locally)

--- cex-funding-py/src/agent.py
+++ cex-funding-py/src/agent.py
@@ -1,44 +1,48 @@
+import asyncio
+from web3 import Web3, AsyncWeb3
 from os import environ
-import forta_agent
-from functools import lru_cache
+from forta_bot import scan_ethereum, scan_base, TransactionEvent, get_chain_id, run_health_check
+from async_lru import alru_cache
 from hexbytes import HexBytes
-from forta_agent import get_json_rpc_url
-from src.constants import CEXES
-from web3 import Web3
+from constants import CEXES
 
-from src.findings import CEXFundingFinding
-from src.storage import get_secrets
-
-SECRETS_JSON = get_secrets()
-
-web3 = Web3(Web3.HTTPProvider(get_json_rpc_url()))
+from findings import CEXFundingFinding
+from storage import get_secrets
 
 
-def initialize():
+async def initialize():
+    SECRETS_JSON = await get_secrets()
     environ["ZETTABLOCK_API_KEY"] = SECRETS_JSON['apiKeys']['ZETTABLOCK']
 
-@lru_cache(maxsize=1_000_000)
-def is_contract(w3, address) -> bool:
+@alru_cache(maxsize=1_000_000)
+async def is_contract(w3, address) -> bool:
     """
     this function determines whether address is a contract
     :return: is_contract: bool
     """
     if address is None:
         return True
-    code = w3.eth.get_code(Web3.toChecksumAddress(address))
+    code = await w3.eth.get_code(Web3.to_checksum_address(address))
     return code != HexBytes("0x")
 
 
-def detect_cex_funding(
-    w3, transaction_event: forta_agent.transaction_event.TransactionEvent
-) -> list:
+async def is_new_account(w3, address, block_number):
+    if address is None:
+        return True
+    return await w3.eth.get_transaction_count(Web3.to_checksum_address(address), block_number) == 0
+
+
+async def detect_cex_funding(w3, transaction_event: TransactionEvent) -> list:
     findings = []
 
     # alert on funding tx from CEXes
     value = transaction_event.transaction.value
-    if w3.eth.get_transaction_count(Web3.toChecksumAddress(transaction_event.transaction.to), transaction_event.block.number,) == 0 and not is_contract(w3, transaction_event.transaction.to):
+    is_new_acc = await is_new_account(w3, transaction_event.to, transaction_event.block_number)
+    is_contr = await is_contract(w3, transaction_event.transaction.to)
+
+    if is_new_acc and not is_contr:
         for chainId, chain_data in CEXES.items():
-            if chainId == w3.eth.chainId:
+            if chainId == get_chain_id():
                 threshold = chain_data["threshold"]
                 for address, name in chain_data["exchanges"]:
                     if address.lower() == transaction_event.transaction.from_ and value < threshold:
@@ -51,20 +55,28 @@ def detect_cex_funding(
 
     return findings
 
+async def handle_transaction(transaction_event: TransactionEvent, web3: AsyncWeb3.AsyncHTTPProvider):
+    return await detect_cex_funding(web3, transaction_event)
 
-def provide_handle_transaction(w3):
-    def handle_transaction(
-        transaction_event: forta_agent.transaction_event.TransactionEvent,
-    ) -> list:
-        return detect_cex_funding(w3, transaction_event)
+async def main():
+    SECRETS_JSON = await get_secrets()
+    environ["ZETTABLOCK_API_KEY"] = SECRETS_JSON['apiKeys']['ZETTABLOCK']
+    
+    await asyncio.gather(
+        scan_ethereum({
+            'rpc_url': "https://eth-mainnet.g.alchemy.com/v2",
+            'rpc_key_id': "e698634d-79c2-44fe-adf8-f7dac20dd33c",
+            'local_rpc_url': "1",
+            'handle_transaction': handle_transaction
+        }),
+        scan_base({
+            'rpc_url': "https://base-mainnet.g.alchemy.com/v2",
+            'rpc_key_id': "1d3097d9-6e44-4ca7-a61b-2209a85d262f",
+            'local_rpc_url': "8453",
+            'handle_transaction': handle_transaction
+        }),
+        run_health_check()
+    )
 
-    return handle_transaction
-
-
-real_handle_transaction = provide_handle_transaction(web3)
-
-
-def handle_transaction(
-    transaction_event: forta_agent.transaction_event.TransactionEvent,
-):
-    return real_handle_transaction(transaction_event)
+if __name__ == "__main__":
+    asyncio.run(main())
--- cex-funding-py/src/findings.py
+++ cex-funding-py/src/findings.py
@@ -1,11 +1,11 @@
 from bot_alert_rate import calculate_alert_rate, ScanCountType
-from forta_agent import Finding, FindingSeverity, FindingType, EntityType
+from forta_bot import Finding, FindingSeverity, FindingType, EntityType
 
 BOT_ID = "0xf496e3f522ec18ed9be97b815d94ef6a92215fc8e9a1a16338aee9603a5035fb"
 
 
 class CEXFundingFinding:
-    def __init__(self, name: str, to: str, value: int, chain_id: str):
+    def __init__(self, name: str, to: str, value: int, chain_id: str, txn_hash: str):
         # Set alert_id based on the name
         if name in ["eXch", "ChangeNOW", "FixedFloat"]:
             self.alert_id = "CEX-FUNDING-1"
@@ -15,6 +15,7 @@ class CEXFundingFinding:
         self.to = to
         self.value = value
         self.chain_id = chain_id
+        self.txn_hash = txn_hash
 
     def emit_finding(self) -> Finding:
         labels = [
@@ -32,15 +33,19 @@ class CEXFundingFinding:
             "value": self.value,
         }
 
-        if self.chain_id not in [43114, 10, 250]:
-            metadata["anomaly_score"] = (
-                calculate_alert_rate(
+        source = {
+            "chains": [{"chainId": self.chain_id}],
+            "transactions": [{"chainId": self.chain_id, "hash": self.txn_hash}]
+        }
+
+        if self.chain_id not in [43114, 10, 250, 8453]:
+            anomaly_score = calculate_alert_rate(
                     self.chain_id,
                     BOT_ID,
                     self.alert_id,
                     ScanCountType.TRANSFER_COUNT,
                 )
-            )
+            metadata["anomaly_score"] = str(anomaly_score)
 
         return Finding(
             {
@@ -50,6 +55,7 @@ class CEXFundingFinding:
                 "type": FindingType.Suspicious,
                 "severity": FindingSeverity.Low,
                 "metadata": metadata,
+                'source': source,
                 "labels": labels,
             }
         )
