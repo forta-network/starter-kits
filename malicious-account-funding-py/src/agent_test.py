3ce89d99543dfe84aee349cc4d84917cbe3eb617
Tayfun Sur <tayfunsur@gmail.com>
2024-02-06 16:36:21 +0300

    Amend testing to comply with v2

--- malicious-account-funding-py/src/agent_test.py
+++ malicious-account-funding-py/src/agent_test.py
@@ -1,6 +1,8 @@
-from forta_agent import FindingSeverity, create_transaction_event
+from forta_bot import FindingSeverity, create_transaction_event
 from web3_mock import Web3Mock
+from unittest.mock import patch
 import agent
+import pytest
 
 EOA_ADDRESS = "0x0000000000000000000000000000000000000001"
 KNOWN_MALICIOUS_ACCOUNT = "0x000000000532b45f47779fce440748893b257865"
@@ -9,46 +11,50 @@ w3 = Web3Mock()
 
 
 class TestKnownMaliciousAccountFunding:
-    def test_funding(self):
-        agent.initialize()
+    @pytest.mark.asyncio
+    @patch('agent.get_chain_id', return_value=1)
+    async def test_funding(self, mocker):
+        await agent.initialize()
 
-        tx_event = create_transaction_event(
-            {
-                "transaction": {
-                    "hash": "0",
-                    "from": KNOWN_MALICIOUS_ACCOUNT,
-                    "value": 10,
-                    "to": EOA_ADDRESS,
-                },
-                "block": {"number": 0},
-                "logs": [{}],
-                "receipt": {"logs": []},
-            }
+        tx_event = create_transaction_event (
+            transaction= {
+                "hash": "0",
+                "from": KNOWN_MALICIOUS_ACCOUNT,
+                "value": 10,
+                "to": EOA_ADDRESS,
+            },
+            block={
+                'number': 0
+            },
+            network_id=1,
+            logs= []
         )
 
-        findings = agent.detect_funding(w3, tx_event)
+        findings = await agent.detect_funding(tx_event)
         assert len(findings) == 1, "this should have triggered a finding"
         assert findings[0].alert_id == "MALICIOUS-ACCOUNT-FUNDING"
         assert findings[0].severity == FindingSeverity.High
 
-    def test_funding_novalue(self):
-        agent.initialize()
+    @pytest.mark.asyncio
+    @patch('agent.get_chain_id', return_value=1)
+    async def test_funding_novalue(self, mocker):
+        await agent.initialize()
 
-        tx_event = create_transaction_event(
-            {
-                "transaction": {
-                    "hash": "0",
-                    "from": KNOWN_MALICIOUS_ACCOUNT,
-                    "value": 0,
-                    "to": EOA_ADDRESS,
-                },
-                "block": {"number": 0},
-                "logs": [{}],
-                "receipt": {"logs": []},
-            }
+        tx_event = create_transaction_event (
+            transaction= {
+                "hash": "0",
+                "from": KNOWN_MALICIOUS_ACCOUNT,
+                "value": 0,
+                "to": EOA_ADDRESS,
+            },
+            block={
+                'number': 0
+            },
+            network_id=1,
+            logs= []
         )
 
-        findings = agent.detect_funding(w3, tx_event)
+        findings = await agent.detect_funding(tx_event)
         assert (
             len(findings) == 0
         ), "this should have not triggered a finding as no funds were transferred"
