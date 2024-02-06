bb41fcbcc833e0396028de27c2b300659fd419bf
Roberto Cantu <Roberto_Cantu@live.com>
2024-02-06 14:53:30 -0600

    Test: Update Latency test logic

--- changenow-funding-py/src/agent_test.py
+++ changenow-funding-py/src/agent_test.py
@@ -1,102 +1,96 @@
 import agent
-from forta_bot import create_transaction_event, FindingSeverity, get_provider
+from forta_bot import create_transaction_event, FindingSeverity
 from unittest.mock import patch
 import pytest
-from web3 import Web3
+from web3 import AsyncWeb3
 from src.web3_mock import Web3Mock, NEW_EOA, OLD_EOA, NEW_CONTRACT
-import timeit
+import time
+from forta_agent import get_json_rpc_url
 
 w3 = Web3Mock()
+real_w3 = AsyncWeb3(AsyncWeb3.AsyncHTTPProvider(get_json_rpc_url()))
+
+async def async_timeit(func, *args, **kwargs):
+    start_time = time.time()
+    await func(*args, **kwargs)
+    end_time = time.time()
+    return (end_time - start_time) * 1000  # Return time in milliseconds
 
 class TestChangeNowFundingAgent:
 
-    # @pytest.mark.asyncio
-    # async def test_agent_latency(self):
+    @pytest.mark.asyncio
+    @patch('agent.get_chain_id', return_value=1)
+    async def test_agent_latency(self, mocker):
+        global real_w3
+        agent.initialize()
 
-    #     global real_w3
-    #     eth_provider = await get_provider({
-    #         'rpc_url': "https://eth-mainnet.g.alchemy.com/v2",
-    #         'rpc_key_id': "e698634d-79c2-44fe-adf8-f7dac20dd33c",
-    #         'local_rpc_url': "1",
-    #     })
-    #     real_w3 = Web3(Web3.HTTPProvider(eth_provider))
+        # Chain: Blocktime, Number of Tx -> Avg processing time in ms target
+        # Ethereum: 12s, 150 -> 80ms
+        # BSC: 3s, 70 -> 43ms
+        # Polygon: 2s, 50 -> 40ms
 
-    #     agent.initialize()
-
-    #     # Chain: Blocktime, Number of Tx in block -> Avg processing time in ms target
-    #     # Ethereum: 12s, 150 -> 80ms
-    #     # Base: 2s, 12 -> 167ms
-
-    #     # Ethereum: 12s, 150 -> 80ms - 2
-    #     # Base: 2s, 12 -> 167ms - N
+        # Ethereum: 12s, 150 -> 80ms - 2
+        # BSC: 3s, 70 -> 43ms - 3
+        # Polygon: 2s, 50 -> 40ms - 4
         
-    #     # we're assuming 1% of tx will contain a tc funding event, but only 50% of tx are funding
-    #     # so our target for polygon is 1 tx with a tc funding and 49 without tc funding and 50% are no funding
+        # we're assuming 1% of tx will contain a tc funding event, but only 50% of tx are funding
+        # so our target for polygon is 1 tx with a tc funding and 49 without tc funding and 50% are no funding
 
-    #     global transaction_with_no_funding
-    #     transaction_with_no_funding = create_transaction_event(
-    #         transaction = {
-    #             'hash': "0x4a419b16152cc6513db84bdfb94818827ef8e64c2bc52f9a95f960c47ef02817",
-    #             'from': "0x5b7d833a4aa182bddd754db797d43d3c29c171a0",
-    #             'value': 100,
-    #             'to': "0xb2880739e3bd3f535d38760cd8f2ee058737341b",
-    #         },
-    #         block = {
-    #             'number': 1
-    #         },
-    #         network_id = 1
-    #     )
+        global transaction_with_no_funding
+        transaction_with_no_funding = create_transaction_event(
+            transaction = {
+                'hash': "0x4a419b16152cc6513db84bdfb94818827ef8e64c2bc52f9a95f960c47ef02817",
+                'from': "0x5b7d833a4aa182bddd754db797d43d3c29c171a0",
+                'value': 100,
+                'to': "0xb2880739e3bd3f535d38760cd8f2ee058737341b",
+            },
+            block = {
+                'number': 1
+            },
+            network_id = 1
+        )
 
-    #     global transaction_from_changenow_to_contract
-    #     transaction_from_changenow_to_contract = create_transaction_event(
-    #         transaction = {
-    #             'hash': "0x4a419b16152cc6513db84bdfb94818827ef8e64c2bc52f9a95f960c47ef02817",
-    #             'from': "0x077d360f11d220e4d5d831430c81c26c9be7c4a4",
-    #             'value': 100,
-    #             'to': "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
-    #         },
-    #         block = {
-    #             'number': 1
-    #         },
-    #         network_id = 1
-    #     )
+        global transaction_from_changenow_to_contract
+        transaction_from_changenow_to_contract = create_transaction_event(
+            transaction = {
+                'hash': "0x4a419b16152cc6513db84bdfb94818827ef8e64c2bc52f9a95f960c47ef02817",
+                'from': "0x077d360f11d220e4d5d831430c81c26c9be7c4a4",
+                'value': 100,
+                'to': "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
+            },
+            block = {
+                'number': 1
+            },
+            network_id = 1
+        )
 
-    #     global transaction_with_changenow_funding
-    #     transaction_with_changenow_funding = create_transaction_event(
-    #         transaction = {
-    #             'hash': "0xc4af34bc84bbdda599ccf915a9c6cb62481899086767480212a4b28f4636c0f7",
-    #             'from': "0x077d360f11d220e4d5d831430c81c26c9be7c4a4",
-    #             'value': 1381970000000000,
-    #             'to': "0xfea89fe5d3e0d24a1f219a33e3fd3b1831cd171b",
-    #         },
-    #         block = {
-    #             'number': 1
-    #         },
-    #         network_id = 1
-    #     )
+        global transaction_with_changenow_funding
+        transaction_with_changenow_funding = create_transaction_event(
+            transaction = {
+                'hash': "0xc4af34bc84bbdda599ccf915a9c6cb62481899086767480212a4b28f4636c0f7",
+                'from': "0x077d360f11d220e4d5d831430c81c26c9be7c4a4",
+                'value': 1381970000000000,
+                'to': "0xfea89fe5d3e0d24a1f219a33e3fd3b1831cd171b",
+            },
+            block = {
+                'number': 1
+            },
+            network_id = 1
+        )
 
-    #     processing_runs = 10
+        processing_runs = 10
+        processing_time_no_funding = await async_timeit(agent.detect_changenow_funding, real_w3, transaction_with_no_funding) / processing_runs
+        processing_time_changenow_to_contract = await async_timeit(agent.detect_changenow_funding, real_w3, transaction_from_changenow_to_contract) / processing_runs
+        processing_time_with_changenow_funding = await async_timeit(agent.detect_changenow_funding, real_w3, transaction_with_changenow_funding) / processing_runs
 
-    #     processing_time_no_funding = (await timeit.timeit(
-    #         'agent.detect_changenow_funding(real_w3, transaction_with_no_funding)', number=processing_runs, globals=globals()
-    #     )) * 1000 / processing_runs
+        eth_shard_count = 10
+        polygon_shard_count = 17
+        bsc_shard_count = 15
+        avg_processing_time = (processing_time_no_funding * 0.90 + processing_time_changenow_to_contract * 0.09 + processing_time_with_changenow_funding * 0.01)
 
-    #     processing_time_changenow_to_contract = (await timeit.timeit(
-    #         'agent.detect_changenow_funding(real_w3, transaction_from_changenow_to_contract)', number=processing_runs, globals=globals()
-    #     )) * 1000 / processing_runs
-
-    #     processing_time_with_changenow_funding = (await timeit.timeit(
-    #         'agent.detect_changenow_funding(real_w3, transaction_with_changenow_funding)', number=processing_runs, globals=globals()
-    #     )) * 1000 / processing_runs
-
-    #     eth_shard_count = 10
-    #     polygon_shard_count = 17
-    #     bsc_shard_count = 15
-    #     avg_processing_time = (processing_time_no_funding * 0.90 + processing_time_changenow_to_contract * 0.09 + processing_time_with_changenow_funding * 0.01)
-
-    #     assert (avg_processing_time/eth_shard_count) < 80, f"ETH -> Avg processing: {avg_processing_time}, Avg processing per shard: {avg_processing_time/eth_shard_count}"
-    #     assert (avg_processing_time/polygon_shard_count) < 43, f"MATIC -> Avg processing: {avg_processing_time}, Avg processing per shard: {avg_processing_time/polygon_shard_count}"
-    #     assert (avg_processing_time/bsc_shard_count) < 40, f"BSC -> Avg processing: {avg_processing_time}, Avg processing per shard: {avg_processing_time/bsc_shard_count}"
+        assert (avg_processing_time/eth_shard_count) < 80, f"ETH -> Avg processing: {avg_processing_time}, Avg processing per shard: {avg_processing_time/eth_shard_count}"
+        assert (avg_processing_time/polygon_shard_count) < 43, f"MATIC -> Avg processing: {avg_processing_time}, Avg processing per shard: {avg_processing_time/polygon_shard_count}"
+        assert (avg_processing_time/bsc_shard_count) < 40, f"BSC -> Avg processing: {avg_processing_time}, Avg processing per shard: {avg_processing_time/bsc_shard_count}"
     
     @pytest.mark.asyncio
     @patch('agent.get_chain_id', return_value=1)
