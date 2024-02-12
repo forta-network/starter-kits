f0259d3839c42245f857f87cd3fbffd514fa84f4
Roberto Cantu <Roberto_Cantu@live.com>
2024-02-12 09:37:27 -0600

    Refac: Update logic for v2 SDK

--- role-change-detection-py/src/agent.py
+++ role-change-detection-py/src/agent.py
@@ -1,17 +1,20 @@
+import asyncio
 import logging
 import sys
 
-from forta_agent import get_json_rpc_url, Web3, Finding, FindingSeverity, FindingType, EntityType
+# from forta_agent import get_json_rpc_url, Web3, Finding, FindingSeverity, FindingType, EntityType
+from forta_bot import scan_ethereum, scan_base, TransactionEvent, get_chain_id, run_health_check, Finding, FindingSeverity, FindingType, EntityType
+from web3 import Web3, AsyncWeb3
 from hexbytes import HexBytes
-from src.constants import *
-from src.blockexplorer import *
+from constants import *
+from blockexplorer import *
 
 
 # Initialize web3
-web3 = Web3(Web3.HTTPProvider(get_json_rpc_url()))
+# web3 = Web3(Web3.HTTPProvider(get_json_rpc_url()))
 
 # Replace with blockexplorer instance
-blockexplorer = BlockExplorer(web3.eth.chain_id)
+blockexplorer = BlockExplorer(get_chain_id())
 
 # Logging set up.
 root = logging.getLogger()
@@ -25,7 +28,7 @@ root.addHandler(handler)
 DENOMINATOR_COUNT = 0
 ALERT_COUNT = 0
 
-def initialize():
+async def initialize():
     """
     Initialize variables for anomaly score.
     """
@@ -35,19 +38,21 @@ def initialize():
     global ALERT_COUNT
     ALERT_COUNT = 0
 
+    await blockexplorer.set_api_key()
 
-def is_contract(w3, address):
+
+async def is_contract(w3, address):
     """
     this function determines whether address is a contract
     :return: is_contract: bool
     """
     if address is None:
         return True
-    code = w3.eth.get_code(Web3.toChecksumAddress(address))
+    code = await w3.eth.get_code(Web3.to_checksum_address(address))
     return code != HexBytes('0x')
 
 
-def detect_role_change(w3, blockexplorer, transaction_event):
+async def detect_role_change(w3, blockexplorer, transaction_event):
     """
     search transaction input when to is a contract for key words indicating a function call triggering a role change
     :return: detect_role_change: Finding
@@ -57,23 +62,25 @@ def detect_role_change(w3, blockexplorer, transaction_event):
 
     findings = []
 
-    if is_contract(w3, transaction_event.to):
+    if await is_contract(w3, transaction_event.to):
         DENOMINATOR_COUNT += 1
         try:
-            abi = blockexplorer.get_abi(transaction_event.to)
+            abi = await blockexplorer.get_abi(transaction_event.to)
             if abi == None:
-                logging.warn(f"Unable to retrieve ABI for {transaction_event.to}")
+                logging.warning(f"Unable to retrieve ABI for {transaction_event.to}")
                 return findings
         except Exception:
             logging.warn(f"Unable to retrieve ABI for {transaction_event.to}")
             return findings
-        contract = w3.eth.contract(address=Web3.toChecksumAddress(transaction_event.to), abi=abi)
-        transaction = w3.eth.get_transaction(transaction_event.hash)
+        contract = w3.eth.contract(address=Web3.to_checksum_address(transaction_event.to), abi=abi)
+        transaction = await w3.eth.get_transaction(transaction_event.hash)
+        print(f"transaction: {transaction}")
         try:
             transaction_data = contract.decode_function_input(transaction.input)
+            print(f"transaction_data: {transaction_data}")
             function_call = str(transaction_data[0])[10:-1]
         except Exception as e:
-            logging.warn(f"Failed to decode tx input: {e}")
+            logging.warning(f"Failed to decode tx input: {e}")
             return findings
         matching_keywords = []
         for keyword in ROLE_CHANGE_KEYWORDS:
@@ -91,7 +98,7 @@ def detect_role_change(w3, blockexplorer, transaction_event):
                     "metadata": {
                         "matching keywords": matching_keywords,
                         "function signature": str(transaction_data[0])[10:-1],
-                        "anomaly_score": (1.0 * ALERT_COUNT) / DENOMINATOR_COUNT
+                        "anomaly_score": str((1.0 * ALERT_COUNT) / DENOMINATOR_COUNT)
                     },
                     "labels": [
                         {
@@ -119,15 +126,28 @@ def detect_role_change(w3, blockexplorer, transaction_event):
     return findings
 
 
-def provide_handle_transaction(w3, blockexplorer):
-    def handle_transaction(transaction_event):
-        return detect_role_change(w3, blockexplorer, transaction_event)
+async def handle_transaction(transaction_event: TransactionEvent, web3: AsyncWeb3.AsyncHTTPProvider):
+    return await detect_role_change(web3, blockexplorer, transaction_event)
 
-    return handle_transaction
+async def main():
+    await initialize()
+    
+    await asyncio.gather(
+        scan_ethereum({
+            'rpc_url': "https://eth-mainnet.g.alchemy.com/v2",
+            'rpc_key_id': "e698634d-79c2-44fe-adf8-f7dac20dd33c",
+            'local_rpc_url': "1",
+            'handle_transaction': handle_transaction
+        }),
+        # NOTE: Currently don't have BaseScan API
+        # scan_base({
+        #     'rpc_url': "https://base-mainnet.g.alchemy.com/v2",
+        #     'rpc_key_id': "1d3097d9-6e44-4ca7-a61b-2209a85d262f",
+        #     'local_rpc_url': "8453",
+        #     'handle_transaction': handle_transaction
+        # }),
+        run_health_check()
+    )
 
-
-real_handle_transaction = provide_handle_transaction(web3, blockexplorer)
-
-
-def handle_transaction(transaction_event):
-    return real_handle_transaction(transaction_event)
+if __name__ == "__main__":
+    asyncio.run(main())
--- role-change-detection-py/src/blockexplorer.py
+++ role-change-detection-py/src/blockexplorer.py
@@ -1,50 +1,73 @@
 import json
 import logging
 import requests
+import aiohttp
 
-from src.storage import get_secrets
+from storage import get_secrets
 
 
 class BlockExplorer:
     api_key = ""
     host = ""
+    chain_id = None
     SECRETS_JSON = None
 
     def __init__(self, chain_id):
-        if BlockExplorer.SECRETS_JSON is None:
-            BlockExplorer.SECRETS_JSON = get_secrets()
+        self.chain_id = chain_id
 
-        if chain_id == 1:
+        if self.chain_id == 1:
             self.host = "https://api.etherscan.io"
-            self.api_key = BlockExplorer.SECRETS_JSON['apiKeys']['ETHERSCAN_TOKEN']
-        elif chain_id == 137:
+        elif self.chain_id == 137:
             self.host = "https://api.polygonscan.com"
-            self.api_key = BlockExplorer.SECRETS_JSON['apiKeys']['POLYGONSCAN_TOKEN']
-        elif chain_id == 56:
+        elif self.chain_id == 56:
             self.host = "https://api.bscscan.com"
-            self.api_key = BlockExplorer.SECRETS_JSON['apiKeys']['BSCSCAN_TOKEN']
-        elif chain_id == 42161:
+        elif self.chain_id == 42161:
             self.host = "https://api.arbiscan.io"
-            self.api_key = BlockExplorer.SECRETS_JSON['apiKeys']['ARBISCAN_TOKEN']
-        elif chain_id == 10:
+        elif self.chain_id == 10:
             self.host = "https://api-optimistic.etherscan.io"
-            self.api_key = BlockExplorer.SECRETS_JSON['apiKeys']['OPTIMISTICSCAN_TOKEN']
-        elif chain_id == 250:
+        elif self.chain_id == 250:
             self.host = "https://api.ftmscan.com"
-            self.api_key = BlockExplorer.SECRETS_JSON['apiKeys']['FTMSCAN_TOKEN']
-        elif chain_id == 43114:
+        elif self.chain_id == 43114:
             self.host = "https://api.snowtrace.io"
+        elif self.chain_id == 8453:
+            self.host = "https://api.basescan.org"
+
+    async def set_api_key(self):
+        if BlockExplorer.SECRETS_JSON is None:
+            BlockExplorer.SECRETS_JSON = await get_secrets()
+
+        if self.chain_id == 1:
+            self.api_key = BlockExplorer.SECRETS_JSON['apiKeys']['ETHERSCAN_TOKEN']
+        elif self.chain_id == 137:
+            self.api_key = BlockExplorer.SECRETS_JSON['apiKeys']['POLYGONSCAN_TOKEN']
+        elif self.chain_id == 56:
+            self.api_key = BlockExplorer.SECRETS_JSON['apiKeys']['BSCSCAN_TOKEN']
+        elif self.chain_id == 42161:
+            self.api_key = BlockExplorer.SECRETS_JSON['apiKeys']['ARBISCAN_TOKEN']
+        elif self.chain_id == 10:
+            self.api_key = BlockExplorer.SECRETS_JSON['apiKeys']['OPTIMISTICSCAN_TOKEN']
+        elif self.chain_id == 250:
+            self.api_key = BlockExplorer.SECRETS_JSON['apiKeys']['FTMSCAN_TOKEN']
+        elif self.chain_id == 43114:
             self.api_key = BlockExplorer.SECRETS_JSON['apiKeys']['SNOWTRACE_TOKEN']
+        elif self.chain_id == 8453:
+            self.api_key = BlockExplorer.SECRETS_JSON['apiKeys']['BASESCAN_TOKEN']
 
 
-    def get_abi(self, address):
+    async def get_abi(self, address):
         url = self.host + "/api?module=contract&action=getabi&address=" + address + "&apikey=" + self.api_key
         response = requests.get(url)
-        if (response.status_code == 200):
-            data = response.json()
-            if data['status'] == '1':
-                abi = data['result']
-                return abi
-        else:
-            logging.warn("Unable to retrieve ABI. Etherscan returned status code " + str(response.status_code))
-            pass
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as res:
+                if res.status == 200:
+                    try:
+                        # Attempt to parse the response as JSON regardless of the Content-Type
+                        data = await res.json(content_type=None)
+                        if data['status'] == '1':
+                            abi = data['result']
+                            return abi
+                    except json.JSONDecodeError:
+                        raise Exception("Failed to decode JSON response")
+                else:
+                    logging.warn("Unable to retrieve ABI. Etherscan returned status code " + str(response.status_code))
+                    pass
