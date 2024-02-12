b47a5fc93bf9f23b432d78979db90563812f4020
Poupou <108153089+poupou-web3@users.noreply.github.com>
2024-02-12 21:39:30 +0100

    Expand the entity clustering bot to emit findings and cluster highly suspicious contract creation

--- entity-cluster-bot/src/constants.py
+++ entity-cluster-bot/src/constants.py
@@ -9,16 +9,16 @@ GRAPH_KEY = "graph_key"
 NEW_FUNDED_MAX_NONCE = 1
 NEW_FUNDED_MAX_WEI_TRANSFER_THRESHOLD = 1000000000000000000 # 1 ETH
 
-S3_BUCKET= "prod-research-bot-data"
-S3_REGION="us-east-1"
+S3_BUCKET = "forta-entity-clustering"
+S3_REGION = "eu-west-1"
 
-DYNAMO_TABLE= "prod-research-bot-data"
-TEST_TAG= "test"
-PROD_TAG= "prod"
-DYNAMO_REGION="us-east-1"
-DYNAMODB_PRIMARY_KEY = 'itemId'
-DYNAMODB_SORT_KEY = 'sortKey'
-DYNAMODB_TTL_KEY = 'expiresAt'
+DYNAMO_TABLE = "forta-entity-bot-dynamo-db"
+TEST_TAG = "test"  # ??
+PROD_TAG = "test"  # ?? has to be test for now
+DYNAMO_REGION = "eu-west-1"
+DYNAMODB_PRIMARY_KEY = "itemId"
+DYNAMODB_SORT_KEY = "sortKey"
+DYNAMODB_TTL_KEY = "expiresAt"
 
 # if True  it will profile every transaction and dump it to entity_cluster_prof_stats file so can be viewed with snakeviz
 PROFILING = False
@@ -28,5 +28,10 @@ TX_SAVE_STEP = 150*6
 # Timeout for w3 calls in seconds 
 HTTP_RPC_TIMEOUT = 2
 # timeout of the lock in the mutex db 10s
-MUTEX_TIMEOUT_MILLIS=10*10000
+MUTEX_TIMEOUT_MILLIS = 10 * 10000
+MALICIOUS_SMART_CONTRACT_BOT_ID = (
+    "0x9aaa5cd64000e8ba4fa2718a467b90055b70815d60351914cc1cbe89fe1c404c"
+)
+SEVERITY_ALERT_FILTER = "HIGH"
 
+CLUSTER_SENDER = True
