55c73f15cf3348c2f429bee8debb408c069ac550
Vxatz <vxatzistratis@gmail.com>
2024-02-29 12:51:22 +0200

    chore: Fix indentation

--- large-transfer-out-py/src/constants.py
+++ large-transfer-out-py/src/constants.py
@@ -15,5 +15,5 @@ SWAP_TOPICS = ["0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d8
                "0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67",
                "0x19b47279256b2a23a1665c810c8d55a1758940ee09377d4f8d26497a3577dc83"]
 
-IGNORED_FUNCTION_SIGS = ["0xf305d719" # addLiquidityETH
-                         ]
+IGNORED_FUNCTION_SIGS = ["0xf305d719"] # addLiquidityETH
+                        
