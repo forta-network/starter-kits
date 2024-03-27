8596086aa3abf03e8054ae6be59d1b6996c4d296
Vxatz <vxatzistratis@gmail.com>
2024-03-27 17:41:31 +0200

    feat: push suspicious funding bot logic

---
+++ suspicious-funding-ts/README.md
@@ -0,0 +1,25 @@
+# Suspicious Funding Detector
+
+## Description
+
+This bot identifies when a new EOA receives funds from an address previously funded by Tornado Cash or Fixed Float, or from addresses flagged by the Attack Detector or this bot itself.
+
+## Supported Chains
+
+- Ethereum
+- BNB Chain
+- Fantom
+- Polygon
+- Optimism
+- Arbitrum
+- Avalanche
+
+## Alerts
+
+- FORTA-1
+  - Fired when a transaction contains a Tether transfer over 10,000 USDT
+  - Severity is always set to "Medium" (mention any conditions where it could be something else)
+  - Type is always set to "Suspicious" (mention any conditions where it could be something else)
+  - Metadata includes:
+    - `sender`: The address that sent funds
+    - `receiver`: The address that received funds
---
+++ suspicious-funding-ts/src/agent.ts
@@ -0,0 +1,95 @@
+import {
+  Finding,
+  HandleAlert,
+  AlertEvent,
+  TransactionEvent,
+  LabelQueryOptions,
+  ethers,
+  getEthersProvider,
+} from "forta-agent";
+import {
+  BOTS_TO_MONITOR,
+  DAYS_TO_LOOK_BACK,
+  VALUE_THRESHOLDS,
+} from "./constants";
+import { createFinding, getAllLabels } from "./utils";
+
+const ethersProvider = getEthersProvider();
+const attackers = new Set<string>();
+let chainId: number;
+
+export const provideInitialize =
+  (provider: ethers.providers.Provider) => async () => {
+    chainId = Number((await provider.getNetwork()).chainId);
+
+    const query: LabelQueryOptions = {
+      sourceIds: BOTS_TO_MONITOR,
+      state: true,
+      labels: ["attacker"],
+      createdSince: Date.now() - DAYS_TO_LOOK_BACK * 60 * 60 * 24 * 1000,
+      first: 2000,
+    };
+    await getAllLabels(query, attackers);
+
+    return {
+      alertConfig: {
+        subscriptions: [
+          {
+            botId: BOTS_TO_MONITOR[0],
+            alertIds: ["FUNDING-TORNADO-CASH"],
+          },
+          {
+            botId: BOTS_TO_MONITOR[1],
+            alertIds: ["FUNDING-FIXED-FLOAT-NEW-ACCOUNT"],
+          },
+          {
+            botId: BOTS_TO_MONITOR[2],
+            alertIds: ["EARLY-ATTACK-DETECTOR-1"],
+          },
+          {
+            botId: BOTS_TO_MONITOR[3],
+            alertIds: ["SUSPICIOUS-FUNDING"],
+          },
+        ],
+      },
+    };
+  };
+
+export const provideHandleTransaction =
+  (attackers: Set<string>, provider: ethers.providers.Provider) =>
+  async (txEvent: TransactionEvent) => {
+    const findings: Finding[] = [];
+
+    if (
+      txEvent.to &&
+      attackers.has(txEvent.from.toLowerCase()) &&
+      txEvent.transaction.value != "0x0" &&
+      Number(txEvent.transaction.value) / 1e18 <= VALUE_THRESHOLDS[chainId]
+    ) {
+      if (
+        (await provider.getTransactionCount(txEvent.to)) === 0 &&
+        (await provider.getCode(txEvent.to)) === "0x"
+      ) {
+        findings.push(createFinding(txEvent.from, txEvent.to));
+      }
+    }
+
+    return findings;
+  };
+
+const handleAlert: HandleAlert = async (alertEvent: AlertEvent) => {
+  const findings: Finding[] = [];
+
+  alertEvent.alert.labels?.forEach((label) => {
+    if (label.label === "attacker") {
+      attackers.add(label.entity.toLowerCase());
+    }
+  });
+  return findings;
+};
+
+export default {
+  initialize: provideInitialize(ethersProvider),
+  handleTransaction: provideHandleTransaction(attackers, ethersProvider),
+  handleAlert,
+};
