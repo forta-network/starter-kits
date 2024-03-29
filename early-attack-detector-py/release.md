ab02135fd88a771fa58c23f894fafc83bd5c915e
Carlos Salort Sanchez <salort@forta.org>
2024-03-29 10:18:23 +0100

    Added new funding sources to beta

--- early-attack-detector-py/release.md
+++ early-attack-detector-py/release.md
@@ -1,6 +1,10 @@
 # V0
+
+## V0.2.2
+- [beta] Added squid, 1-hops, and SWFT Swap funding sources 
+
 ## V0.2.1
-- Removed squid from funding sources temporarily
+- [prod] Removed squid from funding sources temporarily
 
 ## V0.2.0
 - Updated model, new threshold. Updated docker version to be more lightweight.
--- early-attack-detector-py/src/constants.py
+++ early-attack-detector-py/src/constants.py
@@ -20,10 +20,12 @@ FUNDING_BOTS = [
         '0x025251bd9b67b18804249a61a19f8dd45e3dd30caba295ed2cdc9392039f6272',  # funding union chain
         '0xd2a25c368501ec7f9cd1219858c8a53cc4da7bd64c43633658a90b54bfcc595a',  # funding railgun
         '0x2d3bb89e9cecc0024d8ae1a9c93ca42664472cb074cc200fa2c0f77f2be34bf3',  # funding fixed float
-        # '0xa5a23e3fd7a5401db47008329061bb9dca48fc0199151e043d93376d31adb40d',  # funding squid
+        '0xa5a23e3fd7a5401db47008329061bb9dca48fc0199151e043d93376d31adb40d',  # funding squid'
         '0xf2ee3554a13ee126dae179918e89010afc1bfc1ffabd3a381b529632ebf7497a',  # funding thorchain
         '0x29daabf74506f2aa4feb93fa8ec0f4ac61c4e9ba3f3190072bc680bc70e71bd7',  # funding eXch
         '0x9324d7865e1bcb933c19825be8482e995af75c9aeab7547631db4d2cd3522e0e',  # funding ChangeNow
+        '0x90596fcef715e22cc073fdc7018039e7af742276dda1baed03032411480c65fd',  # funding with hops
+        '0xf1c75d2674ecf85be5e95b4959284ce8ed758885ddd7be64923c893ceff49efe',  # SWFT swap
     ]
 FUNDING_TIME = 1  # How many days in the past to look for labels
 
