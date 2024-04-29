3e2d7b784f47e7aee4ec36dd55e3f1e04266857b
Tayfun Sur <tayfunsur@gmail.com>
2024-04-29 13:00:58 +0300

    Create RPC_ENDPOINTS

--- funding-tornado-cash-py/src/constants.py
+++ funding-tornado-cash-py/src/constants.py
@@ -14,3 +14,11 @@ TORNADO_CASH_ADDRESSES_HIGH = {1: ["0xa160cdab225685da1d56aa342ad8841c3b53f291"]
                           }
 
 TORNADO_CASH_WITHDRAW_TOPIC = '0xe9e508bad6d4c3227e881ca19068f099da81b5164dd6d62b2eaf1e8bc6c34931'
+
+RPC_ENDPOINTS = {1: ['https://rpc.ankr.com/eth', 'https://rpc.flashbots.net/', 'https://cloudflare-eth.com/',
+                     'https://nodes.mewapi.io/rpc/eth', 'https://rpc.builder0x69.io', 'https://g.w.lavanet.xyz:443/gateway/eth/rpc-http/f7ee0000000000000000000000000000',
+                     'https://endpoints.omniatech.io/v1/eth/mainnet/public', 'https://ethereum.blinklabs.xyz/', 'https://cloudflare-eth.com/v1/mainnet',
+                     'https://eth.drpc.org/', 'https://rpc.propellerheads.xyz/eth', 'https://ethereum.blockpi.network/v1/rpc/public',
+                     'https://ethereum.rpc.thirdweb.com/', 'https://mainnet.gateway.tenderly.co', 'https://rpc.builder0x69.io',
+                     'https://gateway.tenderly.co/public/mainnet', 'https://api.stateless.solutions/ethereum/v1/demo', 'https://eth1.lava.build/lava-referer-ed07f753-8c19-4309-b632-5a4a421aa589',
+                     'https://eth1.lava.build/lava-referer-16223de7-12c0-49f3-8d87-e5f1e6a0eb3b', 'https://rpc.blocknative.com/boost', 'https://rpc.flashbots.net/fast']}
