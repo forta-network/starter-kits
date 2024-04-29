620f470c01acafabf0d71481663ffdd191cf95ce
Tayfun Sur <tayfunsur@gmail.com>
2024-04-29 13:14:12 +0300

    Create RPC_ENDPOINTS

--- cex-funding-py/src/constants.py
+++ cex-funding-py/src/constants.py
@@ -92,3 +92,11 @@ CEXES = {
         ]
     }
 }
+
+RPC_ENDPOINTS = {1: ['https://rpc.ankr.com/eth', 'https://rpc.flashbots.net/', 'https://cloudflare-eth.com/',
+                     'https://rpc.builder0x69.io', 'https://g.w.lavanet.xyz:443/gateway/eth/rpc-http/f7ee0000000000000000000000000000',
+                     'https://endpoints.omniatech.io/v1/eth/mainnet/public', 'https://ethereum.blinklabs.xyz/', 'https://cloudflare-eth.com/v1/mainnet',
+                     'https://eth.drpc.org/', 'https://rpc.propellerheads.xyz/eth', 'https://ethereum.blockpi.network/v1/rpc/public',
+                     'https://ethereum.rpc.thirdweb.com/', 'https://mainnet.gateway.tenderly.co', 'https://rpc.builder0x69.io',
+                     'https://gateway.tenderly.co/public/mainnet', 'https://api.stateless.solutions/ethereum/v1/demo',
+                     'https://rpc.blocknative.com/boost', 'https://rpc.flashbots.net/fast']}
