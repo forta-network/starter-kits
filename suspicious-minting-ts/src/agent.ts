87b237748918e8632b1d8931710c49de3f01af86
Vxatz <vxatzistratis@gmail.com>
2024-05-29 13:21:16 +0300

    feat: Update heuristics

--- suspicious-minting-ts/src/agent.ts
+++ suspicious-minting-ts/src/agent.ts
@@ -28,10 +28,10 @@ export const provideHandleTransaction =
           )
       );
 
-    if (!mintEvents.length) {
+    if (!mintEvents.length || mintEvents.length > 5) {
       return findings;
     }
-    console.log("THERE WAS A MINT!!!");
+
     const receivers = mintEvents.map((event) => event.args.to);
     const uniqueReceivers = Array.from(new Set(receivers));
 
@@ -44,62 +44,178 @@ export const provideHandleTransaction =
     );
 
     const isEoaRecord = Object.fromEntries(isEoaMap);
+    const burnEvents = txEvent
+      .filterLog(ERC20_TRANSFER_EVENT)
+      .filter((event) => event.args.to === ethers.constants.AddressZero);
+    const burnOriginators = burnEvents.map((event) => event.args.from);
+    const uniqueBurnOriginators = Array.from(new Set(burnOriginators));
 
     await Promise.all(
       mintEvents.map(async (event) => {
-        if (isEoaRecord[event.args.to]) {
+        const mintRecipient = event.args.to;
+        if (
+          isEoaRecord[mintRecipient] &&
+          !uniqueBurnOriginators.includes(mintRecipient)
+        ) {
           const {
             address: token,
-            args: { value: amount, to },
+            args: { value: amount },
           } = event;
-          const usdValue = await fetcher.getValueInUsd(
-            txEvent.network,
-            amount.toString(),
-            token
+
+          // Check for transfer events from the mint recipient
+          const transferEvents = txEvent
+            .filterLog(ERC20_TRANSFER_EVENT)
+            .filter(
+              (transferEvent) => transferEvent.args.from === mintRecipient
+            );
+
+          // Check if the total transferred amount is within ±20% of the minted amount
+          const isTransferAmountWithinRange = transferEvents.some(
+            (transferEvent) => {
+              const transferAmount = transferEvent.args.value;
+              const lowerBound = amount.sub(amount.div(5));
+              const upperBound = amount.add(amount.div(5));
+
+              return (
+                transferAmount.gte(lowerBound) && transferAmount.lte(upperBound)
+              );
+            }
           );
-
-          if (usdValue > 50000) {
-            findings.push(
-              createFinding(
-                token,
-                usdValue.toFixed(2),
-                txEvent.hash,
-                to,
-                FindingSeverity.High,
-                txEvent.from
-              )
-            );
-          } else if (usdValue === 0 || usdValue > 10000) {
-            const toTxCount = await provider.getTransactionCount(
-              to,
-              txEvent.blockNumber - 1
+          if (!isTransferAmountWithinRange) {
+            const mintUsdValue = await fetcher.getValueInUsd(
+              txEvent.network,
+              amount.toString(),
+              token
             );
 
-            if (toTxCount === 0) {
-              if (usdValue > 10000) {
+            // Check if any transfer event USD value is within ±20% of the mint USD value
+            const isTransferUsdValueWithinRange = await Promise.all(
+              transferEvents.map(async (transferEvent) => {
+                const transferAmount = transferEvent.args.value;
+                const transferUsdValue = await fetcher.getValueInUsd(
+                  txEvent.network,
+                  transferAmount.toString(),
+                  transferEvent.address
+                );
+
+                if (mintUsdValue === 0 && transferUsdValue === 0) {
+                  return false;
+                }
+
+                const lowerBound = mintUsdValue * 0.8;
+                const upperBound = mintUsdValue * 1.2;
+                return (
+                  transferUsdValue >= lowerBound &&
+                  transferUsdValue <= upperBound
+                );
+              })
+            ).then((results) => results.some((result) => result));
+
+            let isNativeTokenValueWithinRange = false;
+            let isNativeTokenUsdValueWithinRange = false;
+
+            if (mintRecipient.toLowerCase() === txEvent.from.toLowerCase()) {
+              // Check if the native token value is within ±20% of the minted amount
+              const nativeTokenValue = txEvent.transaction.value;
+              isNativeTokenValueWithinRange =
+                ethers.BigNumber.from(nativeTokenValue).gte(
+                  amount.sub(amount.div(5))
+                ) &&
+                ethers.BigNumber.from(nativeTokenValue).lte(
+                  amount.add(amount.div(5))
+                );
+
+              // Check if the native token USD value is within ±20% of the mint USD value
+              const nativeTokenUsdValue = await fetcher.getValueInUsd(
+                txEvent.network,
+                nativeTokenValue.toString(),
+                "native"
+              );
+              isNativeTokenUsdValueWithinRange =
+                nativeTokenUsdValue > 0 &&
+                nativeTokenUsdValue >= mintUsdValue * 0.8 &&
+                nativeTokenUsdValue <= mintUsdValue * 1.2;
+            }
+            if (
+              !isTransferUsdValueWithinRange &&
+              !isNativeTokenValueWithinRange &&
+              !isNativeTokenUsdValueWithinRange
+            ) {
+              const toTxCount = await provider.getTransactionCount(
+                mintRecipient,
+                txEvent.blockNumber - 1
+              );
+              if (mintUsdValue > 50000 && toTxCount < 100) {
                 findings.push(
                   createFinding(
                     token,
-                    usdValue.toFixed(2),
+                    mintUsdValue.toFixed(2),
                     txEvent.hash,
-                    to,
-                    FindingSeverity.Medium,
+                    mintRecipient,
+                    FindingSeverity.High,
                     txEvent.from
                   )
                 );
-              }
+              } else if (mintUsdValue === 0 || mintUsdValue > 10000) {
+                if (toTxCount === 0) {
+                  if (mintUsdValue > 10000) {
+                    findings.push(
+                      createFinding(
+                        token,
+                        mintUsdValue.toFixed(2),
+                        txEvent.hash,
+                        mintRecipient,
+                        FindingSeverity.Medium,
+                        txEvent.from
+                      )
+                    );
+                  }
 
-              if (usdValue === 0) {
-                findings.push(
-                  createFinding(
-                    token,
-                    usdValue.toFixed(2),
-                    txEvent.hash,
-                    to,
-                    FindingSeverity.Info,
-                    txEvent.from
-                  )
-                );
+                  if (mintUsdValue === 0) {
+                    const tokenBalance = await fetcher.getBalance(
+                      txEvent.blockNumber,
+                      mintRecipient,
+                      token
+                    );
+
+                    if (
+                      tokenBalance.lte(amount) &&
+                      amount.gt("1000000000000000000")
+                    ) {
+                      let isInitialMint = false;
+                      if (!txEvent.to) {
+                        const createdContractAddress =
+                          ethers.utils.getContractAddress({
+                            from: txEvent.from,
+                            nonce: txEvent.transaction.nonce,
+                          });
+                        isInitialMint =
+                          token.toLowerCase() ===
+                          createdContractAddress.toLowerCase();
+                      }
+                      if (!isInitialMint) {
+                        const totalSupply = await fetcher.getTotalSupply(
+                          txEvent.blockNumber,
+                          token
+                        );
+
+                        // Trigger an alert only if the minted amount is more than / equal to 0.2% of the total supply
+                        if (amount.gte(totalSupply.div(500))) {
+                          findings.push(
+                            createFinding(
+                              token,
+                              mintUsdValue.toFixed(2),
+                              txEvent.hash,
+                              mintRecipient,
+                              FindingSeverity.Info,
+                              txEvent.from
+                            )
+                          );
+                        }
+                      }
+                    }
+                  }
+                }
               }
             }
           }
