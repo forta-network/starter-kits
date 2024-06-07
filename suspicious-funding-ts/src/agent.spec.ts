961106fcfb047c5ba4be4e62fbbadf95708c6d7b
Roberto Cantu <Roberto_Cantu@live.com>
2024-06-07 11:09:02 -0500

    Add 'handleBlock' test suite

--- suspicious-funding-ts/src/agent.spec.ts
+++ suspicious-funding-ts/src/agent.spec.ts
@@ -1,12 +1,13 @@
-import { HandleTransaction, Initialize } from "forta-agent";
+import { HandleTransaction, Initialize, HandleBlock } from "forta-agent";
 import {
   TestTransactionEvent,
+  TestBlockEvent,
   MockEthersProvider,
 } from "forta-agent-tools/lib/test";
 import { createAddress } from "forta-agent-tools";
 import { when } from "jest-when";
 
-import { provideHandleTransaction, provideInitialize } from "./agent";
+import { provideHandleTransaction, provideInitialize, provideHandleBlock } from "./agent";
 import { createFinding } from "./utils";
 
 class MockEthersProviderExtended extends MockEthersProvider {
@@ -34,9 +35,13 @@ class MockEthersProviderExtended extends MockEthersProvider {
 
 describe("Suspicious funding detector bot", () => {
   let handleTransaction: HandleTransaction;
+  let handleBlock: HandleBlock;
   let initialize: Initialize;
 
   const mockProvider = new MockEthersProviderExtended();
+  const mockTruePositiveFetcher = {
+    getTruePositiveList: jest.fn()
+  };
   const mockAttacker1 = createAddress("0x0123");
   const mockAttackers = new Map<string, { origin: string; hops: number }>([
     [mockAttacker1, { origin: "Tornado Cash", hops: 0 }],
@@ -44,85 +49,114 @@ describe("Suspicious funding detector bot", () => {
 
   beforeAll(async () => {
     mockProvider.setNetwork(1);
-    initialize = provideInitialize(mockProvider as any);
+    initialize = provideInitialize(mockProvider as any, mockTruePositiveFetcher as any);
     handleTransaction = provideHandleTransaction(
       mockAttackers,
       mockProvider as any
     );
+    handleBlock = provideHandleBlock(mockTruePositiveFetcher as any);
 
     await initialize();
   });
 
-  it("returns empty findings if there is no native token transfer", async () => {
-    const mockTxEvent = new TestTransactionEvent()
-      .setTo(createAddress("0xabc"))
-      .setValue("0x0");
-    const findings = await handleTransaction(mockTxEvent);
+  describe("handleTransaction", () => {
+    it("returns empty findings if there is no native token transfer", async () => {
+      const mockTxEvent = new TestTransactionEvent()
+        .setTo(createAddress("0xabc"))
+        .setValue("0x0");
+      const findings = await handleTransaction(mockTxEvent);
 
-    expect(findings).toStrictEqual([]);
+      expect(findings).toStrictEqual([]);
+    });
+
+    it("returns empty findings if there is a native token transfer but not from a past attacker", async () => {
+      const mockTxEvent = new TestTransactionEvent()
+        .setFrom(createAddress("0xdef"))
+        .setTo(createAddress("0xabc"))
+        .setValue("0x0");
+      const findings = await handleTransaction(mockTxEvent);
+
+      expect(findings).toStrictEqual([]);
+    });
+
+    it("returns empty findings if there is a native token transfer from an attacker but the value is over the threshold", async () => {
+      const mockTxEvent = new TestTransactionEvent()
+        .setFrom(mockAttacker1)
+        .setTo(createAddress("0xabc"))
+        .setValue("0xDE0B6B3A7640000"); // 1 ETH
+      const findings = await handleTransaction(mockTxEvent);
+
+      expect(findings).toStrictEqual([]);
+    });
+
+    it("returns empty findings if there is a native token transfer from an attacker to an old EOA", async () => {
+      const mockTxTo = createAddress("0xabc");
+      const mockTxEvent = new TestTransactionEvent()
+        .setFrom(mockAttacker1)
+        .setTo(mockTxTo)
+        .setValue("0x01");
+
+      mockProvider.setNonce(mockTxTo, 10000); // old EOA
+
+      const findings = await handleTransaction(mockTxEvent);
+
+      expect(findings).toStrictEqual([]);
+    });
+
+    it("returns empty findings if there is a native token transfer from an attacker to a contract", async () => {
+      const mockTxTo = createAddress("0xabc");
+      const mockTxEvent = new TestTransactionEvent()
+        .setFrom(mockAttacker1)
+        .setTo(mockTxTo)
+        .setValue("0x01");
+
+      mockProvider.setNonce(mockTxTo, 0);
+      mockProvider.setCode(mockTxTo, "0x1234"); // contract
+      const findings = await handleTransaction(mockTxEvent);
+
+      expect(findings).toStrictEqual([]);
+    });
+
+    it("returns a finding if there is a native token transfer from an attacker to new EOA", async () => {
+      const mockTxTo = createAddress("0xabc");
+      const mockTxEvent = new TestTransactionEvent()
+        .setFrom(mockAttacker1)
+        .setTo(mockTxTo)
+        .setValue("0x01");
+
+      mockProvider.setNonce(mockTxTo, 0);
+      mockProvider.setCode(mockTxTo, "0x");
+      const findings = await handleTransaction(mockTxEvent);
+
+      expect(findings).toStrictEqual([
+        createFinding(mockAttacker1, mockTxTo, "Tornado Cash", 1),
+      ]);
+    });
   });
 
-  it("returns empty findings if there is a native token transfer but not from a past attacker", async () => {
-    const mockTxEvent = new TestTransactionEvent()
-      .setFrom(createAddress("0xdef"))
-      .setTo(createAddress("0xabc"))
-      .setValue("0x0");
-    const findings = await handleTransaction(mockTxEvent);
+  describe("handleBlock", () => {
+    it("doesn't attempt to fetch True Positive list if it is not the correct block number", async () => {
+      const mockBlockNumber = 55;
+      const mockBlockEvent = new TestBlockEvent().setNumber(mockBlockNumber);
 
-    expect(findings).toStrictEqual([]);
-  });
+      await handleBlock(mockBlockEvent);
 
-  it("returns empty findings if there is a native token transfer from an attacker but the value is over the threshold", async () => {
-    const mockTxEvent = new TestTransactionEvent()
-      .setFrom(mockAttacker1)
-      .setTo(createAddress("0xabc"))
-      .setValue("0xDE0B6B3A7640000"); // 1 ETH
-    const findings = await handleTransaction(mockTxEvent);
+      // Expect only one call, during the `initialize`
+      expect(mockTruePositiveFetcher.getTruePositiveList).toHaveBeenCalledTimes(1)
+    });
 
-    expect(findings).toStrictEqual([]);
-  });
+    it("attempts to fetcher True Positive list if it is the correct block number", async () => {
+      const oneDay = 60 * 60 * 24;
+      // Using Ethereum since the network was set to chainId of `1`
+      const ethBlockTime = 12;
+      const ethBlocksInOneDay = oneDay / ethBlockTime;
 
-  it("returns empty findings if there is a native token transfer from an attacker to an old EOA", async () => {
-    const mockTxTo = createAddress("0xabc");
-    const mockTxEvent = new TestTransactionEvent()
-      .setFrom(mockAttacker1)
-      .setTo(mockTxTo)
-      .setValue("0x01");
+      const mockBlockEvent = new TestBlockEvent().setNumber(ethBlocksInOneDay);
 
-    mockProvider.setNonce(mockTxTo, 10000); // old EOA
+      await handleBlock(mockBlockEvent);
 
-    const findings = await handleTransaction(mockTxEvent);
-
-    expect(findings).toStrictEqual([]);
-  });
-
-  it("returns empty findings if there is a native token transfer from an attacker to a contract", async () => {
-    const mockTxTo = createAddress("0xabc");
-    const mockTxEvent = new TestTransactionEvent()
-      .setFrom(mockAttacker1)
-      .setTo(mockTxTo)
-      .setValue("0x01");
-
-    mockProvider.setNonce(mockTxTo, 0);
-    mockProvider.setCode(mockTxTo, "0x1234"); // contract
-    const findings = await handleTransaction(mockTxEvent);
-
-    expect(findings).toStrictEqual([]);
-  });
-
-  it("returns a finding if there is a native token transfer from an attacker to new EOA", async () => {
-    const mockTxTo = createAddress("0xabc");
-    const mockTxEvent = new TestTransactionEvent()
-      .setFrom(mockAttacker1)
-      .setTo(mockTxTo)
-      .setValue("0x01");
-
-    mockProvider.setNonce(mockTxTo, 0);
-    mockProvider.setCode(mockTxTo, "0x");
-    const findings = await handleTransaction(mockTxEvent);
-
-    expect(findings).toStrictEqual([
-      createFinding(mockAttacker1, mockTxTo, "Tornado Cash", 1),
-    ]);
+      // Expect above call and during the `initialize`
+      expect(mockTruePositiveFetcher.getTruePositiveList).toHaveBeenCalledTimes(2)
+    });
   });
 });
